52279091927b76e72b21e31a8ce7767ea20abb20
Paul-André Duchesne <duchesne@naturalsciences.be>
2009-03-17 13:04:18 +0100

    Added unittest schema for testing purposes and use of pgtap

--- data/db/createschema.sql
+++ data/db/createschema.sql
@@ -1,3 +1,5 @@
 comment on role :dbname is 'Main user';
 create schema authorization :dbname ;
+create schema unittest authorization :dbname ;
 comment on schema :dbname is 'Main collection management tool schema';
+comment on schema unittest is 'Schema for tests purposes only';
