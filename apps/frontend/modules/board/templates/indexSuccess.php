e86a91e60a61d5acf7bea271bbd88e36f6711a2d
Brice Maron <bmaron@naturalsciences.be>
2009-07-12 15:15:36 +0200

    Adding Opened from DB too

--- apps/frontend/modules/board/templates/indexSuccess.php
+++ apps/frontend/modules/board/templates/indexSuccess.php
@@ -1,16 +1,16 @@
 <?php slot('title', __('Dashboard'));  ?>
 <?php include_partial('boardwidget/list') ?>
 <div class="board">
-	<ul class="board_col">
-	<?php $changed_col=false;?>
-	<?php foreach($widgets as $id => $widget):?>
-	  <?php if($changed_col==false && $widget->getColNum()==2):?>
+  <ul class="board_col">
+    <?php $changed_col=false;?>
+    <?php foreach($widgets as $id => $widget):?>
+      <?php if($changed_col==false && $widget->getColNum()==2):?>
 	    <?php $changed_col=true;?>
 	      </ul>
 	      <div class="board_spacer">&nbsp;</div>
 	      <ul class="board_col">
-	  <?php endif;?>
-	  <?php include_partial('boardwidget/wlayout',array('widget' => $widget->getGroupName())) ?>
-	<?php endforeach;?>
-	</ul>
+      <?php endif;?>
+	  <?php include_partial('boardwidget/wlayout',array('widget' => $widget->getGroupName(),'is_opened' => $widget->getOpened())) ?>
+    <?php endforeach;?>
+  </ul>
 </div>
--- apps/frontend/modules/boardwidget/templates/_wlayout.php
+++ apps/frontend/modules/boardwidget/templates/_wlayout.php
@@ -1,8 +1,8 @@
 <?php $widget_content = get_component('boardwidget',$widget); ?>
 <li class="widget">
-    <div class="widget_top_button">
+    <div class="widget_top_button" <?php if(! $is_opened):?> style="display:block"<?php endif;?>>
         <?php echo image_tag('widget_box_expand_button.png', 'alt=Close');?><?php echo image_tag('widget_close.png', 'alt=Collapse');?>
-	</div>
+    </div>
 	<div class="widget_top_bar">
 		<div class="widget_top_bar_button">
             <?php echo image_tag('widget_refresh.png', 'alt=Refresh');?>
@@ -10,10 +10,10 @@
 		</div>
 		<span><?php include_slot('widget_title'); ?></span>
 	</div>
-	<div class="widget_content">
+	<div class="widget_content" <?php if(! $is_opened):?> style="display:none"<?php endif;?>>
 
         <?php echo $widget_content; ?>
-    <div class="widget_bottom_button">
+    <div class="widget_bottom_button" <?php if(! $is_opened):?> style="display:none"<?php endif;?>>
 		<?php echo image_tag('widget_box_expand_up_button.png', 'alt=Collapse');?>
 	</div>
 </li>
