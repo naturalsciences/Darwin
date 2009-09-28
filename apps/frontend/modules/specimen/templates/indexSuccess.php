3349d3b61556fcb2e7b0c54fb067b0479414839a
Brice Maron <bmaron@naturalsciences.be>
2009-09-28 13:16:15 +0200

    Correct tests messup...everything should be ok now

--- apps/frontend/modules/specimen/templates/indexSuccess.php
+++ apps/frontend/modules/specimen/templates/indexSuccess.php
@@ -21,7 +21,7 @@ function retrieve_spec_result(data)
 //     console.log(data);
     if(data[0]=='ok')
     {
-		$('#tab_'+(tab_idx+1)).removeClass('disabled').addClass('enabled');
+		$('#tab_1').removeClass('disabled').addClass('enabled');
 		$('#arrow_right').attr('src','/images/encod_right_enable.png');
     }
     else
@@ -42,54 +42,44 @@ function retrieve_spec_result(data)
 <div class="encoding">
     <?php echo image_tag('encod_left_disable.png','id="arrow_left" alt="Go Previous" class="scrollButtons left"');?>
 	<div class="page">
-			<ul class="tabs">
-				<li class="enabled selected" id="tab_0"> &lt; New Specimen &gt; </li>
-				<li class="disabled" id="tab_1">Individuals</li>
-				<li class="disabled" id="tab_2">Properties</li>
-			</ul>
-		<div class="tab_box" id="slider">
-			<div class="scroll">
-				<!-- the element that will be scrolled during the effect -->
-				<div class="scrollContainer">
-					<div class="panel" id="intro">
-					<form action="<?php echo url_for('specimen/submit') ?>" method="post">
-					<div><ul class="spec_error_list"></ul>
-					
-  <ul class="board_col">
-    <?php $changed_col=false;?>
-    <?php foreach($widgets as $id => $widget):?>
-      <?php if(!$widget->getVisible()) continue;?>
-
-      <?php if($changed_col==false && $widget->getColNum()==2):?>
-	    <?php $changed_col=true;?>
-	      </ul>
-	      <div class="board_spacer">&nbsp;</div>
-	      <ul class="board_col">
-      <?php endif;?>
-	  <?php include_partial('widgets/wlayout', array(
-            'widget' => $widget->getGroupName(),
-            'is_opened' => $widget->getOpened(),
-            'category' => 'specimenwidget',
-            'options' => $form,
-            )
-	  ); ?>
-    <?php endforeach;?>
-    <?php if($changed_col==false):?>
-      </ul>
-      <div class="board_spacer">&nbsp;</div>
-      <ul class="board_col">
-      <?php endif;?>
-  </ul>
-  </div><p class="clear"/>
-    <p>
-        <input type="submit" value="Submit" id="submit_spec_f1"/>
-    </p>
-                    </form>
+		<ul class="tabs">
+			<li class="enabled selected" id="tab_0"> &lt; New Specimen &gt; </li>
+			<li class="disabled" id="tab_1">Individuals</li>
+			<li class="disabled" id="tab_2">Properties</li>
+		</ul>
+ 		<div class="panel" id="intro">
+            <form action="<?php echo url_for('specimen/submit') ?>" method="post">
+                <div><ul class="spec_error_list"></ul>
+                <ul class="board_col">
+                    <?php $changed_col=false;?>
+                    <?php foreach($widgets as $id => $widget):?>
+                        <?php if(!$widget->getVisible()) continue;?>
+                        <?php if($changed_col==false && $widget->getColNum()==2):?>
+                            <?php $changed_col=true;?>
+                            </ul>
+                            <div class="board_spacer">&nbsp;</div>
+                            <ul class="board_col">
+                        <?php endif;?>
+                        <?php include_partial('widgets/wlayout', array(
+                            'widget' => $widget->getGroupName(),
+                            'is_opened' => $widget->getOpened(),
+                            'category' => 'specimenwidget',
+                            'options' => $form,
+                            )); ?>
+                    <?php endforeach;?>
+                        <?php if($changed_col==false):?>
+                            </ul>
+                            <div class="board_spacer">&nbsp;</div>
+                            <ul class="board_col">
+                        <?php endif;?>
+                        </ul>
                     </div>
-                    <div class="panel"> <a href="#" onclick="$('#submit').trigger('click');return false;">Click here</a></div>
-					<div class="panel"> How it Works </div>
-				</div>
-			</div>
+                    <p class="clear"/>
+                    <p>
+                        <input type="submit" value="Submit" id="submit_spec_f1"/>
+                    </p>
+                </form>
+            </div>
 		</div>
 	</div>
 	<?php echo image_tag('encod_right_disable.png','id="arrow_right" alt="Go next" class="scrollButtons right"');?>
