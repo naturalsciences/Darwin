88ce3d9e4f3781ce6b62ec823360a0918ea39c1c
Brice Maron <bmaron@naturalsciences.be>
2009-09-30 14:27:49 +0200

    Adding possibility to add other Tools for Cathy \o/   Works with edit

--- apps/frontend/modules/specimenwidget/templates/_tool.php
+++ apps/frontend/modules/specimenwidget/templates/_tool.php
@@ -1,2 +1,12 @@
 <?php slot('widget_title',__('Collecting Tool'));  ?>
-<?php echo $form['collecting_tool']->renderRow() ?>
+<script type="text/javascript">
+$(document).ready(function () {
+    $('#specimen_tool_add').click(function()
+    {
+        $('#specimen_collecting_tool').after('<input name="specimen[collecting_tool]" id="specimen_collecting_tool_input" type="text"/>');
+        $('#specimen_collecting_tool').remove();
+    });
+});
+</script>
+<?php echo $form['collecting_tool']->renderRow() ?> 
+<?php echo image_tag('add_green.png', array('id'=> 'specimen_tool_add', 'alt'=>'+'));?> Add Another Tool
