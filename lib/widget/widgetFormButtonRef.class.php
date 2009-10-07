b98a72cfd21409752df818d303d6866d6ff6ed1f
Brice Maron <bmaron@naturalsciences.be>
2009-10-07 08:52:21 +0200

    Correcting button for Ref Form

--- lib/widget/widgetFormButtonRef.class.php
+++ lib/widget/widgetFormButtonRef.class.php
@@ -6,15 +6,20 @@ class widgetFormButtonRef extends sfWidgetFormInputHidden
     public function render($name, $value = null, $attributes = array(), $errors = array())
     {
         $values = array_merge(array('text' => '', 'is_empty' => false), is_array($value) ? $value : array());
-
+        $name = $this->getName($value);
         $input = parent::render($name, $value, $attributes, $errors);
-        $input .= '<div id="'.$this->generateId($name).'_name">'.$this->getName($value).'</div>';
+        $input .= '<div id="'.$this->generateId($name).'_name">'.$name.'</div>';
         $input .= '<div id="'.$this->generateId($name).'_button" class="button">';
         $input .= '<img class="left_part" src="/images/button_grey_left.png" alt=""/>';
-        $input .= '<span class="but_text" class="button">'.'Choose !'.'</span>';
+        $input .= '<span class="but_text" class="button">';
+        if($name == '')
+            $input .= __('Choose !');
+        else
+            $input .= __('Change !');
+        $input .= '</span>';
         $input .= '<img class="right_part" src="/images/button_grey_right.png" alt=""/>';
         $input .= '</div><div style="clear: left;"> </div>';
-       
+
         return $input;
     }
     
