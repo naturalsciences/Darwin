3f977a8debabe23fc8ee3ef4e33621c169e6538a
Brice Maron <bmaron@naturalsciences.be>
2009-11-14 00:56:40 +0100

    Test again...not really clean way

--- lib/test/DarwinTestFunctional.class.php
+++ lib/test/DarwinTestFunctional.class.php
@@ -9,6 +9,7 @@ class DarwinTestFunctional extends sfTestFunctional
 
     $conn = Doctrine_Manager::connection();
     $conn->exec("SELECT nextval('taxonomy_id_seq')");
+    $conn->exec("SELECT setval('taxonomy_id_seq',10000)");
     Doctrine::loadData(sfConfig::get('sf_test_dir').'/fixtures');
     return $this;
   }
