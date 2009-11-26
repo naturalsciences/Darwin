75e6f65d6a4800b03ab6bc25fde2865f3e2da1a6
Brice Maron <bmaron@naturalsciences.be>
2009-11-26 13:54:41 +0100

    Correct unseen taxo ref in catalogue

--- apps/frontend/modules/cataloguewidget/templates/_relationRecombination.php
+++ apps/frontend/modules/cataloguewidget/templates/_relationRecombination.php
@@ -6,12 +6,12 @@
     Combination of
     </th>
     <td>
-      <a class="link_catalogue" title="<?php echo __('Recombination');?>" href="<?php echo url_for('catalogue/relation?type=recombined&table=taxonomy&id='.$eid.'&relid='.$renamed[0]) ?>"><?php echo $renamed[5]//Rec Name?></a>
+      <a class="link_catalogue" title="<?php echo __('Recombination');?>" href="<?php echo url_for('catalogue/relation?type=recombined&table='.$table.'&id='.$eid.'&relid='.$renamed[0]) ?>"><?php echo $renamed[5]//Rec Name?></a>
     <td>
   </tr>
   <?php endforeach;?>
 </table>
   <?php if(count($relations) <=1 ):?>
     <br />
-    <?php echo image_tag('add_green.png');?><a title="<?php echo __('Recombination');?>" class="link_catalogue" href="<?php echo url_for('catalogue/relation?type=recombined&table=taxonomy&id='.$eid) ?>"><?php echo __('Add');?></a>
+    <?php echo image_tag('add_green.png');?><a title="<?php echo __('Recombination');?>" class="link_catalogue" href="<?php echo url_for('catalogue/relation?type=recombined&table='.$table.'&id='.$eid) ?>"><?php echo __('Add');?></a>
   <?php endif;?>
--- apps/frontend/modules/cataloguewidget/templates/_relationRename.php
+++ apps/frontend/modules/cataloguewidget/templates/_relationRename.php
@@ -6,12 +6,12 @@
     Renamed to
     </th>
     <td>
-      <a class="link_catalogue" title="<?php echo __('Rename');?>" href="<?php echo url_for('catalogue/relation?type=rename&table=taxonomy&id='.$eid.'&relid='.$renamed[0]) ?>"><?php echo $renamed[5]//Rec Name?></a>
+      <a class="link_catalogue" title="<?php echo __('Rename');?>" href="<?php echo url_for('catalogue/relation?type=rename&table='.$table.'&id='.$eid.'&relid='.$renamed[0]) ?>"><?php echo $renamed[5]//Rec Name?></a>
     <td>
   </tr>
   <?php endforeach;?>
 </table>
   <?php if(count($relations) == 0 ):?>
     <br />
-    <?php echo image_tag('add_green.png');?><a title="<?php echo __('Rename');?>" class="link_catalogue" href="<?php echo url_for('catalogue/relation?type=rename&table=taxonomy&id='.$eid) ?>"><?php echo __('Add');?></a>
+    <?php echo image_tag('add_green.png');?><a title="<?php echo __('Rename');?>" class="link_catalogue" href="<?php echo url_for('catalogue/relation?type=rename&table='.$table.'&id='.$eid) ?>"><?php echo __('Add');?></a>
   <?php endif;?>
