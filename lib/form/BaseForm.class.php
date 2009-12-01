25b3bc98def669abeda5d9276bb63de457de36d2
Brice Maron <bmaron@naturalsciences.be>
2009-12-01 15:08:49 +0100

    migration again

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    darwin
+ * @subpackage form
+ * @author     DB team <collections@naturalsciences.be> 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
