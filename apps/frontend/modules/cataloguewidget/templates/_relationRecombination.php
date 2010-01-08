470bb9979d9a94d23125f7e3e97dca1d17473b01
Brice Maron <bmaron@naturalsciences.be>
2010-01-08 15:24:30 +0100

    Style a bit relation and fix hidden issue

--- apps/frontend/modules/cataloguewidget/templates/_relationRecombination.php
+++ apps/frontend/modules/cataloguewidget/templates/_relationRecombination.php
@@ -1,15 +1,19 @@
 <?php slot('widget_title',__('Recombination'));  ?>
-<table>
+<table class="catalogue_table">
+  <thead>
+    <th>
+     Combination of
+    </th>
+  </thead>
+  <tbody>
   <?php foreach($relations as $renamed):?>
   <tr>
-    <th>
-    Combination of
-    </th>
     <td>
       <a class="link_catalogue" title="<?php echo __('Recombination');?>" href="<?php echo url_for('catalogue/relation?type=recombined&table='.$table.'&id='.$eid.'&relid='.$renamed[0]) ?>"><?php echo $renamed[5]//Rec Name?></a>
     </td>
   </tr>
   <?php endforeach;?>
+  </tbody>
 </table>
   <?php if(count($relations) <=1 ):?>
     <br />
--- apps/frontend/modules/cataloguewidget/templates/_relationRename.php
+++ apps/frontend/modules/cataloguewidget/templates/_relationRename.php
@@ -1,15 +1,19 @@
 <?php slot('widget_title',__('Renamed'));  ?>
-<table>
-  <?php foreach($relations as $renamed):?>
-  <tr>
+<table class="catalogue_table">
+  <thead>
     <th>
     Renamed to
     </th>
+  </thead>
+  <tbody>
+  <?php foreach($relations as $renamed):?>
+  <tr>
     <td>
       <a class="link_catalogue" title="<?php echo __('Rename');?>" href="<?php echo url_for('catalogue/relation?type=rename&table='.$table.'&id='.$eid.'&relid='.$renamed[0]) ?>"><?php echo $renamed[5]//Rec Name?></a>
     </td>
   </tr>
   <?php endforeach;?>
+  </tbody>
 </table>
   <?php if(count($relations) == 0 ):?>
     <br />
