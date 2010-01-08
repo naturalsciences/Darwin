147d4ea545d28b2b9e7707b8a68459c54bd43807
Brice Maron <bmaron@naturalsciences.be>
2010-01-08 12:51:51 +0100

    Fix taxo search criteria

--- lib/model/doctrine/TaxonomyTable.class.php
+++ lib/model/doctrine/TaxonomyTable.class.php
@@ -18,7 +18,7 @@ class TaxonomyTable extends Doctrine_Table
     if($level)
       $q->andWhere('level_ref = ?',$level);
 
-    $q->andWhere("id > 0 ");
+    $q->andWhere("id != 0 ");
     return $q;
   }
 
