f3b36008c7498108d86f203160f191109e3b46c5
Paul-André Duchesne <duchesne@naturalsciences.be>
2010-01-16 14:28:14 +0100

    - Added one more fixture for the igs data so two begins with the same three first chars   - Corrected the script in charge of autocompletion and of ajax geting an id for an ig num because of bugs that were left   - Added options so we can choose to use or not the autocompletion and options to customize it

--- lib/widget/widgetFormInputChecked.class.php
+++ lib/widget/widgetFormInputChecked.class.php
@@ -7,11 +7,15 @@ class widgetFormInputChecked extends sfWidgetFormInputHidden
         parent::configure($options, $attributes);
         $this->addRequiredOption('model');
         $this->addOption('method', '__toString');
-        $this->addOption('link_url', '/#');
+        $this->addRequiredOption('link_url', '#');
         $this->addOption('notExistingAddDisplay', true);
         $this->addOption('notExistingAddTitle', 'This entry do not exist. Would you like we had it ?');
         $this->addOption('notExistingAddValues', array('Yes', 'No'));
         $this->addOption('notExistingAddSelected', 0);
+        $this->addOption('autocomplete', true);
+        $this->addOption('autocomplete_max', 40);
+        $this->addOption('autocomplete_minChars', 3);
+        $this->addOption('autocomplete_autoFill', true);
         $this->addOption('nullable', false);
         $this->setOption('is_hidden', false);
     }
@@ -48,7 +52,7 @@ class widgetFormInputChecked extends sfWidgetFormInputHidden
         $input .= '</li>';
         if($this->getOption('notExistingAddDisplay'))
         {
-          $input .= '<li class="hidden">'.
+          $input .= '<li id="toggledMsg" class="hidden">'.
                     '<label for="'.$this->generateId($name).'_check">'.$this->getOption('notExistingAddTitle').':</label>'.
                     '<select id="'.$this->generateId($name).'_check">';
           foreach ($this->getOption('notExistingAddValues') as $key => $option)
@@ -64,43 +68,61 @@ class widgetFormInputChecked extends sfWidgetFormInputHidden
         $input .= '</select></li></ul>';
         $input .= sprintf(<<<EOF
 <script type="text/javascript">
+  var lastCaller;
+  function updateHiddenId (value, link_url)
+  {
+    $.ajax({type: "GET",
+            url: link_url,
+            data: {'searchedCrit' : value},
+            success: function(html){
+                                     if (html == 'not found')
+                                     {
+                                       if (lastCaller == 'onChange')
+                                       {
+                                         $('#%1\$s').closest('ul').children('li#toggledMsg').show("slow");
+                                       }
+                                     }
+                                     else
+                                     {
+                                       $('#%1\$s').closest('ul').children('li#toggledMsg').slideUp("fast");
+                                       $('#%1\$s').prev().val(html);
+                                     }
+                                     showAfterRefresh($('#%1\$s').closest('.widget_content'));
+                                   }
+           });
+  }
 $(document).ready(function () {
+    var link_url = '%2\$s';
     $('#%1\$s').live('change', function()
     {
+      hideForRefresh($(this).closest('.widget_content'));
       $(this).prev().val('');
-      toggledMsg = $(this).closest('ul').children('li:last');
-      toggledMsg.slideUp("slow");
+      toggledMsg = $(this).closest('ul').children('li#toggledMsg');
       toggledMsg.find('option:first').attr('selected', "selected");
       toggledMsg.find('option:last').removeAttr('selected');
-      link_url = '%2\$s';
-      hideForRefresh($(this).closest('.widget_content'));
-      $.ajax({
-              type: "GET",
-              url: link_url,
-              data: {'searchedCrit' : $(this).val()},
-              success: function(html){
-                                       if (html == 'not found')
-                                       {
-                                         $('#%1\$s').closest('ul').children('li:last').show("slow");
-                                       }
-                                       else
-                                       {
-                                         $('#%1\$s').prev().val(html);
-                                       }
-                                       showAfterRefresh($('#%1\$s').closest('.widget_content'));
-                                     }
-            }
-           );
-      return false;
+      lastCaller = "onChange";
+      updateHiddenId($(this).val(), link_url);
     });
-    $("#%1\$s").autocomplete('%3\$s').setOptions({max: 50, minChars: 3, autoFill: true})
-
+    if ('%4\$s')
+    {
+      $("#%1\$s").autocomplete('%3\$s').setOptions({max: %5\$s, minChars: %6\$s, autoFill: '%7\$s'});
+      $("#%1\$s").result(function(event, data, formatted) 
+                         {
+                           hideForRefresh($(this).closest('.widget_content'));
+                           lastCaller = "result";
+                           updateHiddenId(formatted, link_url);
+                         });
+    }
 });
 </script>
 EOF
     , $this->generateId($name).'_name',
       url_for($this->getOption('link_url')),
-      url_for($this->getOption('link_url').'Limited'));
+      url_for($this->getOption('link_url').'Limited'),
+      $this->getOption('autocomplete'),
+      $this->getOption('autocomplete_max'),
+      $this->getOption('autocomplete_minChars'),
+      $this->getOption('autocomplete_autoFill'));
         return $input;
      }  
 }
