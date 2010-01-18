8e006dd43d45cb164dea350733a0dc8c15822b92
Paul-André Duchesne <duchesne@naturalsciences.be>
2010-01-18 17:03:52 +0100

    - Tests for insurances are ok (normally... feel free to give an eye on it ;) )

--- test/functional/frontend/insurancesActionsTest.php
+++ test/functional/frontend/insurancesActionsTest.php
@@ -2,6 +2,68 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
+Doctrine_Query::create()->delete('Insurances')->execute();
+
 $browser = new DarwinTestFunctional(new sfBrowser());
 $browser->loadData($configuration)->login('root','evil');
 
+$items = Doctrine::getTable('Igs')->findByIgNum('10795');
+
+$browser->
+  info('New Insurance for a given IG')->
+  get('/insurances/add?table=igs&id='.$items[0]->getId())->
+  with('response')->
+  begin()->
+    isStatusCode()->
+    checkElement('div#insurances_screen')->
+    checkElement('form[class="edition"][id="insurances_form"]')->
+    checkElement('form input[type="hidden"][name="insurances[referenced_relation]"][value="igs"]')->
+    checkElement('form input[type="hidden"][name="insurances[record_id]"][value="'.$items[0]->getId().'"]')->
+    checkElement('form table tbody tr:nth-child(2) label[for="insurances_insurance_value"]')->
+    checkElement('form table tbody tr:nth-child(2) input[id="insurances_insurance_value"]')->
+    checkElement('form table tbody tr:nth-child(3) input[id="insurances_insurance_currency_input"][value="€"]')->
+    checkElement('form table tbody tr:nth-child(3) select option', 0)->
+    checkElement('form table tbody tr:nth-child(4) select[id="insurances_insurance_year"] option[value="0"][selected="selected"]', '-')->
+  end()->
+  info('Try to save no value for IG 10795: wait for value required error...')->
+  click('Save', 
+        array('insurances' => array('insurance_value' => '',
+                                    'insurance_currency' => '€',
+                                    'insurance_year' => 0,
+                                   )
+             )
+       )->
+  with('form')->
+  begin()->
+    hasErrors(1)->
+    isError('insurance_value', 'required')->
+  end()->
+  info('Save 750€ value for IG 10795')->
+  click('Save', 
+        array('insurances' => array('insurance_value' => '750',
+                                    'insurance_currency' => '€',
+                                    'insurance_year' => 0,
+                                   )
+             )
+       )->
+  with('response')->
+  begin()->
+    checkElement('form table tbody tr:nth-child(3) input[id="insurances_insurance_currency_input"][value=""][class="hidden"]')->
+    checkElement('form table tbody tr:nth-child(3) select option[value="€"]', '€')->
+  end();
+
+$items = Doctrine::getTable('Insurances')->findByInsuranceValue('750');
+
+$browser->
+  info('Delete last insurance inserted...')->
+  get('insurances/delete?id='.$items[0]->getId())->
+  with('response')->
+  begin()->
+    isStatusCode()->
+  end();
+
+$counting = Doctrine_Query::create()->select('count(i.id) as counting')->from('insurances i')->execute();
+
+$browser->
+  info('Check record has been deleted from DB')->
+  test()->is($counting[0]->getCounting(),0);
