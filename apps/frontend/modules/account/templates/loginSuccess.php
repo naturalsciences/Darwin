a5987c92d91f999cf9c5858823409612172f010e
Brice Maron <bmaron@naturalsciences.be>
2010-01-19 15:58:38 +0100

    Correct typo

--- apps/frontend/modules/account/templates/loginSuccess.php
+++ apps/frontend/modules/account/templates/loginSuccess.php
@@ -1,5 +1,5 @@
 <div class="login">
-    <?php echo __('This application require a authentication.
+    <?php echo __('This application requires an authentication.
     Please Log in with your username and password.');?>
     <form action="<?php echo url_for('account/login') ?>" method="post">
         <ul class="form">
