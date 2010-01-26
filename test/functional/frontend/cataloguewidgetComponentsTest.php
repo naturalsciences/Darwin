2f720c61ac5d216ceca0a05cbb65198f6de09314
Paul-André Duchesne <duchesne@naturalsciences.be>
2010-01-26 13:06:01 +0100

    - Modified the catalogueWidgets tests for vernacular names

--- test/functional/frontend/cataloguewidgetComponentsTest.php
+++ test/functional/frontend/cataloguewidgetComponentsTest.php
@@ -5,6 +5,15 @@ include(dirname(__FILE__).'/../../bootstrap/functional.php');
 $browser = new DarwinTestFunctional(new sfBrowser());
 $browser->loadData($configuration)->login('root','evil');
 
+$vernacular_names = new ClassVernacularNames;
+
+$vernacular_names->setCommunity('French');
+$vernacular_names->setReferencedRelation('taxonomy');
+$vernacular_names->setRecordId(4);
+$vernacular_names->VernacularNames[]->name ='Faux con';
+$vernacular_names->VernacularNames[]->name ='Con de pélerin';
+$vernacular_names->save();
+
 $browser->
     info('1 - Rename')->
      get('/widgets/reloadContent?widget=relationRename&category=catalogue_taxonomy&eid=4')->
@@ -58,4 +67,13 @@ $browser->
 	checkElement('table tbody tr:first td::nth-child(6)','/Show 2 Values/')->
 	checkElement('table tbody tr:last td:first','/protection status/')->
 	checkElement('table tbody tr:last td::nth-child(6)','/Show 1 Value/')->
+    end()->
+
+    info('5 - Vernacular Names')->
+     get('/widgets/reloadContent?widget=vernacularNames&category=catalogue_taxonomy&eid=4')->
+     with('response')->begin()->
+        isStatusCode(200)->
+        checkElement('table tbody tr',1)->
+        checkElement('table tbody tr:first td:first','/French/')->
+	checkElement('table tbody tr:first td::nth-child(2)','/Show 2 Names/')->
     end();
