a0a947808eeacbcb240ae0b3db4e9801eb6facf1
Paul-André Duchesne <duchesne@naturalsciences.be>
2010-02-10 12:12:55 +0100

    - - Test corrected

--- test/functional/frontend/insurancesActionsTest.php
+++ test/functional/frontend/insurancesActionsTest.php
@@ -46,6 +46,7 @@ $browser->
                                    )
              )
        )->
+  get('/insurances/add?table=igs&id='.$items[0]->getId())->
   with('response')->
   begin()->
     checkElement('form table tbody tr:nth-child(3) input[id="insurances_insurance_currency_input"][value=""][class="hidden"]')->
