e908ecfec5611e0dedfdf63ec358f16c357803b1
Brice Maron <bmaron@naturalsciences.be>
2010-02-15 16:03:50 +0100

    Correct tests

--- data/db/tests/chk_one_prefered_language.sql
+++ data/db/tests/chk_one_prefered_language.sql
@@ -3,26 +3,26 @@
 \i unit_launch.sql
 SELECT plan(26);
 --select count(*) from people_languages;
-SELECT ok(fct_chk_one_pref_language(1, true), 'Table people_languages. No preferred language yet -> is ok');
-SELECT ok(fct_chk_one_pref_language(1, false), 'Table people languages. No preferred language yet -> is ok');
-SELECT ok(fct_chk_one_pref_language(1, true, 'users'), 'Table users_languages. No preferred language yet -> is ok');
-SELECT ok(fct_chk_one_pref_language(1, false, 'users'), 'Table users_languages. No preferred language yet -> is ok');
+SELECT ok(fct_chk_one_pref_language(1,1, true), 'Table people_languages. No preferred language yet -> is ok');
+SELECT ok(fct_chk_one_pref_language(1,1, false), 'Table people languages. No preferred language yet -> is ok');
+SELECT ok(fct_chk_one_pref_language(1,1, true, 'users'), 'Table users_languages. No preferred language yet -> is ok');
+SELECT ok(fct_chk_one_pref_language(1,1, false, 'users'), 'Table users_languages. No preferred language yet -> is ok');
 
-SELECT lives_ok('INSERT INTO people_languages (people_ref) VALUES (1)', 'People: First data and not a preferred language -> should return ok');
-SELECT lives_ok('INSERT INTO users_languages (users_ref) VALUES (1)', 'Users: First data and not a preferred language -> should return ok');
+SELECT lives_ok('INSERT INTO people_languages (id, people_ref) VALUES (10,1)', 'People: First data and not a preferred language -> should return ok');
+SELECT lives_ok('INSERT INTO users_languages (id, users_ref) VALUES (11,1)', 'Users: First data and not a preferred language -> should return ok');
 
-SELECT ok(fct_chk_one_pref_language(1, true), 'Table people_languages. No preferred language yet -> is ok');
-SELECT ok(fct_chk_one_pref_language(1, false), 'Table people languages. No preferred language yet -> is ok');
-SELECT ok(fct_chk_one_pref_language(1, true, 'users'), 'Table users_languages. No preferred language yet -> is ok');
-SELECT ok(fct_chk_one_pref_language(1, false, 'users'), 'Table users_languages. No preferred language yet -> is ok');
+SELECT ok(fct_chk_one_pref_language(10,1, true), 'Table people_languages. No preferred language yet -> is ok');
+SELECT ok(fct_chk_one_pref_language(10,1, false), 'Table people languages. No preferred language yet -> is ok');
+SELECT ok(fct_chk_one_pref_language(11,1, true, 'users'), 'Table users_languages. No preferred language yet -> is ok');
+SELECT ok(fct_chk_one_pref_language(11,1, false, 'users'), 'Table users_languages. No preferred language yet -> is ok');
 
-SELECT lives_ok('INSERT INTO people_languages (people_ref, language_country, preferred_language) VALUES (1, ''fr_be'', true)', 'People: First preferred language inserted -> should be ok');
-SELECT lives_ok('INSERT INTO users_languages (users_ref, language_country, preferred_language) VALUES (1, ''fr_be'', true)', 'Users: First preferred language inserted -> should be ok');
+SELECT lives_ok('INSERT INTO people_languages (id, people_ref, language_country, preferred_language) VALUES (13,1, ''fr_be'', true)', 'People: First preferred language inserted -> should be ok');
+SELECT lives_ok('INSERT INTO users_languages (id, users_ref, language_country, preferred_language) VALUES (14,1, ''fr_be'', true)', 'Users: First preferred language inserted -> should be ok');
 
-SELECT ok(not fct_chk_one_pref_language(1, true), 'Table people_languages. One preferred language yet -> should not be ok');
-SELECT ok(fct_chk_one_pref_language(1, false), 'Table people languages. One preferred language yet -> should be ok');
-SELECT ok(not fct_chk_one_pref_language(1, true, 'users'), 'Table users_languages. One preferred language yet -> should not be ok');
-SELECT ok(fct_chk_one_pref_language(1, false, 'users'), 'Table users_languages. One preferred language yet -> should be ok');
+SELECT ok(not fct_chk_one_pref_language(1, 1, true), 'Table people_languages. One preferred language yet -> should not be ok');
+SELECT ok(fct_chk_one_pref_language(13, 1, false), 'Table people languages. One preferred language yet -> should be ok');
+SELECT ok(not fct_chk_one_pref_language(1, 1, true, 'users'), 'Table users_languages. One preferred language yet -> should not be ok');
+SELECT ok(fct_chk_one_pref_language(14, 1, false, 'users'), 'Table users_languages. One preferred language yet -> should be ok');
 
 SELECT throws_ok('INSERT INTO people_languages (people_ref, language_country, preferred_language) VALUES (1, ''nl_be'', true)', '23514');
 SELECT throws_ok('INSERT INTO users_languages (users_ref, language_country, preferred_language) VALUES (1, ''fr_be'', true)', '23514');
