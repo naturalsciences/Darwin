7f09a30412cda7d940a49be86a04a8eb7531726e
Brice Maron <bmaron@gmail.com>
2010-03-24 19:29:19 +0100

    Remove testing prints
    Signed-off-by: Brice Maron <bmaron@gmail.com>

--- data/db/tests/formated_name.sql
+++ data/db/tests/formated_name.sql
@@ -5,8 +5,6 @@ SELECT plan(18);
 insert into people (id, db_people_type, is_physical,family_name, given_name, birth_date, gender,end_date)
  VALUES (3,6, true, 'Zé Doe', 'Jo-zé', DATE 'June 20, 1989', 'M', DEFAULT);
 
-SELECT formated_name FROM people WHERE id=3;
-
 SELECT ok( 'Zé Doe Jo-zé' = (SELECT formated_name FROM people WHERE id=3),'formated_name composed');
 
 
