c39ac682fba56d8b10a49510ac7ea7750c669225
Brice Maron <bmaron@naturalsciences.be>
2010-03-31 15:42:23 +0200

    Add tag Group Unit tests refs #32

---
+++ test/unit/model/TagGroupsTableTest.php
@@ -0,0 +1,42 @@
+<?php 
+include(dirname(__FILE__).'/../../bootstrap/Doctrine.php');
+$t = new lime_test(16, new lime_output_color());
+
+$t->info('distinct SubGroup()');
+$sgroups= Doctrine::getTable('TagGroups')->getDistinctSubGroups('administrative');
+$t->is(count($sgroups),3, 'Get all administrative sub groups');
+$t->is($sgroups['country'],'country', 'Country is set');
+
+$sgroups= Doctrine::getTable('TagGroups')->getDistinctSubGroups('brol');
+
+$t->is(count($sgroups),1, 'Get administrative sub groups for this unused');
+$t->is($sgroups[''],'', 'thre is only the empty');
+
+$t->info('getPropositions');
+
+$props = Doctrine::getTable('TagGroups')->getPropositions('brussels');
+$t->is(count($props),4, 'We got 4 props');
+$t->is(count($props[0]),3, 'With 3 property in each');
+$t->is($props[0]['tag'],'Big White Mountain', 'Purpose from all groups is showed');
+
+
+$props = Doctrine::getTable('TagGroups')->getPropositions('Bruselo');
+$t->is(count($props),1, 'Got 1 prop');
+$t->is($props[0]['tag'],'Brussels', 'Brussels is showed');
+
+
+$props = Doctrine::getTable('TagGroups')->getPropositions('brussels','administrative', 'city');
+$t->is(count($props),3, 'We got 3 props');
+$t->isnt($props[0]['tag'],'Big White Mountain', 'Purpose from only 1 group');
+
+$t->info('fetchTag');
+
+$gtu = Doctrine::getTable('Gtu')->findOneByCode('irsnb');
+$tags = Doctrine::getTable('TagGroups')->fetchTag(array($gtu->getId()));
+$t->is(count($tags),1, 'We got 1 gtu');
+
+$t->is(count($tags[$gtu->getId()]),2, 'We got 2 group');
+$t->is($tags[$gtu->getId()][0]->getGroupName(), 'administrative', 'administrative is the  group');
+$t->is($tags[$gtu->getId()][0]->getSubGroupName(), 'country', 'country is the sub group');
+$t->is(count($tags[$gtu->getId()][0]->Tags),4, 'We got 4 tags');
+
