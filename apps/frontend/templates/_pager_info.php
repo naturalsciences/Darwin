a4d9305ad6b8d974a701a199d0eeee91dfc7547c
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2010-04-06 09:16:18 +0200

    Corrected the record(s) translation that caused the tests to fail

--- apps/frontend/templates/_pager_info.php
+++ apps/frontend/templates/_pager_info.php
@@ -3,7 +3,7 @@
       <table>
         <tr>
           <td><?php echo image_tag('info2.png');?></td>
-          <td><?php $recTotal = $pagerLayout->getPager()->getNumResults();echo __('Your query retrieved').'&nbsp;'.$recTotal.'&nbsp;'.__('records');?></td>
+          <td><?php $recTotal = $pagerLayout->getPager()->getNumResults(); $recordLabel = ($recTotal > 1)?'records':'record'; echo __('Your query retrieved').'&nbsp;'.$recTotal.'&nbsp;'.__($recordLabel);?></td>
           <td><ul><li><?php echo $form['rec_per_page']->renderLabel(); echo $form['rec_per_page']->render(); ?></li></ul></td>
         </tr>
       </table>
