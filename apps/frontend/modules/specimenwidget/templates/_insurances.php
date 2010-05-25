9af535c633ceee6d238424cc532a451c8e27a860
Brice Maron <bmaron@naturalsciences.be>
2010-05-25 15:18:54 +0200

    Add 2 widgets to parts

---
+++ apps/frontend/modules/specimenwidget/templates/_insurances.php
@@ -0,0 +1,1 @@
+<?php echo get_component('cataloguewidget', 'insurances', array('table' => "specimen_parts", 'eid' => $eid));?>
--- test/unit/model/MyPreferencesTableTest.php
+++ test/unit/model/MyPreferencesTableTest.php
@@ -147,7 +147,7 @@ $q = Doctrine_Query::create()
     ->from('MyPreferences p')
     ->Where('p.user_ref = ?', $brol_user->getId())
     ->execute();
-$t->is($q->count(),75,'Now brolus has his 75 widgets') ; 
+$t->is($q->count(),77,'Now brolus has his 77 widgets') ; 
 
 $t->comment('->updateWigetsAvailabilityForRole()');  
 $t->is(count(Doctrine::getTable('MyPreferences')
