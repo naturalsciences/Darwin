3c2cc94e3bab762b5005cf951e00ac555f467248
Brice Maron <bmaron@naturalsciences.be>
2010-05-26 11:03:10 +0200

    Correct a bit arrows layout, Add prevention for Back/forward button...Plz report pbms

--- apps/frontend/modules/specimen/templates/_specAfterTab.php
+++ apps/frontend/modules/specimen/templates/_specAfterTab.php
@@ -1,8 +1,8 @@
        </div>
 	</div>
-  </div>
 <?php if(!isset($specimen) || $specimen->isNew()):?>
   <?php echo image_tag('encod_right_disable.png','id="arrow_right" alt="'.__('Go next').'" class="scrollButtons right"');?>
 <?php else:?>
   <?php echo link_to(image_tag('encod_right_enable.png','id="arrow_right" alt="'.__('Go next').'" class="scrollButtons right"'),'individuals/edit?id='.$specimen->getId());?>
 <?php endif;?>
+  </div>
