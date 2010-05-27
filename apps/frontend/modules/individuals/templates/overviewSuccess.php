ffc9b043c105b03ca969c5ab910f3e6c9117d5a1
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2010-05-27 18:11:37 +0200

    Step further for specimen individuals
    refs #126

--- apps/frontend/modules/individuals/templates/overviewSuccess.php
+++ apps/frontend/modules/individuals/templates/overviewSuccess.php
@@ -2,21 +2,73 @@
 
 <?php include_partial('specimen/specBeforeTab', array('specimen' => $specimen, 'mode'=>'individuals_overview'));?>
 
-<table>
+<table class="property_values">
   <thead style="<?php echo (count($individuals))?'':'display: none;';?>">
     <tr>
+      <th>
+	<?php echo __('Type');?>
+      </th>
+      <th>
+	<?php echo __('Sex');?>
+      </th>
+      <th>
+	<?php echo __('State');?>
+      </th>
+      <th>
+	<?php echo __('Stage');?>
+      </th>
+      <th>
+	<?php echo __('Social status');?>
+      </th>
+      <th>
+	<?php echo __('Rock form');?>
+      </th>
+      <th colspan="3">
+      </th>
     </tr>
   </thead>
   <tbody>
     <?php foreach($individuals as $i => $individual):?>
       <tr>
 	<td>
-	  <?php echo $individual->getType();?> - <?php echo $individual->getSex();?>
+	  <?php echo $individual->getType();?>
+	</td>
+	<td>
+	  <?php echo $individual->getSex();?>
+	</td>
+	<td>
+	  <?php echo $individual->getState();?>
+	</td>
+	<td>
+	  <?php echo $individual->getStage();?>
+	</td>
+	<td>
+	  <?php echo $individual->getSocialStatus();?>
+	</td>
+	<td>
+	  <?php echo $individual->getRockForm();?>
+	</td>
+	<td>
+	  <?php echo link_to(image_tag('edit.png'),'individuals/edit?spec_id='.$specimen->getId().'&individual_id='.$individual->getId());?>
+	</td>
+	<td>
+	  <?php echo link_to(image_tag('remove.png'),'individuals/delete?individual_id='.$individual->getId());?>
+	</td>
+	<td>
 	  <?php echo link_to(image_tag('slide_right_enable.png'),'parts/edit?id='.$individual->getId(), array('class'=>'part_detail_slide'));?>
 	</td>
       </tr>
     <?php endforeach;?>
   </tbody>
+  <tfoot>
+    <tr>
+      <td colspan='9'>
+        <div class="add_spec_individual">
+          <a href="<?php echo url_for('individuals/edit?spec_id='.$specimen->getId());?>" id="add_spec_individual"><?php echo __('Add Individual');?></a>
+        </div>
+      </td>
+    </tr>
+  </tfoot>
 </table>
 
 <?php include_partial('specimen/specAfterTab');?>
--- apps/frontend/modules/specimen/templates/_specAfterTab.php
+++ apps/frontend/modules/specimen/templates/_specAfterTab.php
@@ -3,6 +3,6 @@
 <?php if(!isset($specimen) || $specimen->isNew()):?>
   <?php echo image_tag('encod_right_disable.png','id="arrow_right" alt="'.__('Go next').'" class="scrollButtons right"');?>
 <?php else:?>
-  <?php echo link_to(image_tag('encod_right_enable.png','id="arrow_right" alt="'.__('Go next').'" class="scrollButtons right"'),'individuals/edit?id='.$specimen->getId());?>
+  <?php echo link_to(image_tag('encod_right_enable.png','id="arrow_right" alt="'.__('Go next').'" class="scrollButtons right"'),'individuals/overview?spec_id='.$specimen->getId());?>
 <?php endif;?>
   </div>
