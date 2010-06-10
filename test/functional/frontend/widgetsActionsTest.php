3e0e658a12e978860115a8a688aecffb66b17eff
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2010-06-10 18:01:53 +0200

    Tests corrected

--- test/functional/frontend/widgetsActionsTest.php
+++ test/functional/frontend/widgetsActionsTest.php
@@ -29,7 +29,7 @@ $browser->
   get('/specimen/new')->
   with('response')->begin()->
     checkElement('.board_col:first .widget',5)->
-    checkElement('.board_col:last .widget',7)->
+    checkElement('.board_col:last .widget',8)->
   end()->
   
   info('1.3 - add widget when the category is ok')->
