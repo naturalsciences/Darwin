2f529ae6dd871f05965c10bee094c6d84b58ae6b
Brice Maron <bmaron@naturalsciences.be>
2010-06-15 11:11:46 +0200

    Change the way named queries are searched. Lookup once for words and search after in table Allow -- query should fixes #114,#135

--- lib/filter/doctrine/BaseFormFilterDoctrine.class.php
+++ lib/filter/doctrine/BaseFormFilterDoctrine.class.php
@@ -55,24 +55,165 @@ abstract class BaseFormFilterDoctrine extends sfFormFilterDoctrine
     return intval(sfConfig::get('app_catalogueRecLimit'));
   }
 
+  /**
+  * Add query for table containing "name" and "name_indexed" columns
+  * @param Doctrine_Query $query an existing doctrine query
+  * @param string $table a table where we should look at
+  * @param string $field The field searched
+  * @param string $values The value of the search field
+  * @return Doctrine_Query the modified doctrine query
+  */
   public function addNamingColumnQuery(Doctrine_Query $query, $table, $field, $values)
   {
-	$conn = Doctrine_Manager::connection();
 	if ($values != "" && $table != "" && $field != "")
 	{
-	  $values = preg_replace('/[^A-Za-z0-9\-_]/', ' ', $values);
-	  $words = explode(" ", $values);
-	  foreach($words as $word)
-	  {
-		if($word=='') continue;
+	  $alias = $query->getRootAlias();
+	  $search = self::splitNameQuery($values);
+	  $terms = self::getAllTerms($search);
+	  if(empty($terms)) return $query;
+	  
+	  $conn_MGR = Doctrine_Manager::connection();
+	  $conn = $conn_MGR->getDbh();
+	  $pg_array_string  = '';
+	  foreach($terms as $term)
+		$pg_array_string .= $conn_MGR->quote($term, 'string').',';
 
-		$qWord = $conn->quote($word, 'string');
-		$query->andWhere($field . " @@ search_words_to_query('" . $table . "' , '" . $field . "', ".$qWord. " , 'contains') ");
-		//UGLY HAckish hack...
-		//$query->andWhere($field . " @@ search_words_to_query('" . $table . "' , '" . $field . "', ? , 'contains') ",$word);
+	  $pg_array_string = substr($pg_array_string, 0, -1); //remove last ','
+
+	  $statement = $conn->prepare("SELECT vt.anyelement as search, word from fct_explode_array(array[$pg_array_string]) as vt  
+		LEFT JOIN words on word % vt.anyelement
+		WHERE referenced_relation = :table
+		AND field_name = :field
+		AND word like '%' || vt.anyelement || '%'
+		GROUP BY word,  vt.anyelement");
+
+	  $statement->execute(array(':table' => $table, ':field' => $field));
+	  $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+	  //print_r($results);
+	  if(count($results) == 0) return $query;
+
+	  foreach ($search['with'] as $search_term)
+	  {
+		$tsquery =  self::getWordsForTerms($search_term, $results);
+		$query->andWhere($alias.'.'.$field." @@ to_tsquery('simple',?) ",$tsquery);
 	  }
+
+	  unset($search['with']);
+	  foreach($search as $search_group)
+	  {
+		$tsquery_arr = array();
+		foreach($search_group as $search_term)
+		{
+		  $tsquery_arr[] =  self::getWordsForTerms($search_term, $results);
+		}
+		$tsquery = implode(' & ',$tsquery_arr);
+		$tsquery = $conn_MGR->quote('!('.$tsquery.')','string');
+		$query->andWhere($alias.'.'.$field." @@ to_tsquery('simple',".$tsquery.") ");
+	  }
+	  
 	}
 	return $query;
+  }
+
+  /**
+  * Get a searched term and give a string containing all proposition separeted by $separator 
+  * @param string $term the searched term
+  * @param string $proposition the array of propositions fetched in word db
+  * @param string $separator the separator between fields
+  * @return a string containing all proposition for this term separeted by $separator
+  */
+  protected static function getWordsForTerms($term, $propositions, $separator = '|' )
+  {
+	$str = '';
+	foreach($propositions as $i => $result)
+	{
+	  if($result['search'] == $term)
+	  {
+		if($str == '')
+		  $str = $result['word'];
+		else
+		  $str .= $separator.$result['word'];
+	  }
+	}
+	return $str;
+  }
+
+  /**
+  * splitNameQuery: Analyse a query string to split elements 
+  * in remove all non-alphanum char (except - and _ ) 
+  * The array si organized with a 'with' key (all search terms) and other keys represent a 'NOT' search
+  * Terms will be trimed and -- is used as not term
+  * falco, peregrin (1985) --brolus test -- choze will be exported as:
+  * Array ( [with] => Array(
+  *          [0] => falco
+  *          [1] => peregrin
+  *          [2] => 1985)
+  *   [0] => Array(
+  *          [0] => brolus
+  *          [1] => test)
+  *   [1] => Array(
+  *          [0] => choze)
+  * )
+  *
+  * @param string $query_string The search string
+  * @return array an array of analysed terms
+  */
+  public static function splitNameQuery($query_string)
+  {
+	$results = preg_split('/--/',$query_string);
+	$query_array = array('with'=>'', 'without'=>array());
+	foreach($results as $i => $query_part)
+	{
+	  if(trim($query_part) == '')
+		continue;
+	  
+	  $query_part = preg_replace('/[^A-Za-z0-9\-_]/', ' ', $query_part);
+
+	  if($i == 0)
+		$query_array['with'] = trim($query_part);
+	  else
+		$query_array['without'][] = trim($query_part);
+	}
+
+	$searched_terms = array();
+	foreach(array_merge(array('with'=>$query_array['with']),$query_array['without']) as $i => $query_words)
+	{
+	  $words = explode(" ", $query_words);
+	  $searched_terms[$i] = array();
+	  foreach($words as $word)
+	  {
+		$word = trim($word);
+		
+		if($word == '') continue;
+		
+		$searched_terms[$i][] = $word;
+	  }
+	  if(empty($searched_terms[$i]))
+		unset($searched_terms[$i]);
+	}
+	if(!isset($searched_terms['with']))
+	  $searched_terms['with'] = array();
+
+	return $searched_terms;
+  }
+
+  /**
+  * Extract query items from the analysed array
+  * @param array analysed array with key with and without and query splitted in terms
+  * @return array array with all terms searched
+  */
+  protected static function getAllTerms($analysed_array)
+  {
+	$terms = array();
+	foreach($analysed_array as $key => $analysed)
+	{
+	  foreach($analysed as $query)
+	  {
+		$terms[] = $query;
+	  }
+	}
+	return $terms;
   }
 
   public function addDateFromToColumnQuery(Doctrine_Query $query, array $dateFields, $val_from, $val_to)
