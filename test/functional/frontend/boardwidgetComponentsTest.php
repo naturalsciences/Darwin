4f82a15f415231b03be4942e1c09515c4d9ffbca
Brice Maron <bmaron@naturalsciences.be>
2010-08-11 14:40:52 +0200

    Correct tests

--- test/functional/frontend/boardwidgetComponentsTest.php
+++ test/functional/frontend/boardwidgetComponentsTest.php
@@ -10,10 +10,10 @@ $browser->
     get('/widgets/reloadContent?widget=savedSearch&category=board')->
     with('response')->begin()->
         isStatusCode(200)->
-        checkElement('ul.saved_search_widget li',2)->
-        checkElement('ul.saved_search_widget li img[src$="favorite_off.png"]',1)->
-        checkElement('ul.saved_search_widget li img[src$="favorite_on.png"]',1)->
-        checkElement('ul.saved_search_widget li:first img[src$="favorite_on.png"]',1)->
+        checkElement('.saved_searches_board',1)->
+        checkElement('.saved_searches_board img.favorite_off.hidden',1)->
+        checkElement('.saved_searches_board img.favorite_on.hidden',1)->
+        checkElement('.saved_searches_board tr:first img.favorite_on',1)->
     end()->
 
     info('1 - SavedSpecimens')->
