d4a235312faa078a2cf32010b4ca9c7ec86f5e72
Brice Maron <bmaron@naturalsciences.be>
2010-08-25 10:22:18 +0200

    Add error messages to codes fix #252

--- apps/frontend/modules/specimensearchwidget/templates/_codeline.php
+++ apps/frontend/modules/specimensearchwidget/templates/_codeline.php
@@ -1,4 +1,12 @@
 <tr>
+  <td><?php echo $code['category']->renderError();?></td>
+  <td><?php echo $code['code_part']->renderError();?></td>
+  <td></td>
+  <td><?php echo $code['code_from']->renderError();?></td>
+  <td><?php echo $code['code_to']->renderError();?></td>
+  <td></td>
+</tr>
+<tr>
   <td><?php echo $code['category'];?></td>
   <td><?php echo $code['code_part'];?></td>
   <td class="and_col">
@@ -6,7 +14,7 @@
     <?php echo link_to(image_tag('previous.png'),'specimen/index', array('class'=>'code_between hidden prev'));?>
   </td>
   <td class="between_col"><?php echo $code['code_from'];?></td>
-  <td class="between_col"><?php echo $code['code_to'];?></td>
+  <td class="between_col"><?php echo $code['code_to']->renderError();?><?php echo $code['code_to'];?></td>
   <td>
     <?php echo image_tag('remove.png', 'alt=Delete class=clear_prop id=clear_code_'.$row_line); ?>
   </td>
@@ -15,13 +23,15 @@
   $('#clear_code_<?php echo $row_line;?>').click(function(event)
   {
     event.preventDefault();
-    if($(this).closest('tbody').find('tr').length == 2)
+    if($(this).closest('tbody').find('tr').length == 3)
     {
       $(this).closest('tr').find('td input').val('');
     }
     else
     {
+      other_row = $(this).closest('tr').prev();
       $(this).closest('tr').remove();
+      other_row.remove();
     }
     checkBetween();
   });
--- lib/validator/CodesLineValidatorSchema.class.php
+++ lib/validator/CodesLineValidatorSchema.class.php
@@ -3,6 +3,7 @@ class CodesLineValidatorSchema extends sfValidatorSchema
 {
   protected function configure($options = array(), $messages = array())
   {
+    $this->addMessage('wrong_type', '"Between" concerns only numerical parts of codes');
   }
  
   protected function doClean($value)
@@ -18,6 +19,11 @@ class CodesLineValidatorSchema extends sfValidatorSchema
     if($value['code_from']==="" && $value['code_to'])
     {
       $errorSchemaLocal->addError(new sfValidatorError($this, 'required'), 'code_from');
+    }
+
+    if($value['code_from'] !=="" && $value['code_to'] !=="" && ( !ctype_digit($value['code_from']) || !ctype_digit($value['code_to'])) )
+    {
+      $errorSchemaLocal->addError(new sfValidatorError($this, 'wrong_type'), 'code_from');
     }
 
     if (!$value['code_from'] && !$value['code_to'] && !$value['code_part'])
