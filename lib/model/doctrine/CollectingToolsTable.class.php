1d26fe7b9ef0201fb024d5bb709215159ff16f6a
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2010-08-30 09:09:55 +0200

    Indentations in Table classes

--- lib/model/doctrine/CollectingToolsTable.class.php
+++ lib/model/doctrine/CollectingToolsTable.class.php
@@ -4,25 +4,25 @@
 class CollectingToolsTable extends Doctrine_Table
 {
     
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('CollectingTools');
-    }
+  public static function getInstance()
+  {
+      return Doctrine_Core::getTable('CollectingTools');
+  }
 
-    public function fetchTools()
+  public function fetchTools()
+  {
+    $response = array();
+    $q = Doctrine_Query::create()
+        ->select('id, tool')
+        ->from('CollectingTools')
+        ->orderBy('tool_indexed');
+    $result = $q->fetchArray();
+    foreach ($result as $value)
     {
-      $response = array();
-      $q = Doctrine_Query::create()
-          ->select('id, tool')
-          ->from('CollectingTools')
-          ->orderBy('tool_indexed');
-      $result = $q->fetchArray();
-      foreach ($result as $value)
-      {
-        $response[$value['id']] = $value['tool'];
-      }
-      return $response;
+      $response[$value['id']] = $value['tool'];
     }
+    return $response;
+  }
 
   public function getAll()
   {
@@ -32,22 +32,22 @@ class CollectingToolsTable extends Doctrine_Table
     return $q->execute();
   }
 
-    public function addTool($tool)
+  public function addTool($tool)
+  {
+    // Define a new object and try to add and save new value passed
+    $newTool = new CollectingTools;
+    $newTool->setTool($tool);
+    try
     {
-      // Define a new object and try to add and save new value passed
-      $newTool = new CollectingTools;
-      $newTool->setTool($tool);
-      try
-      {
-        $newTool->save();
-      }
-      catch (Doctrine_Exception $ne)
-      {
-        // Return database error if occurs
-        $e = new DarwinPgErrorParser($ne);
-        return $e->getMessage();
-      }
-      // Return id of new record saved
-      return $newTool->getId();
+      $newTool->save();
     }
+    catch (Doctrine_Exception $ne)
+    {
+      // Return database error if occurs
+      $e = new DarwinPgErrorParser($ne);
+      return $e->getMessage();
+    }
+    // Return id of new record saved
+    return $newTool->getId();
+  }
 }
