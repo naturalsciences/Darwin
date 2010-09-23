a53b8b7f4f64c74caae750c505c38dd8351c5171
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2010-09-23 17:54:48 +0200

    Tiny corrections again ;)
    refs #298

--- apps/public/modules/search/templates/_tagCommonName.php
+++ apps/public/modules/search/templates/_tagCommonName.php
@@ -5,7 +5,6 @@
                       <li><?php echo $tag;?></li>
                     <?php endforeach;?>
                   </ul>
-                <?php else : ?>-
                 <?php endif ; ?>
               </td>
               <td class="col_chrono_common_name">
@@ -14,8 +13,7 @@
                     <?php foreach(explode(',',$common_names['chronostratigraphy'][$spec->getChronoRef()]['name']) as $tag):?>
                       <li><?php echo $tag;?></li>
                     <?php endforeach;?>
-                  </ul>                
-                <?php else : ?>-
+                  </ul>
                 <?php endif ; ?>
               </td>
               <td class="col_litho_common_name"> 
@@ -24,8 +22,7 @@
                     <?php foreach(explode(',',$common_names['lithostratigraphy'][$spec->getLithoRef()]['name']) as $tag):?>
                       <li><?php echo $tag;?></li>:q
                     <?php endforeach;?>
-                  </ul>  
-                <?php else : ?>-
+                  </ul>
                 <?php endif ; ?>
               </td> 
               <td class="col_lithologic_common_name">
@@ -34,17 +31,15 @@
                     <?php foreach(explode(',',$common_names['lithology'][$spec->getLithologyRef()]['name']) as $tag):?>
                       <li><?php echo $tag;?></li>
                     <?php endforeach;?>
-                  </ul>                  
-                <?php else : ?>-          
+                  </ul>
                 <?php endif ; ?>
-              </td>  
+              </td>
               <td class="col_mineral_common_name"> 
                 <?php if(isset($common_names['mineralogy'][$spec->getTaxonRef()])) : ?>
                   <ul class="country_tags">
                     <?php foreach(explode(',',$common_names['mineralogy'][$spec->getMineralRef()]['name']) as $tag):?>
                       <li><?php echo $tag;?></li>
                     <?php endforeach;?>
-                  </ul>                  
-                <?php else : ?>-
+                  </ul>
                 <?php endif ; ?>
               </td>
