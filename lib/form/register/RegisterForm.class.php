53c88794551e25dd8f63725e4e8220819add7a82
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2010-09-24 16:28:31 +0200

    A step ahead for register
    refs #168
    
    - A functional form, but still pb. with embedded forms like I forgot how it should work ;)

--- lib/form/register/RegisterForm.class.php
+++ lib/form/register/RegisterForm.class.php
@@ -11,39 +11,76 @@ class RegisterForm extends BaseUsersForm
 {
   public function configure()
   {
-    $this->useFields(array('is_physical','sub_type','title','family_name','given_name')) ;
-    $this->widgetSchema['is_physical'] = new sfWidgetFormInputCheckbox(array ('default' => 'true')); 
-    $this->widgetSchema['sub_type'] = new widgetFormSelectComplete(array('model' => 'Users',
-                                                                  'table_method' => 'getDistinctSubType',
-                                                                  'method' => 'getSubType',
-                                                                  'key_method' => 'getSubType',
-                                                                  'add_empty' => true,
-                                                                  'change_label' => 'Pick a sub type in the list',
-                                                                  'add_label' => 'Add another sub type',
-                                                                        )
-                                                                  );
-    $this->widgetSchema['title'] = new widgetFormSelectComplete(array('model' => 'Users',
-                                                                      'table_method' => 'getDistinctTitle',
-                                                                      'method' => 'getTitle',
-                                                                      'key_method' => 'getTitle',
-                                                                      'add_empty' => true,
-                                                                      'change_label' => 'Pick a title in the list',
-                                                                      'add_label' => 'Add another title',
-                                                                        )
-                                                                  );
-    $this->widgetSchema['title']->setAttributes(array('class'=>'small_size'));
-
+    $this->useFields(array('id','is_physical','sub_type','title','family_name','given_name')) ;
+    $choices = array($this->getI18N()->__('Moral person'), $this->getI18N()->__('Physical person'));
+    $this->widgetSchema['is_physical'] = new sfWidgetFormChoice(array ('choices' => $choices));
+    $this->setDefault('is_physical', 1);
+    $this->widgetSchema['is_physical']->setAttributes(array('class'=>'required_field'));
+    $this->widgetSchema['sub_type'] = new sfWidgetFormDoctrineChoice(array('model' => 'Users',
+                                                                           'table_method' => 'getDistinctSubType',
+                                                                           'method' => 'getSubType',
+                                                                           'key_method' => 'getSubType',
+                                                                           'add_empty'=>true,
+                                                                          )
+                                                                    );
+    $this->widgetSchema['title'] = new sfWidgetFormDoctrineChoice(array('model' => 'Users',
+                                                                        'table_method' => 'getDistinctTitle',
+                                                                        'method' => 'getTitle',
+                                                                        'key_method' => 'getTitle',
+                                                                        'add_empty'=>true,
+                                                                       )
+                                                                 );
     $this->widgetSchema['given_name'] = new sfWidgetFormInput();
-    $this->widgetSchema['given_name']->setAttributes(array('class'=>'medium_size'));    
+    $this->widgetSchema['given_name']->setAttributes(array('class'=>'medium_size'));
     $this->widgetSchema['family_name'] = new sfWidgetFormInput();
-    $this->widgetSchema['family_name']->setAttributes(array('class'=>'medium_size'));
+    $this->widgetSchema['family_name']->setAttributes(array('class'=>'medium_size required_field'));
+
+    $this->widgetSchema['terms_of_use'] = new sfWidgetFormInputCheckbox();
+    $this->widgetSchema['terms_of_use']->setAttributes(array('class'=>'required_field'));
+    $this->setDefault('terms_of_use',false);
+
+    /* Validators */
+    
+    // Write a post validator for is_physical here
+    $this->validatorSchema['terms_of_use'] = new sfValidatorBoolean(array('required'=>true),
+                                                                    array('required'=> 'You cannot register without accepting DaRWIN 2  term of use',
+                                                                          'invalid' => 'The value provided for term of use is invalid'
+                                                                         )
+                                                                   );
+    $this->validatorSchema['is_physical'] = new sfValidatorBoolean(array('required'=>true),
+                                                                   array('required'=> 'Status is required',
+                                                                         'invalid' => 'The value provided for status is invalid'
+                                                                        )
+                                                                  );
+    $this->validatorSchema['family_name'] = new sfValidatorString(array('trim'=>true, 
+                                                                        'required'=>true
+                                                                       ),
+                                                                   array('required'=> 'Name is required',
+                                                                         'invalid' => 'The value provided for name is invalid'
+                                                                        )
+                                                                 );
 
     /* Labels */
-    $this->widgetSchema->setLabels(array('family_name'=>'Name','given_name'=>'First name', 'is_physical'=>'Physical person ?', 'sub_type'=>'Type'));
+    $this->widgetSchema->setLabels(array('family_name'=>'Name',
+                                         'given_name'=>'First name', 
+                                         'is_physical'=>'Status', 
+                                         'sub_type'=>'Type'
+                                        )
+                                  );
 
     /* Login infos as embedded form */
-    $regLoginSubForm = new RegisterLoginInfosForm();
-    $this->embedForm('RegisterLoginInfosForm',$regLoginSubForm);
+
+      $login = new UsersLoginInfos();
+      $login->User = $this->getObject();
+      $form = new RegisterLoginInfosForm($login);
+      $this->embedForm('RegisterLoginInfosForm',$form);
+//       $this->embeddedForms['RegisterLoginInfos']->embedForm($form);
+//       //Re-embedding the container
+//       $this->embedForm('RegisterLoginInfos', $this->embeddedForms['RegisterLoginInfos']);
+
+//     $regLoginSubForm = new RegisterLoginInfosForm();
+//      $this->embeddedForms['RegisterLoginInfosForm']->embedForm(1, $form);
+//      $this->embedForm('RegisterLoginInfosForm', $this->embeddedForms['RegisterLoginInfosForm']);
 
     /* Comm means as embedded form */
     $regCommSubForm = new RegisterCommForm();
--- lib/form/register/RegisterLoginInfosForm.class.php
+++ lib/form/register/RegisterLoginInfosForm.class.php
@@ -9,10 +9,36 @@
  */
 class RegisterLoginInfosForm extends UsersLoginInfosForm
 {
+
   public function configure()
   {
     parent::configure();
-    unset($this['login_type']);
+    unset($this['login_type'], $this['id']);
     $this->widgetSchema->setLabels(array('new_password'=>'Password', 'user_name'=>'Login'));
+    $this->widgetSchema['user_name']->setAttributes(array('class'=>'medium_small_size required_field'));
+    $this->widgetSchema['new_password']->setAttributes(array('class'=>'medium_small_size required_field'));
+    $this->widgetSchema['confirm_password']->setAttributes(array('class'=>'medium_small_size required_field'));
+    $this->validatorSchema['user_name'] = new sfValidatorString(array('required' => true, 'min_length' => 4),
+                                                                array('min_length' => '"%value%" must be at least %min_length% characters.',
+                                                                      'required' => 'Login is required'
+                                                                     )
+                                                               ) ;
+    $this->validatorSchema['new_password'] = new sfValidatorString(array('trim'=>true, 'required'=>true),
+                                                                   array('required'=>'You need to provide a password')
+                                                                  );
+    $this->validatorSchema['confirm_password'] = new sfValidatorString(array('trim'=>true, 'required'=>true),
+                                                                       array('required'=>'You need to confirm your password')
+                                                                      );
+    $this->validatorSchema['user_ref'] = new sfValidatorInteger(array('required'=>false));
   }
+
+  public function checkPassword($validator, $values)
+  {
+    if($values['new_password'] != $values['confirm_password'])
+    {
+      $error = new sfValidatorError($validator, 'Password does not match' ) ;
+      throw new sfvalidatorErrorSchema($validator, array('new_password' => $error));
+    }
+    return $values;
+  } 
 }
