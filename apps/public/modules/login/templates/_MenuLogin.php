6fc0b23dbc031d2c82679017076eea7002d4d1b0
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2010-09-30 13:17:47 +0200

    A few stylin' modifications (but in progress ;))

--- apps/public/modules/login/templates/_MenuLogin.php
+++ apps/public/modules/login/templates/_MenuLogin.php
@@ -3,7 +3,6 @@
     <td><?php echo $form['username'] ; ?></td>
     <td><?php echo $form['password'] ; ?></td>
     <td><input type="submit" name="login" value=">>" id="login_bt"></td>
-    <td><a href="<?php echo url_for('register/login') ;?>" id="register"><?php echo __('Register') ; ?></a></td>
+    <td><a href="<?php echo url_for('register/index') ;?>" id="register"><?php echo __('Register') ; ?></a></td>
   </tr>
 </table>
-    
