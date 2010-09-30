9386d016337b34cda3528249f32dea14a5a4457d
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2010-09-30 17:11:07 +0200

    A few more stylin'

--- apps/frontend/modules/specimensearch/templates/_result_content_individual.php
+++ apps/frontend/modules/specimensearch/templates/_result_content_individual.php
@@ -3,15 +3,15 @@
     <?php echo $specimen->getIndividualTypeGroup();?>
   <?php endif ; ?>
 </td>
-<td class="col_sex"><?php echo $specimen->getIndividualSex();?></td> 
-<td class="col_state"><?php echo $specimen->getIndividualState();?></td> 
-<td class="col_stage"><?php echo $specimen->getIndividualStage();?></td> 
-<td class="col_social_status"><?php echo $specimen->getIndividualSocialStatus();?></td> 
-<td class="col_rock_form"><?php echo $specimen->getIndividualRockForm();?></td> 
+<td class="col_sex"><?php echo ucfirst(($specimen->getIndividualSex()=='undefined')?'':$specimen->getIndividualSex());?></td> 
+<td class="col_state"><?php echo ucfirst(($specimen->getIndividualState()=='not applicable')?'':$specimen->getIndividualState());?></td> 
+<td class="col_stage"><?php echo ucfirst(($specimen->getIndividualStage()=='undefined')?'':$specimen->getIndividualStage());?></td> 
+<td class="col_social_status"><?php echo ucfirst(($specimen->getIndividualSocialStatus()=='not applicable')?'':$specimen->getIndividualSocialStatus());?></td> 
+<td class="col_rock_form"><?php echo ucfirst(($specimen->getIndividualRockForm()=='not applicable')?'':$specimen->getIndividualRockForm());?></td> 
 <td class="col_individual_count">
   <?php if($specimen->getIndividualCountMin() != $specimen->getIndividualCountMax()):?>
     <?php echo $specimen->getIndividualCountMin() . ' - '.$specimen->getIndividualCountMax();?>
   <?php else:?>
     <?php echo $specimen->getIndividualCountMin();?>
   <?php endif;?>
-</td> 
+</td>
