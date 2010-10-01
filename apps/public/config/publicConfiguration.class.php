0307907c0c85cc32f64655c6f930f9e2761527ef
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2010-10-01 16:15:32 +0200

    Cross application routing and login from public functional
    fix #359
    refs #168

--- apps/public/config/publicConfiguration.class.php
+++ apps/public/config/publicConfiguration.class.php
@@ -2,7 +2,24 @@
 
 class publicConfiguration extends sfApplicationConfiguration
 {
-  public function configure()
+  protected $frontendRouting = null;
+
+  public function generateFrontendUrl($name, $parameters = array())
   {
+    return 'http://'.$_SERVER['SERVER_NAME'].$this->getFrontendRouting()->generate($name, $parameters);
+  }
+
+  public function getFrontendRouting()
+  {
+    if (!$this->frontendRouting)
+    {
+      $this->frontendRouting = new sfPatternRouting(new sfEventDispatcher());
+
+      $config = new sfRoutingConfigHandler();
+      $routes = $config->evaluate(array(sfConfig::get('sf_apps_dir').'/frontend/config/routing.yml'));
+      $this->frontendRouting->setRoutes($routes);
+    }
+
+    return $this->frontendRouting;
   }
 }
