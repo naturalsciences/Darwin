c4fd64843fe2cdff84c527686559483cc685c96f
Yann CHAMBERT <ychambert@gmail.com>
2010-10-06 10:36:35 +0200

    added the same action for public search (#ref 374)

--- apps/public/modules/search/templates/_andSearch.php
+++ apps/public/modules/search/templates/_andSearch.php
@@ -1,7 +1,7 @@
 <tr class="tag_line">
   <td>
     <?php echo $form['tag'];?>
-    <div class="purposed_tags">
+    <div class="purposed_tags" id="purposed_tags_<?php echo $row_line;?>">
     </div>
   </td>
   <td>
@@ -9,6 +9,7 @@
   </td>
 </tr>
 <script  type="text/javascript">
+  $('input.tag_line_<?php echo $row_line ; ?>').bind('keydown click',purposeTags);
   $('#clear_tag_<?php echo $row_line;?>').click(function(){
     if($(this).closest('tbody').find('tr.tag_line').length == 1)
     {
@@ -17,4 +18,35 @@
     else
       $(this).closest('tr').remove();
   });
+  function purposeTags(event)
+  {
+    if (event.type == 'keydown')
+    {
+      var code = (event.keyCode ? event.keyCode : event.which);
+      if (code != 59 /* ;*/ && code != $.ui.keyCode.SPACE ) return;
+    }        
+    parent_el = $(this).closest('tr');
+
+    if($(this).val() == '') return;
+    $(this).find('#purposed_tags_<?php echo $row_line ; ?>').html('<img src="/images/loader.gif" />');
+    $.ajax({
+      type: "GET",
+      url: "<?php echo url_for('search/purposeTag');?>" + '/value/'+ $(this).val(),
+      success: function(html)
+      {
+        parent_el.find('#purposed_tags_<?php echo $row_line ; ?>').html(html);
+        parent_el.find('#purposed_tags_<?php echo $row_line ; ?>').show();
+      }
+    });
+  }
+
+  $('#purposed_tags_<?php echo $row_line ; ?> li').live('click',function()
+  {
+    input_el = $(this).closest('tr').find('input.tag_line_<?php echo $row_line ; ?>');
+    if(input_el.val().match("\;\s*$"))
+      input_el.val( input_el.val() + $(this).text() );
+    else
+      input_el.val( input_el.val() + " ; " +$(this).text() );
+    input_el.trigger('click');
+  });    
 </script>
