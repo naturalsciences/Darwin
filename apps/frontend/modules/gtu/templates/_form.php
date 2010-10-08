cf0ec47ea48e2a85c90105acc5efae467040a7ad
Brice Maron <bmaron@naturalsciences.be>
2010-10-08 16:55:18 +0200

    Put map related functions in external file correct bugs fix #379

--- apps/frontend/modules/gtu/templates/_form.php
+++ apps/frontend/modules/gtu/templates/_form.php
@@ -120,187 +120,44 @@ foreach($form['newVal'] as $group)
         <td><strong><?php echo _('m');?></strong> <?php echo image_tag('remove.png', 'alt=Delete class=clear_prop'); ?></td>
       </tr>
       <tr>
-        <td colspan="3"><div style="width:100%; height:300px;" id="map"></div></td>
+        <td colspan="3"><div style="width:100%; height:400px;" id="map"></div></td>
         <td>
 
 <script src="http://www.openlayers.org/api/OpenLayers.js"></script>
 <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<?php echo javascript_include_tag('map.js'); ?>
 
 <script type="text/javascript">
+$(document).ready(function () {
 
-  var epsg4326 = new OpenLayers.Projection("EPSG:4326");
-  var markers;
-  var marker;
-  var center;
-  var zoom = 2;
-  var pointFeature;
-  var point;
-  options = {
-//     restrictedExtent: extent,
-    controls: [
-      new OpenLayers.Control.Navigation(), 
-      new OpenLayers.Control.PanZoomBar(),
-      new OpenLayers.Control.LayerSwitcher()
-    ],
-    numZoomLevels: 20,
-    displayProjection: new OpenLayers.Projection("EPSG:4326"),
-    maxExtent: new OpenLayers.Bounds(-180,-90, 180, 90),
-    maxResolution: 0.3515625,
-    units: "m"
-  };
-  var style_blue = OpenLayers.Util.extend({}, OpenLayers.Feature.Vector.style['default']);
-  style_blue.strokeColor = "blue"; 
-  style_blue.fillColor = "blue"; 
-// OpenLayers.Layer.XYZ.
+  initMap();
 
-  var map = new OpenLayers.Map("map", options);
-
-  var mapnik = new OpenLayers.Layer.OSM();
-  mapnik.addOptions({wrapDateLine:true});
-  map.addLayer(mapnik);
-
-  // the SATELLITE layer has all 22 zoom level, so we add it first to
-    // become the internal base layer that determines the zoom levels of the
-    // map.
-    var gsat = new OpenLayers.Layer.Google(
-        "Google Satellite",
-        {sphericalMercator: true,type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
-    );
-    var gphy = new OpenLayers.Layer.Google(
-        "Google Physical",
-        {sphericalMercator: true,type: google.maps.MapTypeId.TERRAIN, visibility: false}
-    );
-    var gmap = new OpenLayers.Layer.Google(
-        "Google Streets", // the default
-        {sphericalMercator: true,numZoomLevels: 20, visibility: false}
-    );
-    var ghyb = new OpenLayers.Layer.Google(
-        "Google Hybrid",
-        {sphericalMercator: true,type: google.maps.MapTypeId.HYBRID, numZoomLevels: 22, visibility: false}
-    );
-    var vectorLayer = new OpenLayers.Layer.Vector("Simple Geometry", { displayInLayerSwitcher: false, projection: new OpenLayers.Projection("EPSG:4326")});
-    map.addLayers([gsat, gphy, gmap, ghyb,vectorLayer]);
-
-
-  markers = new OpenLayers.Layer.Markers("Markers", {
-    displayInLayerSwitcher: false,
-    units: "m",
-    projection: "EPSG:900913"
-   });
-  map.addLayer(markers);
-
-  map.zoomToMaxExtent();
-  
-
-  map.events.register("click", map, setPoint);
-  map.events.register("zoomend", map, setZoom);
   <?php if($form->getObject()->getLongitude() != ''):?>
     centre = new OpenLayers.LonLat(<?php echo $form->getObject()->getLongitude();?>, <?php echo $form->getObject()->getLatitude();?>);
     zoom = 13;
     setMapCenter(centre, zoom);
-    drawLatLong();
   <?php else:?>
-        //var bbox = new OpenLayers.Bounds(2.54694366455078, 49.4936027526855, 6.40386152267456, 51.5054512023926);
     setMapCenter(new OpenLayers.LonLat(0,0), 2);
-// map.zoomToMaxExtent();
-
   <?php endif;?>
-$('#gtu_lat_long_accuracy').change(drawAccuracy);
-$('#gtu_longitude').change(drawLatLong);
-$('#gtu_latitude').change(drawLatLong);
-
-
-function setZoom(e)
-{
-  if(pointFeature)
-  {
-    drawAccuracy()
-  }
-}
-
-function getAccuracySize()
-{
-  zoom = map.getZoom();
-  resolution = map.getResolutionForZoom(zoom);
-  return $('#gtu_lat_long_accuracy').val() / resolution;
-}
-
-function drawAccuracy()
-{
-  vectorLayer.removeAllFeatures();
-  point = new OpenLayers.Geometry.Point(new_pos.lon, new_pos.lat);
-  style_blue.pointRadius = getAccuracySize();
-  pointFeature = new OpenLayers.Feature.Vector(point, null, style_blue);
-  vectorLayer.addFeatures([pointFeature]);
-  vectorLayer.redraw();
-}
-
-function drawLatLong()
-{
-  if (marker) {
-    removeMarkerFromMap(marker);
-  }
-  lonlat = new OpenLayers.LonLat($('#gtu_longitude').val(), $('#gtu_latitude').val())
-  marker = addMarkerToMap(lonlat, null);
-}
-
-function setPoint( e )
-{
-  var lonlat = getEventPosition(e).wrapDateLine();
-  $('#gtu_latitude').val(lonlat.lat);
-  $('#gtu_longitude').val(lonlat.lon);
-
-////GOOGLE ELE
-var latlng = new google.maps.LatLng(lonlat.lat,lonlat.lon);
-elevator = new google.maps.ElevationService();
-var positionalRequest = {'locations': [latlng] };
-elevator.getElevationForLocations(positionalRequest, function(results, status) 
-{
-  if (status == google.maps.ElevationStatus.OK && results[0]) 
-  {
-    $('#gtu_elevation').val(results[0].elevation.toFixed(3));
-  }
-});
-//// GOOGLE ELE
 
   drawLatLong();
-}
-
-function addMarkerToMap(position, icon)
-{
-  new_pos = position.clone().transform(epsg4326, map.getProjectionObject());
-  var marker = new OpenLayers.Marker(position.clone().transform(epsg4326, map.getProjectionObject()), icon);
-  markers.addMarker(marker);
-
-            // create a point feature
-
   drawAccuracy();
-  return marker;
-}
 
-function removeMarkerFromMap(marker)
-{
-   markers.removeMarker(marker);
-}
+  map.events.register("click", map, setPoint);
+  map.events.register("zoomend", map, setZoom);
 
-function getEventPosition(event)
-{
-  return map.getLonLatFromViewPortPx(event.xy).clone().transform(map.getProjectionObject(), epsg4326);
-}
+  $('#gtu_lat_long_accuracy').change(drawAccuracy);
+  $('#gtu_longitude').change(drawLatLong);
+  $('#gtu_latitude').change(drawLatLong);
 
-function setMapCenter(center, zoom)
-{
-  zoom = parseInt(zoom);
-  var numzoom = map.getNumZoomLevels();
-  if (zoom >= numzoom) zoom = numzoom - 1;
-  map.setCenter(center.clone().transform(epsg4326, map.getProjectionObject()), zoom);
-}
- $('#location .clear_prop').click(function()
+
+  $('#location .clear_prop').click(function()
   {
     $(this).closest('tr').find('input').val('');
     drawLatLong();
-
   });
+
+});
 </script>
 </td>
       </tr>
