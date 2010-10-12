569715b59b611c60f72bc333ca58e56b99ef6cc2
Brice Maron <bmaron@naturalsciences.be>
2010-10-12 15:29:28 +0200

    Correct zindex of menu and adjust widget style fixes #382

--- apps/frontend/modules/specimensearchwidget/templates/_latlong.php
+++ apps/frontend/modules/specimensearchwidget/templates/_latlong.php
@@ -1,32 +1,29 @@
-
-<fieldset id="lat_long_set">
-        <legend><?php echo __('Show Result as map');?> <input type="checkbox" id="show_as_map"></legend>
-          <table>
-            <tr>
-              <td>
-              </td>
-              <th>
-                <?php echo $form['lat_from']->renderLabel();?>
-              </th>
-              <th>
-                <?php echo $form['lon_from']->renderLabel();?>
-              </th>
-            </tr>
-            <tr>
-              <th class="right_aligned"><?php echo __('Between');?></th>
-              <td><?php echo $form['lat_from'];?></td>
-              <td><?php echo $form['lon_from'];?><?php echo image_tag('remove.png', 'alt=Delete class=clear_prop'); ?></td>
-            </tr>
-            <tr>
-              <th class="right_aligned"><?php echo __('And');?></th>
-              <td><?php echo $form['lat_to'];?></td>
-              <td><?php echo $form['lon_to'];?><?php echo image_tag('remove.png', 'alt=Delete class=clear_prop'); ?></td>
-            </tr>
-          </table>
-
-          <div style="width: 600px; height:400px;display:none;" id="smap"></div>
-
-    </fieldset>
+ <div id="lat_long_set"><p><strong><?php echo __('Show Result as map');?></strong><input type="checkbox" id="show_as_map"></p>
+  <br /><br />
+  <table>
+    <tr>
+      <td>
+      </td>
+      <th>
+        <?php echo $form['lat_from']->renderLabel();?>
+      </th>
+      <th>
+        <?php echo $form['lon_from']->renderLabel();?>
+      </th>
+    </tr>
+    <tr>
+      <th class="right_aligned"><?php echo __('Between');?></th>
+      <td><?php echo $form['lat_from'];?></td>
+      <td><?php echo $form['lon_from'];?><?php echo image_tag('remove.png', 'alt=Delete class=clear_prop'); ?></td>
+    </tr>
+    <tr>
+      <th class="right_aligned"><?php echo __('And');?></th>
+      <td><?php echo $form['lat_to'];?></td>
+      <td><?php echo $form['lon_to'];?><?php echo image_tag('remove.png', 'alt=Delete class=clear_prop'); ?></td>
+    </tr>
+  </table>
+</div>
+  <div style="width: 600px; height:400px;display:none;" id="smap"></div>
 
 
 <script  type="text/javascript">
