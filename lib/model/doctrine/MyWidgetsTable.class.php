ffe4106b699070ed29e255d558a137aa2105a868
Yann CHAMBERT <ychambert@gmail.com>
2010-11-03 13:19:20 +0100

    added "all_public" when we list all widget available for a user (fix #481)

--- lib/model/doctrine/MyWidgetsTable.class.php
+++ lib/model/doctrine/MyWidgetsTable.class.php
@@ -161,6 +161,7 @@ class MyWidgetsTable extends DarwinTable
             ->from('MyWidgets p')
   		  ->where('p.user_ref = ?', $this->user_ref) ;
      if ($level < Users::MANAGER) $q->andWhere('p.is_available = true') ;
+     if ($level == Users::REGISTERED_USER) $q->andWhere('p.all_public = true');
      $q->orderBy('category,group_name');
    	return $q->execute() ;	
   }
