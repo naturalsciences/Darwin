38bdcca7440aed2ed4fdf4f3504f06fa87f67c4d
Brice Maron <bmaron@naturalsciences.be>
2011-01-21 12:55:11 +0100

    Oups corrected

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -8,7 +8,14 @@ class ProjectConfiguration extends sfProjectConfiguration
   public function setup()
   {
     // for compatibility / remove and enable only the plugins you want
-    $manager->setAttribute(Doctrine::ATTR_QUERY_CACHE, new Doctrine_Cache_Apc());
     $this->enableAllPluginsExcept(array('sfPropelPlugin'));
+  }
+
+ /**
+  * Configure the Doctrine engine
+  **/
+  public function configureDoctrine(Doctrine_Manager $manager)
+  {
+   $manager->setAttribute(Doctrine::ATTR_QUERY_CACHE, new Doctrine_Cache_Apc());
   }
 }
