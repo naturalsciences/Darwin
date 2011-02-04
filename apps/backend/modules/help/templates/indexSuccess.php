ec93ee52499d20f669443c63ba383e62bde5800e
Brice Maron <bmaron@naturalsciences.be>
2011-02-04 17:06:55 +0100

    Quick & dirty add user guide

--- apps/backend/modules/help/templates/indexSuccess.php
+++ apps/backend/modules/help/templates/indexSuccess.php
@@ -1,5 +1,8 @@
 <?php slot('title',__("Dawin Help"));?>
 <div class="page">
   <h1><?php echo __('Darwin Guide :');?></h1>
-  <p></p>
+  
+    <p><a href="/help/Darwin2_NL.pdf">Gebruikershandleiding</a></p><br />
+    <p><a href="/help/Darwin2_FR.pdf">Manuel d'utilisation</a></p><br />
+    <p><a href="/help/Darwin2_EN.pdf">User guide</a></p><br />
 </div>
