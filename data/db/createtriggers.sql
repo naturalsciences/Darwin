75a4cf2d1ca91fb2a2f9724f30f3f7211b5d562b
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2011-02-11 13:49:28 +0100

    Corrected a mistakenly removed trigger

--- data/db/createtriggers.sql
+++ data/db/createtriggers.sql
@@ -647,8 +647,8 @@ CREATE TRIGGER trg_update_gtu_darwin_flat AFTER UPDATE
         ON gtu FOR EACH ROW
         EXECUTE PROCEDURE fct_update_darwin_flat();
 
-CREATE TRIGGER trg_update_users_darwin_flat AFTER UPDATE
-        ON users FOR EACH ROW
+CREATE TRIGGER trg_update_tag_groups_darwin_flat AFTER INSERT OR UPDATE OR DELETE
+        ON tag_groups FOR EACH ROW
         EXECUTE PROCEDURE fct_update_darwin_flat();
 
 CREATE TRIGGER trg_update_igs_darwin_flat AFTER UPDATE
