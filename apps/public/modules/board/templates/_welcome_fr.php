590da7348ab21e74da05c7d41b217ed383946f49
Brice Maron <bmaron@naturalsciences.be>
2011-02-16 17:17:42 +0100

    Correct intro text fr translation.

--- apps/public/modules/board/templates/_welcome_fr.php
+++ apps/public/modules/board/templates/_welcome_fr.php
@@ -1,27 +1,40 @@
-<br 
+<br />
 <h1>Bienvenue dans notre base de données des collections de l’ Institut</h1>
 
 <p>
-L'Institut Royal des Sciences naturelles abrite une précieuse collection de matériels et de documents zoologiques, 
+L'Institut Royal des Sciences naturelles abrite une collection précieuse de matériel et documents zoologiques, 
 anthropologiques, paléontologiques, minéralogiques et géologiques. Les fameux iguanodons de Bernissart, 
-les ambassadeurs de l'Institut Scientifique Belge à Bruxelles, font partie d'une collection d'histoire naturelle,
-qui est actuellement estimée à plus de 37 millions d'exemplaires.
+ambassadeurs de l'Institut scientifique belge à Bruxelles, font partie d'une collection d'histoire naturelle,
+qui est actuellement estimée à plus de 37 millions de spécimens.
 </p>
 <br />
 <p>
-Les racines de la collection actuelle remontent loin dans l'histoire. La collection émergée de la collection d'histoire naturelle de Charles de Lorraine, gouverneur des Pays-Bas (1712-1780), faisait partie du matériel pédagogique de l'École centrale de la ville de Bruxelles. Après l'indépendance de la Belgique, la collection était donnée par la ville au gouvernement Belge et est devenue une partie autonome du «Musée Royal d'Histoire Naturelle" en 1846, plus connue depuis 1948 comme l'Institut Royal des Sciences naturelles. Grâce aux chercheurs et leurs personnels, tant en Belgique qu'à l'étranger, les dons et les achats des collections s’'élargissent à nos jours.
+L'origine de la collection actuelle remonte loin dans l'histoire.
+La collection est issue de la collection d'histoire naturelle de Charles de Lorraine, gouverneur des Pays-Bas (1712-1780),
+qui faisait partie du matériel pédagogique de l'École centrale de la Ville de Bruxelles.
+Après l'indépendance de la Belgique, la collection fut donnée par la ville au gouvernement belge et est devenue une partie autonome du «Musée royal d'Histoire naturelle" en 1846,
+plus connue depuis 1948 comme l'Institut royal des Sciences naturelles.
+Grâce aux chercheurs et à leur personnel, tant belges qu’étrangers, les dons et les achats des collections n’ont cessé de s’accroître jusqu'à nos jours.
 </p>
 <br />
 <p>
-Le site Darwin nous montre une facette importante en termes de taille et la diversité des possessions. Aujourd'hui, notre base de données contient des informations sur plus de 350.000 exemplaires stockées dans les dépôts de l'Institut. Ce nombre augmente chaque jour grâce aux efforts des conservateurs et de leurs adjoints qui sont responsables du maintien des échantillons stockés et les informations archivées. Notre base de données en ligne contient des informations sur les collections de vertébrés, les invertébrés et les insectes. L'application sera bientôt élargie aux données paléontologiques.
+Le site Darwin nous montre une facette importante des collections en terme de taille et de diversité.
+Aujourd'hui, notre base de données contient des informations sur plus de 350.000 spécimens stockés dans les conservatoires de l'Institut.
+Ce nombre augmente chaque jour grâce aux efforts des conservateurs et de leurs adjoints, qui sont responsables de la conservation des échantillons et des informations archivées.
+Notre base de données en ligne contient des informations sur les collections de vertébrés, invertébrés et insectes.
+L'application sera bientôt élargie aux données paléontologiques.
 </p>
 <br />
 <p>
-L’information sur la collection anthropologique est conservée sur un système parallèle: MARS (Multimedia Archaeological Research System). Cette information peut être trouvé à l'adresse suivante: http://www.sciencesnaturelles.be/mars. . Le service Géologique de la Belgique ainsi que le Département de la Gestion des écosystèmes marins fournissent leurs données via d'autres systèmes. Plus d'informations sur ces départements peuvent être consultés sur leurs pages Web: <a href="http://www.sciencesnaturelles.be/institute/structure/geology/gsb_website" target="_pop">www.sciencesnaturelles.be/institute/structure/geology/gsb_website</a> et <a href="http://www.mumm.ac.be" target="_pop">www.mumm.ac.be</a>
+Les données de la collection anthropologique sont conservées dans un système parallèle: MARS (Multimedia Archaeological Research System)qui peut être accédée via le lien suivant: <a href="http://www.sciencesnaturelles.be/mars">http://www.sciencesnaturelles.be/mars</a>.
+Le service géologique de Belgique et le Département de la Gestion des écosystèmes marins fournissent leurs données via d'autres systèmes.
+Plus d'information sur ces départements peu être obtenue en consultant leurs pages Web: <a href="http://www.sciencesnaturelles.be/institute/structure/geology/gsb_website" target="_pop">www.sciencesnaturelles.be/institute/structure/geology/gsb_website</a> et <a href="http://www.mumm.ac.be" target="_pop">www.mumm.ac.be</a>
 </p>
 <br />
 <p>
-La pierre angulaire de la base de données DARWIN est l'échantillon  et de l'information sur son origine et statut. Bien que les données sur le statut des échantillons suivent la réglementation en vigueur du <a href="http://iczn.org/" target="_pop">Code international de nomenclature zoologique </a> , vous y trouverez tout autre statut non reconnu par le ICZN (par exemple topotype) utilisé comme complément d'information sur le ou les échantillon (s) en question.
+La pierre angulaire de la base de données DARWIN est le spécimen et les informations relatives à son origine et son statut.
+Bien que les données sur le statut des spécimens suivent la réglementation en vigueur du <a href="http://iczn.org/" target="_pop">Code international de nomenclature zoologique (CINZ) </a>,
+vous y trouverez tout autre statut non reconnu par le CINZ (par exemple topotype) fournis en tant que complément d'information sur le ou les échantillon (s) en question.
 </p>
 <br />
-<p>Nous vous souhaitons une agréable visite virtuelle à travers nos collections!</p>
+<p>Nous vous souhaitons une agréable visite virtuelle parmi nos collections!</p>
