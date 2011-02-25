db23784d3ba4817c04df37313ec66c8950d0ceb2
Brice Maron <bmaron@naturalsciences.be>
2011-02-25 13:47:43 +0100

    Add another feed tool source. may be changed later

--- tools/fetch_feed.sh
+++ tools/fetch_feed.sh
@@ -3,4 +3,5 @@ dir=$(dirname $(which $0));
 #or http://twitter.com/statuses/user_timeline/255481440.rss
 # From pipes to remove Username
 #http://pipes.yahoo.com/pipes/pipe.info?_id=zncl_xDx2xGVxuG5p2IyXQ
-wget "http://pipes.yahoo.com/pipes/pipe.run?_id=zncl_xDx2xGVxuG5p2IyXQ&_render=rss&user=nat_sciences" -O "$dir/../data/feed/feed.xml"
+#wget "http://pipes.yahoo.com/pipes/pipe.run?_id=zncl_xDx2xGVxuG5p2IyXQ&_render=rss&user=nat_sciences" -O "$dir/../data/feed/feed.xml"
+wget --no-cache "http://pipes.yahoo.com/pipes/pipe.run?_id=237e91d1ba5115b99da40bc4d5faba7c&_render=rss&username=nat_sciences" -O "$dir/../data/feed/feed.xml"
