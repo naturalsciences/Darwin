ef05eb80bc0b2e4abc4648afb642953eefc538fc
Brice Maron <bmaron@naturalsciences.be>
2011-03-01 13:11:58 +0100

    Add lang as title to the vernaculare name tags in public

--- apps/public/modules/search/templates/_tagCommonName.php
+++ apps/public/modules/search/templates/_tagCommonName.php
@@ -3,7 +3,7 @@
   <?php if(isset($common_names['taxonomy'][$spec->getTaxonRef()])) : ?>
     <ul class="common_name_tags">
       <?php foreach($common_names['taxonomy'][$spec->getTaxonRef()]['community'] as $lang => $tag) : ?>
-        <li style="border-left:6px solid #<?php echo word2color($lang);?>;"><?php echo $tag;?></li>
+        <li style="border-left:6px solid #<?php echo word2color($lang);?>;" title="<?php echo $lang;?>"><?php echo $tag;?></li>
       <?php endforeach;?>
     </ul>
   <?php endif ; ?>
@@ -12,7 +12,7 @@
   <?php if(isset($common_names['chronostratigraphy'][$spec->getChronoRef()])) : ?>
     <ul class="common_name_tags">
       <?php foreach($common_names['chronostratigraphy'][$spec->getChronoRef()]['community'] as $lang => $tag) : ?>
-        <li style="border-left:6px solid #<?php echo word2color($lang);?>;"><?php echo $tag;?></li>
+        <li style="border-left:6px solid #<?php echo word2color($lang);?>;" title="<?php echo $lang;?>"><?php echo $tag;?></li>
       <?php endforeach;?>
     </ul>
   <?php endif ; ?>
@@ -21,7 +21,7 @@
   <?php if(isset($common_names['lithostratigraphy'][$spec->getLithoRef()])) : ?>
     <ul class="common_name_tags">
       <?php foreach($common_names['lithostratigraphy'][$spec->getChronoRef()]['community'] as $lang => $tag) : ?>
-        <li style="border-left:6px solid #<?php echo word2color($lang);?>;"><?php echo $tag;?></li>
+        <li style="border-left:6px solid #<?php echo word2color($lang);?>;" title="<?php echo $lang;?>"><?php echo $tag;?></li>
       <?php endforeach;?>
     </ul>
   <?php endif ; ?>
@@ -30,7 +30,7 @@
   <?php if(isset($common_names['lithology'][$spec->getLithologyRef()])) : ?>
     <ul class="common_name_tags">
       <?php foreach($common_names['lithology'][$spec->getChronoRef()]['community'] as $lang => $tag) : ?>
-        <li style="border-left:6px solid #<?php echo word2color($lang);?>;"><?php echo $tag;?></li>
+        <li style="border-left:6px solid #<?php echo word2color($lang);?>;" title="<?php echo $lang;?>"><?php echo $tag;?></li>
       <?php endforeach;?>
     </ul>
   <?php endif ; ?>
@@ -39,7 +39,7 @@
   <?php if(isset($common_names['mineralogy'][$spec->getMineralRef()])) : ?>
     <ul class="common_name_tags">
       <?php foreach($common_names['mineralogy'][$spec->getChronoRef()]['community'] as $lang => $tag) : ?>
-        <li style="border-left:6px solid #<?php echo word2color($lang);?>;"><?php echo $tag;?></li>
+        <li style="border-left:6px solid #<?php echo word2color($lang);?>;" title="<?php echo $lang;?>"><?php echo $tag;?></li>
       <?php endforeach;?>
     </ul>
   <?php endif ; ?>
