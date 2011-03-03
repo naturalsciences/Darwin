e7e7cdc5616597cf6ee1bde4e0569ba178957bdd
Brice Maron <bmaron@naturalsciences.be>
2011-03-03 15:42:18 +0100

    Correct shows of link in specimen search when not encoder

--- apps/backend/modules/catalogue/templates/searchSuccess.php
+++ apps/backend/modules/catalogue/templates/searchSuccess.php
@@ -106,8 +106,10 @@
                 <?php endif ; ?>
                 <?php echo link_to(image_tag('blue_eyel.png', array("title" => __("View"))),$searchForm->getValue('table').'/view?id='.$item->getId());?>
               <?php else:?>
-                <?php echo link_to(image_tag('edit.png', array("title" => __("Edit"))),$searchForm->getValue('table').'/edit?id='.$item->getId(),array('target'=>"_blank"));?>
-                <?php echo link_to(image_tag('duplicate.png', array("title" => __("Duplicate"))),$searchForm->getValue('table').'/new?duplicate_id='.$item->getId(),array('target'=>"_blank"));?>
+                <?php if ($sf_user->isAtLeast(Users::ENCODER)) : ?>
+                  <?php echo link_to(image_tag('edit.png', array("title" => __("Edit"))),$searchForm->getValue('table').'/edit?id='.$item->getId(),array('target'=>"_blank"));?>
+                  <?php echo link_to(image_tag('duplicate.png', array("title" => __("Duplicate"))),$searchForm->getValue('table').'/new?duplicate_id='.$item->getId(),array('target'=>"_blank"));?>
+                <?php endif ; ?>
                 <div class="result_choose"><?php echo __('Choose');?></div>
               <?php endif;?>
             </td>
