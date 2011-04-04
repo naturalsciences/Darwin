20227d9b509ebfba0c943a6cb45f3bb0c9031fba
Brice Maron <bmaron@naturalsciences.be>
2011-04-04 13:00:58 +0200

    Dircty Correction of problems with spec search. looking for a better solution

--- lib/model/doctrine/FlatDict.class.php
+++ lib/model/doctrine/FlatDict.class.php
@@ -12,4 +12,79 @@
  */
 class FlatDict extends BaseFlatDict
 {
+ public function getTypeFormated()
+  {
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getTypeSearchFormated()
+  {
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getTypeGroupFormated()
+  {
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getSexFormated()
+  {
+    if ($this->_get('dict_value') == 'undefined')
+      return '-';
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getSexSearchFormated()
+  {
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getStateFormated()
+  {
+    if ($this->_get('dict_value') == 'not applicable')
+      return '-';
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getStateSearchFormated()
+  {
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getStageFormated()
+  {
+    if ($this->_get('dict_value') == 'undefined')
+      return '-';
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getStageSearchFormated()
+  {
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getSocialStatusFormated()
+  {
+    if ($this->_get('dict_value') == 'not applicable')
+      return '-';
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getSocialStatusSearchFormated()
+  {
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getRockFormFormated()
+  {
+    if ($this->_get('dict_value') == 'not applicable')
+      return '-';
+    return ucfirst($this->_get('dict_value'));
+  }
+
+  public function getRockFormSearchFormated()
+  {
+    return ucfirst($this->_get('dict_value'));
+  }
+
 }
