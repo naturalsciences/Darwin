ccb1a0edf4b62b9885d1cd9da6534f0be8e5d3e0
Brice Maron <bmaron@naturalsciences.be>
2011-04-29 09:10:48 +0200

    Migrate Button Ref to qtip2

--- web/js/button_ref.js
+++ web/js/button_ref.js
@@ -8,55 +8,6 @@ $(document).ready(function () {
     var last_position = $('body').scrollTop() ;              
     scroll(0,0) ;
     $(this).parent().parent().find('input[type="hidden"]').trigger({ type:"loadref"});
-    /*$(this).qtip({
-        content: {
-            title: { text : $(this).parent().attr('title'), button: 'X' },
-            url: $(this).attr('href')+'?with_js=1'
-        },
-        show: { when: 'click', ready: true },
-        position: {
-            target: $(document.body), // Position it via the document body...
-            adjust: { y: 210 },
-            corner: 'topMiddle' // ...at the topMiddle of the viewport
-        },
-        hide: false,
-        style: {
-            width: { min: 876, max: 1000},
-            title: { background: '#5BABBD', color: 'white'}
-        } ,
-        api: {
-            beforeShow: function()
-            {
-                // Fade in the modal "blanket" using the defined show speed
-                ref_element_id = null;
-                ref_element_name = null;
-                addBlackScreen()
-                $('#qtip-blanket').fadeIn(this.options.show.effect.length);
-            },
-            beforeHide: function()
-            {
-                // Fade out the modal "blanket" using the defined hide speed
-                $('#qtip-blanket').fadeOut(this.options.hide.effect.length).remove();
-            },
-        onHide: function()
-        {
-            if(ref_element_id != null && ref_element_name != null)
-            {
-              parent_el = $(this.elements.target).parent().prevAll('.ref_name');                           
-              parent_el.text(ref_element_name);         
-              parent_el.prev().val(ref_element_id);
-              $(this.elements.target).parent().prevAll('.ref_clear').show();
-              $(this.elements.target).parent().prevAll('.ref_clear').removeClass('hidden');
-              $(this.elements.target).find('.off').removeClass('hidden');
-              $(this.elements.target).find('.on').addClass('hidden');
-              parent_el.prev().trigger('change');
-            }
-            $('.result_choose_coll_rights').die('click') ;
-            $(this.elements.target).qtip("destroy");
-            scroll(0,last_position) ;            
-        }
-      }
-    });*/
     $(this).qtip({
       id: 'modal',
       content: {
@@ -76,7 +27,8 @@ $(document).ready(function () {
       
       show: {
         ready: true,
-        event: "click",
+        delay: 0,
+        event: event.type,
         solo: true,
         modal: {
           on: true,
@@ -84,7 +36,6 @@ $(document).ready(function () {
         },
       },
       hide: {
-        //event: false
         event: 'close_modal',
         target: $('body')
       },
@@ -104,10 +55,16 @@ $(document).ready(function () {
             api.elements.target.find('.on').addClass('hidden');
             parent_el.prev().trigger('change');
           }
-          scroll(0,last_position) ;            
+          scroll(0,last_position) ;     
+          api.destroy();
         }
       },
       style: 'ui-tooltip-light ui-tooltip-rounded'
+    },event)
+    // And for IE of course...
+    .each(function(i) {
+      $.attr(this, 'oldtitle', $.attr(this, 'title'));
+      this.removeAttribute('title');
     });
     return false;
   });
