3e25b50449c231e6e92066f19bb5641a70b6ee88
Brice Maron <bmaron@naturalsciences.be>
2011-05-10 11:54:30 +0200

    Add staging table into the new import branch

---
+++ lib/form/doctrine/StagingForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Staging form.
+ *
+ * @package    darwin
+ * @subpackage form
+ * @author     DB team <collections@naturalsciences.be>
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class StagingForm extends BaseStagingForm
+{
+  public function configure()
+  {
+  }
+}
