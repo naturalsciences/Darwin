4f3f350472aa64090682e050e6d057b9e22d904c
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2011-08-08 08:11:45 +0200

    Change owner script ready to be used
    refs #670
    
    Now the script produce alter of objects to change their owner.
    Still needs to adapt script so the user can choose which owner he wants

--- data/db/maintenance/change_owner_of_d2_objects.sql
+++ data/db/maintenance/change_owner_of_d2_objects.sql
@@ -2,18 +2,39 @@
 \o set_darwin2_obj_to_owner.sql
 select sqlcmd 
 from 
-     (select 'ALTER TABLE darwin2.' || tablename || ' OWNER TO darwin2;' as sqlcmd, 
+     (select 'ALTER DOMAIN darwin2.' || domain_name || ' OWNER TO darwin2;' as sqlcmd, 
              1 as orderby 
+      from information_schema.domains 
+      where domain_schema = 'darwin2'
+      UNION
+      select DISTINCT 'ALTER FUNCTION darwin2.' || x.proname || ' (' || array_to_string(darwin2.array_accum(y.typname), ',') || ') OWNER TO darwin2;' as sqlcmd, 
+            2 as orderby 
+      from (select oid as prooid, proname, unnest(proargtypes) as datatype from pg_proc 
+            where pronamespace = (select oid from pg_namespace where nspname = 'darwin2')
+          ) as x 
+          left join 
+          (select oid as typoid, case when typname = 'staging' then 'darwin2.staging' else typname end from pg_type
+          ) as y on x.datatype = y.typoid 
+      where proname not in ('dummy_first', 'array_accum')
+        /*and proname = 'fct_chk_possible_upper_level'*/
+      group by x.prooid, x.proname
+      UNION 
+      select 'ALTER TABLE darwin2.' || tablename || ' OWNER TO darwin2;' as sqlcmd, 
+             3 as orderby 
       from pg_tables 
       where schemaname = 'darwin2'
       UNION 
       select 'ALTER SEQUENCE darwin2.' || relname || ' OWNER TO darwin2;' as sqlcmd, 
-             3 as orderby 
+             4 as orderby 
       from pg_class 
       where relkind = 'S' 
         and relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'darwin2') 
-      UNION 
-      (select 'ALTER FUNCTION darwin2.' || proname || ' (' || array_to_string(array_agg(case when typname = 'staging' then 'darwin2.staging' else typname end), ',') || ') OWNER TO darwin2;' as sqlcmd, 
-              0 as orderby 
-       from (select oid, proname, unnest(proargtypes) as datatype from pg_proc where pronamespace = (select oid from pg_namespace where nspname = 'darwin2')) as x left join (select oid as typoid, typname from pg_type) as y on x.datatype = y.typoid 
-       group by x.oid, x.proname) UNION (select DISTINCT 'ALTER FUNCTION ' || trim(action_statement, 'EXECUTE PROCEDURE ') || ' OWNER TO darwin2;' as sqlcmd, 0 as orderby FROM information_schema.triggers where trigger_schema = 'darwin2')) as x order by orderby;
+      UNION
+      select distinct 'ALTER FUNCTION darwin2.' || trim(trim(action_statement, 'EXECUTE PROCEDURE '), 'darwin2.') || ' OWNER TO darwin2;' as sqlcmd, 
+             5 as orderby 
+      from information_schema.triggers 
+      where trigger_schema = 'darwin2'
+     ) as x order by orderby;
+\o
+\i set_darwin2_obj_to_owner.sql
+\! rm set_darwin2_obj_to_owner.sql
