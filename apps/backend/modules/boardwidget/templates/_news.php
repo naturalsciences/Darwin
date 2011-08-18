84c0770080c4730c64d1bc9fa2ce1f790492cc5e
Yann <ychambert@naturalsciences.be>
2011-08-18 09:35:57 +0200

    commented feed.yml parsing

--- apps/backend/modules/boardwidget/templates/_news.php
+++ apps/backend/modules/boardwidget/templates/_news.php
@@ -4,7 +4,7 @@
 <?php foreach($arrFeeds as $item):?>
   <li><?php echo auto_link_text($item['title'],'all',array('target'=>'_blank'));?>
     <div class="news_info">
-    <!--  <span class="date"><?php echo format_datetime($item['date'],'f');?></span> -->
+      <span class="date"><?php echo format_datetime($item['date'],'f');?></span> 
       :: <span class="link"><a href="<?php echo $item['link'];?>" target="_blank"><?php echo __('More');?></a></span>
     </div>
   </li>
