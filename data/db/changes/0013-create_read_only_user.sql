890ad0d90f35941dc2361d023ada5d446302e9c7
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2011-09-08 09:09:40 +0200

    Mistake in use of pwd prompting... oups

--- data/db/changes/0013-create_read_only_user.sql
+++ data/db/changes/0013-create_read_only_user.sql
@@ -1,5 +1,5 @@
  \prompt 'Please enter the password for the readonly user: ' readonlypwd
- CREATE ROLE d2viewer ENCRYPTED PASSWORD quote_literal(:readonlypwd) NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN; 
+ CREATE ROLE d2viewer ENCRYPTED PASSWORD quote_ident(:readonlypwd) NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN; 
  GRANT USAGE ON SCHEMA darwin2 TO d2viewer;
  GRANT SELECT ON darwin2.template_people TO d2viewer;
  GRANT SELECT ON darwin2.template_people_languages TO d2viewer;
