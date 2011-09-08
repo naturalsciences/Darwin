68a94f05998b2e46fac15ba8564505852d837638
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2011-09-08 09:24:20 +0200

    Hope last correction on pwd providin

--- data/db/changes/0013-create_read_only_user.sql
+++ data/db/changes/0013-create_read_only_user.sql
@@ -1,5 +1,6 @@
  \prompt 'Please enter the password for the readonly user: ' readonlypwd
- CREATE ROLE d2viewer ENCRYPTED PASSWORD quote_ident(:readonlypwd) NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN; 
+ \set newreadonlypwd '\'' :readonlypwd '\''
+ CREATE ROLE d2viewer ENCRYPTED PASSWORD quote_literal(:newreadonlypwd) NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN; 
  GRANT USAGE ON SCHEMA darwin2 TO d2viewer;
  GRANT SELECT ON darwin2.template_people TO d2viewer;
  GRANT SELECT ON darwin2.template_people_languages TO d2viewer;
