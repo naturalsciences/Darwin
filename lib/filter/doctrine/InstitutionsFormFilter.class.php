e318fff620dc3d7ff60cfce157fdbfbfe7790b98
Yann <ychambert@naturalsciences.be>
2011-10-14 10:06:05 +0200

    removed a useless function

--- lib/filter/doctrine/InstitutionsFormFilter.class.php
+++ lib/filter/doctrine/InstitutionsFormFilter.class.php
@@ -20,25 +20,10 @@ class InstitutionsFormFilter extends BaseInstitutionsFormFilter
 
     $this->widgetSchema['family_name'] = new sfWidgetFormInput();
     $this->widgetSchema['family_name']->setAttributes(array('class'=>'medium_size'));
-    $people_types = array(''=>'');
-    $types = People::getTypes();
-    foreach($types as $flag => $name)
-      $people_types[strval($flag)] = $name;
-    $this->widgetSchema['people_type'] = new sfWidgetFormChoice(array('choices' => $people_types )); 
-    $this->widgetSchema['people_type']->setLabel('Role');         
-    $this->validatorSchema['people_type'] = new sfValidatorChoice(array('choices' => $people_types, 'required' => false));
   }
 
   public function addFamilyNameColumnQuery($query, $field, $val)
   {
     return $this->addNamingColumnQuery($query, 'people', 'formated_name_ts', $val);
   }
-
-  public function addOnlyRoleColumnQuery($query, $field, $val)
-  {
-    if($val != 0)
-      $query->andWhere("(db_people_type &  ?) != 0 ", intval($val));
-    return $query;
-  }
-
 }
