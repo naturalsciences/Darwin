3b0a36c64fa147f4c070a443b391d3132bce6e2b
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2011-10-26 10:14:39 +0200

    Faster update flat people script
    refs #841
    
    - Update was to slow - corrections made to quicken the update - tested and ready to be applied this wensday evening (and then closing ticket)

--- data/db/maintenance/recompute_collectors_etc_arrays_in_flat.sql
+++ data/db/maintenance/recompute_collectors_etc_arrays_in_flat.sql
@@ -1,4 +1,5 @@
-﻿drop index if exists idx_gin_darwin_flat_spec_coll_ids;
+﻿
+drop index if exists idx_gin_darwin_flat_spec_coll_ids;
 drop index if exists idx_gin_darwin_flat_spec_don_sel_ids;
 drop index if exists idx_gin_darwin_flat_spec_ident_ids;
 drop index if exists idx_gin_darwin_flat_ind_ident_ids;
@@ -11,30 +12,65 @@ set spec_coll_ids = (
                         and cp.referenced_relation = 'specimens' 
                         and cp.people_type = 'collector'
                       group by cp.referenced_relation, cp.record_id, cp.people_type
-                    ),
-    spec_don_sel_ids = (
+                    )
+where spec_coll_ids != (
+                        SELECT array_agg(distinct cp.people_ref)
+                        FROM catalogue_people as cp 
+                        WHERE cp.record_id = df.spec_ref
+                          and cp.referenced_relation = 'specimens' 
+                          and cp.people_type = 'collector'
+                        group by cp.referenced_relation, cp.record_id, cp.people_type
+                       );
+
+update darwin_flat as df
+set spec_don_sel_ids = (
                         SELECT array_agg(distinct cp.people_ref)
                         FROM catalogue_people as cp 
                         WHERE cp.record_id = df.spec_ref
                           and cp.referenced_relation = 'specimens' 
                           and cp.people_type = 'donator'
                         group by cp.referenced_relation, cp.record_id, cp.people_type
-                       ), 
-    spec_ident_ids = (
+                       )
+where spec_don_sel_ids != (
+                            SELECT array_agg(distinct cp.people_ref)
+                            FROM catalogue_people as cp 
+                            WHERE cp.record_id = df.spec_ref
+                              and cp.referenced_relation = 'specimens' 
+                              and cp.people_type = 'donator'
+                            group by cp.referenced_relation, cp.record_id, cp.people_type
+                          );
+
+update darwin_flat as df
+set spec_ident_ids = (
                       SELECT array_agg(distinct cp.people_ref)
                       FROM catalogue_people as cp inner join identifications as ident on cp.record_id = ident.id and cp.referenced_relation = 'identifications' and cp.people_type = 'identifier'
                       WHERE ident.referenced_relation = 'specimens' 
                         and ident.record_id = df.spec_ref
                       group by ident.referenced_relation, ident.record_id, cp.people_type
-                     ),
-    ind_ident_ids = (
+                     )
+where spec_ident_ids != (
+                          SELECT array_agg(distinct cp.people_ref)
+                          FROM catalogue_people as cp inner join identifications as ident on cp.record_id = ident.id and cp.referenced_relation = 'identifications' and cp.people_type = 'identifier'
+                          WHERE ident.referenced_relation = 'specimens' 
+                            and ident.record_id = df.spec_ref
+                          group by ident.referenced_relation, ident.record_id, cp.people_type
+                        );
+
+update darwin_flat as df
+set ind_ident_ids = (
                       SELECT array_agg(distinct cp.people_ref)
                       FROM catalogue_people as cp inner join identifications as ident on cp.record_id = ident.id and cp.referenced_relation = 'identifications' and cp.people_type = 'identifier'
                       WHERE ident.referenced_relation = 'specimen_individuals' 
                         and ident.record_id = df.individual_ref
                       group by ident.referenced_relation, ident.record_id, cp.people_type
-                     )
-  ;
+                    )
+where ind_ident_ids != (
+                        SELECT array_agg(distinct cp.people_ref)
+                        FROM catalogue_people as cp inner join identifications as ident on cp.record_id = ident.id and cp.referenced_relation = 'identifications' and cp.people_type = 'identifier'
+                        WHERE ident.referenced_relation = 'specimen_individuals' 
+                          and ident.record_id = df.individual_ref
+                        group by ident.referenced_relation, ident.record_id, cp.people_type
+                       );
 
 CREATE INDEX idx_gin_darwin_flat_spec_coll_ids
   ON darwin_flat
