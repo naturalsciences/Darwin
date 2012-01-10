ae5ad0daba88840061ee1eae1b08198dd7dbdc80
Brice Maron <brice@bmaron.net>
2012-01-10 13:08:10 +0100

    Add frame around loan item

--- apps/backend/modules/loanitem/templates/editSuccess.php
+++ apps/backend/modules/loanitem/templates/editSuccess.php
@@ -3,10 +3,20 @@
 <div class="page">
     <h1 class="edit_mode"><?php echo __('Edit Loan Item');?></h1>
     <?php include_partial('loan/tabs', array('loan'=> $form->getObject()->getLoan(), 'item' => $form->getObject())); ?>
-    <?php include_partial('widgets/screen', array(
-	'widgets' => $widgets,
-	'category' => 'loanitem',
-	'columns' => 1,
-	'options' => array('eid' => $form->getObject()->getId(), 'table' => 'loans')
-	)); ?>
+    <div class="tab_content">
+      <?php echo form_tag('loan/create', array('class'=>'edition loan_form'));?>
+
+        <?php include_partial('widgets/screen', array(
+          'widgets' => $widgets,
+          'category' => 'loanitem',
+          'columns' => 1,
+          'options' => array('eid' => $form->getObject()->getId(), 'table' => 'loans')
+          )); ?>
+        <p class="clear"></p>
+        <p class="form_buttons">
+          &nbsp;<a href="<?php echo url_for('loan/index') ?>"><?php echo __('Cancel');?></a>
+          <input type="submit" value="<?php echo __('Save');?>" id="submit_loan"/>
+        </p>
+      </form>
+    </div>
 </div>
