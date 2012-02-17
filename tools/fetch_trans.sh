7bd0b8418656273cba777a70575ed8f942e8fe52
Brice Maron <brice@bmaron.net>
2012-02-17 10:00:21 +0100

    Add possibility to fetch original translation

--- tools/fetch_trans.sh
+++ tools/fetch_trans.sh
@@ -1,6 +1,13 @@
 #/bin/sh
 dir=$(dirname $(which $0));
 trans_url="http://darwin-trans/message/xliff/part"
+orig_url="http://darwin-trans/message/origxliff/part"
+
+
+if [ "$1" = "orig" ]; then
+  wget --no-proxy "$orig_url/2/" -O "$dir/../apps/public/i18n/en/messages.xml"
+  wget --no-proxy "$orig_url/1/" -O "$dir/../apps/backend/i18n/en/messages.xml"
+fi
 
 #Backend 
 wget --no-proxy "$trans_url/1/lang/1" -O "$dir/../apps/backend/i18n/fr/messages.xml"
@@ -9,3 +16,4 @@ wget --no-proxy "$trans_url/1/lang/39" -O "$dir/../apps/backend/i18n/nl/messages
 # Public
 wget --no-proxy "$trans_url/2/lang/1" -O "$dir/../apps/public/i18n/fr/messages.xml"
 wget --no-proxy "$trans_url/2/lang/39" -O "$dir/../apps/public/i18n/nl/messages.xml"
+
