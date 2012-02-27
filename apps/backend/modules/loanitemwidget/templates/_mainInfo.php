de086f6fddeb8f915bb30f49dbb4926b643d99f2
Brice Maron <brice@bmaron.net>
2012-02-27 11:09:08 +0100

    Add info about part into loan item. fix #1003

--- apps/backend/modules/loanitemwidget/templates/_mainInfo.php
+++ apps/backend/modules/loanitemwidget/templates/_mainInfo.php
@@ -4,7 +4,12 @@
     <?php $obj = $form->getObject();?>
     <tr>
       <th><?php echo __('Part Id');?></th>
-      <td><?php echo $obj->getPartRef();?></td>
+      <td>
+        <?php if($obj->getPartRef()):?>
+          <?php echo image_tag('info.png',"title=info class=extd_info data_id=".$obj->getPartRef());?>
+          <?php echo link_to('#'.$obj->getPartRef(),'parts/edit?id='.$obj->getPartRef());?>
+        <?php endif;?>
+      </td>
       <th><?php echo __('Details');?></th>
       <td rowspan="3" class="loanitem_details"><?php echo $obj->getDetails();?></td>
     </tr>
@@ -19,3 +24,37 @@
     </tr>
   </tbody>
 </table>
+
+<script  type="text/javascript">
+$(document).ready(function () {
+  $('.extd_info').mouseover(function(event){
+      $(this).qtip({
+        show: {
+          ready: true,
+          delay: 0,
+          event: event.type,
+          solo: true,
+        },
+        //hide: { event: 'mouseout' },
+        style: {
+          tip: true, // Give it a speech bubble tip with automatic corner detection
+          name: 'cream'
+        },
+        content: {
+          text: '<img src="/images/loader.gif" alt="loading"> Loading ...',
+          title: { text: '<?php echo __("Linked Info") ; ?>' },
+          ajax: {
+            url: '<?php echo url_for("loan/getPartInfo");?>',
+            type: 'GET',
+            data: { id:   $(this).attr('data_id') }
+          }
+        },
+        events: {
+          hide: function(event, api) {
+            api.destroy();
+          }
+        }
+      });
+    });
+  });
+</script>
--- apps/backend/modules/loanitemwidgetview/templates/_mainInfo.php
+++ apps/backend/modules/loanitemwidgetview/templates/_mainInfo.php
@@ -2,7 +2,12 @@
   <tbody>
     <tr>
       <th><?php echo __('Part Id');?></th>
-      <td><?php echo $loan->getPartRef();?></td>
+      <td>
+        <?php if($loan->getPartRef()):?>
+          <?php echo image_tag('info.png',"title=info class=extd_info data_id=".$loan->getPartRef());?>
+          <?php echo link_to('#'.$loan->getPartRef(),'parts/view?id='.$loan->getPartRef());?>
+        <?php endif;?>
+      </td>
       <th><?php echo __('Details');?></th>
       <td rowspan="3" class="loanitem_details"><?php echo $loan->getDetails();?></td>
     </tr>
@@ -17,3 +22,36 @@
     </tr>
   </tbody>
 </table>
+<script  type="text/javascript">
+$(document).ready(function () {
+  $('.extd_info').mouseover(function(event){
+      $(this).qtip({
+        show: {
+          ready: true,
+          delay: 0,
+          event: event.type,
+          solo: true,
+        },
+        //hide: { event: 'mouseout' },
+        style: {
+          tip: true, // Give it a speech bubble tip with automatic corner detection
+          name: 'cream'
+        },
+        content: {
+          text: '<img src="/images/loader.gif" alt="loading"> Loading ...',
+          title: { text: '<?php echo __("Linked Info") ; ?>' },
+          ajax: {
+            url: '<?php echo url_for("loan/getPartInfo");?>',
+            type: 'GET',
+            data: { id:   $(this).attr('data_id') }
+          }
+        },
+        events: {
+          hide: function(event, api) {
+            api.destroy();
+          }
+        }
+      });
+    });
+  });
+</script>
