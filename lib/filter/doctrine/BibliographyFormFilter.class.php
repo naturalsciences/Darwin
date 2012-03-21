7e113ac0940c1a7d8f11d88feb18b8335d6c1e5b
Brice Maron <brice@bmaron.net>
2012-03-21 11:59:14 +0100

    Add new bib table to sf

---
+++ lib/filter/doctrine/BibliographyFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Bibliography filter form.
+ *
+ * @package    darwin
+ * @subpackage filter
+ * @author     DB team <darwin-ict@naturalsciences.be>
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BibliographyFormFilter extends BaseBibliographyFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/filter/doctrine/BibliographyPeopleFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * BibliographyPeople filter form.
+ *
+ * @package    darwin
+ * @subpackage filter
+ * @author     DB team <darwin-ict@naturalsciences.be>
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BibliographyPeopleFormFilter extends BaseBibliographyPeopleFormFilter
+{
+  public function configure()
+  {
+  }
+}
