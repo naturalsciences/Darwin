74f2e682db6fbc3c988890594d8e853f811ea78f
Brice Maron <brice@bmaron.net>
2012-03-22 14:40:53 +0100

    Add message when  edit for checkrights

--- apps/backend/modules/bibliography/templates/editSuccess.php
+++ apps/backend/modules/bibliography/templates/editSuccess.php
@@ -2,9 +2,10 @@
 <?php slot('title', __('Edit Bibliography'));  ?>
 <div class="page">
     <h1 class="edit_mode"><?php echo __('Edit Bibliography');?></h1>
-    <?php if(count($no_right_col) > 0 && !$sf_user->isA(Users::ADMIN) ):?>
-      <?php include_partial('catalogue/warnedit', array('no_right_col' => $no_right_col)); ?>
-    <?php endif;?>
+    <div class="warn_message">
+      <?php echo __('<strong>Warning!</strong><br /> This unit might be used in items where you do no have encoding rights.<br/>Be sure of what you do!');?>
+    </div>
+
     <?php include_partial('form', array('form' => $form)) ?>
 
  <?php include_partial('widgets/screen', array(
