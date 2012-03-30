adde6f13bf107b2d06fef620d28347d40d8e85a6
Brice Maron <brice@bmaron.net>
2012-03-30 11:30:11 +0200

    Correct spring changes

--- data/db/changes/0021-spring_cleanup.sql
+++ data/db/changes/0021-spring_cleanup.sql
@@ -1,3 +1,4 @@
+SET search_path = darwin2, public;
 
 DROP INDEX IF EXISTS idx_associated_multimedia_multimedia_ref;
 DROP INDEX IF EXISTS idx_catalogue_levels_level_type;
@@ -7,8 +8,8 @@ DROP INDEX IF EXISTS idx_catalogue_people_referenced_record;
 
 CREATE INDEX CONCURRENTLY idx_catalogue_people_referenced_record on catalogue_people(referenced_relation, record_id);
 
-drop index idx_users_tracking_modification_date_time;
-drop index idx_users_tracking_user_ref;
+drop index IF EXISTS idx_users_tracking_modification_date_time;
+drop index IF EXISTS idx_users_tracking_user_ref;
 create index idx_users_tracking_user_ref_date_time on users_tracking (user_ref, modification_date_time desc);
 
 DROP INDEX IF EXISTS idx_catalogue_properties_referenced_record;
@@ -33,16 +34,16 @@ DROP INDEX IF EXISTS idx_users_languages_users_ref;
 DROP INDEX IF EXISTS idx_words_table_field;
 
 
-DROP TABLE habitats;
+DROP TABLE IF EXISTS habitats;
 
-DROP sequence habitats_id_seq;
-DROP table multimedia_keywords;
-DROP table soortenregister;
+DROP sequence IF EXISTS habitats_id_seq;
+DROP table IF EXISTS multimedia_keywords;
+DROP table IF EXISTS soortenregister;
 
 
-DROP table associated_multimedia;
-DROP sequence associated_multimedia_id_seq;
+DROP table IF EXISTS  associated_multimedia;
+DROP sequence IF EXISTS  associated_multimedia_id_seq;
 
-DROP table people_multimedia;
-DROP table users_multimedia;
-DROP table template_people_users_multimedia;
+DROP table IF EXISTS people_multimedia;
+DROP table IF EXISTS users_multimedia;
+DROP table IF EXISTS template_people_users_multimedia;
