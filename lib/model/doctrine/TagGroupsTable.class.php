abc932bab900dfb38946db614c354805660ca673
Brice Maron <brice@bmaron.net>
2012-05-08 11:54:33 +0200

    remove dummy_first

--- lib/model/doctrine/TagGroupsTable.class.php
+++ lib/model/doctrine/TagGroupsTable.class.php
@@ -25,7 +25,7 @@ class TagGroupsTable extends DarwinTable
     $q_sub_group = $conn->quote($sub_group, 'string');
     $sub_grouping_clause = " AND sub_group_type = $q_sub_group";
 
-    $sql = "select dummy_first (tag) as tag, count(*) as cnt , 1 as precision FROM (SELECT group_ref 
+    $sql = "select min (tag) as tag, count(*) as cnt , 1 as precision FROM (SELECT group_ref 
                  FROM tags 
                  WHERE tag_indexed IN 
                   (SELECT distinct(fulltoIndex(tags)) as u_tag 
@@ -71,7 +71,7 @@ class TagGroupsTable extends DarwinTable
     {
       if( $group != "" &&  $sub_group != "")
       {
-        $sql = "select dummy_first(tag) as tag, dummy_first(similarity(x.tag, tagsi)) as sims, 2 as size from
+        $sql = "select min(tag) as tag, min(similarity(x.tag, tagsi)) as sims, 2 as size from
           (select distinct(tag) as tag from tags 
             where ". $grouping_clause.$sub_grouping_clause." ) as x
           inner join (select trim(tagsi) as tagsi FROM regexp_split_to_table($tags, ';') AS tagsi) as y
@@ -103,7 +103,7 @@ class TagGroupsTable extends DarwinTable
         $sql .= " ORDER BY similarity(tag, u_tags) desc, tag asc";
 
         $sql = "select tag, 2 as size
-                from (" .$sql.") as subquery group by tag order by dummy_first(sims) desc ".$limit;
+                from (" .$sql.") as subquery group by tag order by min(sims) desc ".$limit;
       }
       $fuzzyResults = $conn->fetchAssoc($sql);
       $result = array_merge($result, $fuzzyResults);
@@ -115,7 +115,7 @@ class TagGroupsTable extends DarwinTable
   public function fetchTag($ids)
   {
     $q = Doctrine_Query::create()
-         ->from('TagGroups g')
+         ->from('TagGroups g')s
          ->innerJoin('g.Tags t')
          ->andWherein('g.gtu_ref', $ids);
     $r = $q->execute();
