4053374e41ff83f623fa2b1642e43b3e4182ef9f
Paul-André Duchesne <Paul-Andre.Duchesne@sciencesnaturelles.be>
2012-05-11 16:22:52 +0200

    Labeling view corrections

--- data/db/reports/ticketing/labeling.sql
+++ data/db/reports/ticketing/labeling.sql
@@ -76,10 +76,11 @@ select df.part_ref as unique_id,
        df.collection_name as collection_name,
        df.collection_path as collection_path,
        case when coalesce(df.part,'') in ('specimen', 'animal', 'undefined', 'unknown', '') then '' else df.part end as part_item,
-       case when df.individual_sex in ('undefined', 'unknown', 'not stated', 'non applicable') then '' else ', ' || df.individual_sex || case when df.individual_state = 'not applicable' then '' else df.individual_state end end as part_sex_state,
-       case when df.individual_type = 'specimen' then '' else ', ' || df.individual_type end as part_type,
-       case when df.individual_stage in ('undefined', 'unknown', 'not stated') then '' else ', ' || df.individual_stage end as part_stage,
-       case when coalesce(df.container_storage, '') in ('unknown', '/', '') then '' || case when coalesce(df.sub_container_storage, '') in ('unknown', '/', '')  then '' else ', ' || df.sub_container_storage end else ', ' || df.container_storage || case when coalesce(df.sub_container_storage, '') in ('unknown', '/', '') or df.sub_container_storage = df.container_storage then '' else ' - ' || df.sub_container_storage end end as part_storage,
+       case when df.individual_sex in ('undefined', 'unknown', 'not stated', 'non applicable') then '' else df.individual_sex || case when df.individual_state = 'not applicable' then '' else df.individual_state end end as part_sex_state,
+       case when df.individual_type = 'specimen' then '' else df.individual_type end as part_type,
+       case when df.individual_stage in ('undefined', 'unknown', 'not stated') then '' else df.individual_stage end as part_stage,
+       case when df.container is not null then case when coalesce(df.container_storage, '') in ('unknown', '/', '') then '' else df.container_storage end else '' end as part_container_storage,
+       case when df.sub_container is not null then case when coalesce(df.sub_container_storage, '') in ('unknown', '/', '') then '' else df.sub_container_storage end else '' end as part_sub_container_storage,
 /*       trim(both ',' from
         trim(case when coalesce(df.part,'') in ('specimen', 'animal', 'undefined', 'unknown', '') then '' else df.part end 
               || 
@@ -161,54 +162,78 @@ select df.part_ref as unique_id,
        case when df.gtu_from_date_mask >= 32 then to_char(df.gtu_from_date, 'DD/MM/YYYY') else '' end || case when df.gtu_to_date_mask >= 32 then ' - ' || to_char(df.gtu_to_date, 'DD/MM/YYYY') else '' end as gtu_date,
        case when df.gtu_location is not null then trunc((ST_Y(ST_Centroid(geometry(df.gtu_location))))::numeric, 6) || '/' || trunc((ST_X(ST_Centroid(geometry(df.gtu_location))))::numeric, 6) else '' end as lat_long,
        case when df.gtu_elevation is not null then trunc(df.gtu_elevation::numeric,2) || ' m' || case when df.gtu_elevation_accuracy is not null then ' +- ' || trunc(df.gtu_elevation_accuracy::numeric,2) || ' m' else '' end else '' end as elevation,
-       (select array_to_string(array_agg(people_list), ', ')
-        from (select distinct trim(family_name) as people_list
-              from catalogue_people as cp inner join people as peo on cp.people_ref = peo.id 
-              where cp.people_type = 'collector'
-                and cp.referenced_relation = 'specimens'
-                and cp.record_id = df.spec_ref
-                and peo.family_name NOT IN ('Unknown', '/')
-/*              order by cp.order_by*/
-             ) as x
-       )::varchar as collectors,
-       (select array_to_string(array_agg(people_list), ', ')
-        from (select distinct trim(family_name) as people_list 
-              from (catalogue_people as cp inner join people as peo on cp.people_ref = peo.id) inner join identifications as ident on cp.record_id = ident.id and cp.referenced_relation = 'identifications' and cp.people_type = 'identifier' 
-              where ident.referenced_relation = 'specimens'
-                and ident.record_id = df.spec_ref
-                and peo.family_name NOT IN ('Unknown', '/')
-                and ident.notion_date = (select max(notion_date)
-                                         from identifications as idt
-                                         where idt.referenced_relation = ident.referenced_relation
-                                           and idt.record_id = ident.record_id
-                                        )
-/*              order by cp.order_by*/
-             ) as x
+       (select case when length(regexp_replace(coll, '[^,]+', '', 'g')) > 2
+                    then substr(coll, 1, strpos(coll, ',')-1) || ' & al.'
+                    else coll
+                end
+        from
+          (select array_to_string(array_agg(people_list), ', ') as coll
+              from (select distinct trim(family_name) as people_list, cp.order_by
+                    from catalogue_people as cp inner join people as peo on cp.people_ref = peo.id
+                    where cp.people_type = 'collector'
+                      and cp.referenced_relation = 'specimens'
+                      and cp.record_id = df.spec_ref
+                      and peo.family_name NOT IN ('Unknown', '/')
+                    order by cp.order_by
+                  ) as x
+          ) as y
+        )::varchar as collectors,
+       (select case when length(regexp_replace(identi, '[^,]+', '', 'g')) > 2
+                    then substr(identi, 1, strpos(identi, ',')-1) || ' & al.'
+                    else identi
+                end || identi_year
+        from
+        (select array_to_string(array_agg(people_list), ', ') as identi, case when max(ident_date) is not null then ', ' || max(ident_date) else '' end as identi_year
+          from (select distinct trim(family_name) as people_list, cp.order_by, case when ident.notion_date_mask != 0 then extract(year from ident.notion_date) else null::double precision end as ident_date
+                from (catalogue_people as cp inner join people as peo on cp.people_ref = peo.id) inner join identifications as ident on cp.record_id = ident.id and cp.referenced_relation = 'identifications' and cp.people_type = 'identifier'
+                where ident.referenced_relation = 'specimens'
+                  and ident.record_id = df.spec_ref
+                  and peo.family_name NOT IN ('Unknown', '/')
+                  and ident.notion_date = (select max(notion_date)
+                                          from identifications as idt
+                                          where idt.referenced_relation = ident.referenced_relation
+                                            and idt.record_id = ident.record_id
+                                          )
+                order by cp.order_by
+              ) as x
+         ) as y
        )::varchar as identifiers,
-       (select array_to_string(array_agg(people_list), ', ')
-        from (select distinct trim(family_name) as people_list
-              from (catalogue_people as cp inner join people as peo on cp.people_ref = peo.id) inner join identifications as ident on cp.record_id = ident.id and cp.referenced_relation = 'identifications' and cp.people_type = 'identifier'
-              where ident.referenced_relation = 'specimen_parts'
-                and ident.record_id = df.spec_ref
-                and peo.family_name NOT IN ('Unknown', '/')
-                and ident.notion_date = (select max(notion_date)
-                                         from identifications as idt
-                                         where idt.referenced_relation = ident.referenced_relation
-                                           and idt.record_id = ident.record_id
-                                        )
-/*              order by cp.order_by*/
-             ) as x
+       (select case when length(regexp_replace(identi, '[^,]+', '', 'g')) > 2
+                    then substr(identi, 1, strpos(identi, ',')-1) || ' & al.'
+                    else identi
+               end || identi_year
+        from
+        (select array_to_string(array_agg(people_list), ', ') as identi, case when max(ident_date) is not null then ', ' || max(ident_date) else '' end as identi_year
+          from (select distinct trim(family_name) as people_list, cp.order_by, case when ident.notion_date_mask != 0 then extract(year from ident.notion_date) else null::double precision end as ident_date
+                from (catalogue_people as cp inner join people as peo on cp.people_ref = peo.id) inner join identifications as ident on cp.record_id = ident.id and cp.referenced_relation = 'identifications' and cp.people_type = 'identifier'
+                where ident.referenced_relation = 'specimen_parts'
+                  and ident.record_id = df.spec_ref
+                  and peo.family_name NOT IN ('Unknown', '/')
+                  and ident.notion_date = (select max(notion_date)
+                                          from identifications as idt
+                                          where idt.referenced_relation = ident.referenced_relation
+                                            and idt.record_id = ident.record_id
+                                          )
+                order by cp.order_by
+              ) as x
+         ) as y
        )::varchar as part_identifiers,
-       (select array_to_string(array_agg(people_list), ', ')
-        from (select distinct trim(family_name) as people_list
-              from catalogue_people as cp inner join people as peo on cp.people_ref = peo.id
-              where cp.people_type = 'donator'
-                and cp.referenced_relation = 'specimens'
-                and cp.record_id = df.spec_ref
-                and peo.family_name NOT IN ('Unknown', '/')
-/*              order by cp.order_by*/
-             ) as x
-       )::varchar as donators,
+       (select case when length(regexp_replace(coll, '[^,]+', '', 'g')) > 2
+                    then substr(coll, 1, strpos(coll, ',')-1) || ' & al.'
+                    else coll
+                end
+        from
+          (select array_to_string(array_agg(people_list), ', ') as coll
+              from (select distinct trim(family_name) as people_list, cp.order_by
+                    from catalogue_people as cp inner join people as peo on cp.people_ref = peo.id
+                    where cp.people_type = 'donator'
+                      and cp.referenced_relation = 'specimens'
+                      and cp.record_id = df.spec_ref
+                      and peo.family_name NOT IN ('Unknown', '/')
+                    order by cp.order_by
+                  ) as x
+          ) as y
+        )::varchar as donators,
        coalesce(df.ig_num, '-') as ig_num,
        df.ig_num_indexed as ig_num_indexed,
        convert_to_integer(coalesce(ig_num, '-')) as ig_numeric,
