e6795cd2ffdf5eb5b390dc2c263766e20c66ea4d
Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2012-09-03 21:38:19 +0200

    Fine tuned stats sql as a function for use in a reporting tool

--- data/db/reports/annual_stats_per_collections.sql
+++ data/db/reports/annual_stats_per_collections.sql
@@ -1,21 +1,30 @@
-﻿WITH RECURSIVE collpath(name, collpath, parent, id, parent_ref) AS (
-  SELECT name, '/', NULL, id, parent_ref FROM collections WHERE id = 2
+﻿create type stats_collections as (collection varchar, new_items bigint, updated_items bigint, new_types bigint, updated_types bigint, new_specimens bigint, updated_specimens bigint, new_species bigint);
+
+alter type stats_collections owner to darwin2;
+
+create or replace function stats_collections_encoding (collections.id%TYPE, timestamp, timestamp) returns setof stats_collections language sql immutable as $$
+WITH RECURSIVE collpath(name, collection_path, parent_path, id, parent_ref) AS (
+  SELECT name, '/' as collection_path, NULL as parent_path, id, parent_ref
+  FROM collections
+  WHERE id = $1
   UNION
   SELECT
     collections.name,
-    parentpath.collpath ||
-      CASE parentpath.collpath
+    parentpath.collection_path ||
+      CASE parentpath.collection_path
         WHEN '/' THEN ''
         ELSE ' / '
       END || collections.name,
-    parentpath.collpath, collections.id, collections.parent_ref
+    parentpath.collection_path,
+    collections.id,
+    collections.parent_ref
   FROM collections, collpath as parentpath
   WHERE collections.parent_ref = parentpath.id
 )
 SELECT *
 FROM
 (
-  SELECT collpath as collection,
+  SELECT collection_path as collection,
        (select count(sp.id)
         from users_tracking as ut inner join
           (specimen_parts as sp
@@ -28,9 +37,9 @@ FROM
         on ut.referenced_relation = 'specimen_parts'
         and ut.action = 'insert'
         and ut.record_id = sp.id
-        and modification_date_time between '2012-06-01' and '2012-08-31'
+        and modification_date_time between $2 and $3
         where s.collection_ref = collpath.id
-        ) as new_items_june_august_2012,
+        ) as new_items,
         (select count(distinct sp.id)
         from users_tracking as ut inner join
           (specimen_parts as sp
@@ -43,9 +52,9 @@ FROM
         on ut.referenced_relation = 'specimen_parts'
         and ut.action = 'update'
         and ut.record_id = sp.id
-        and modification_date_time between '2012-06-01' and '2012-08-31'
+        and modification_date_time between $2 and $3
         where s.collection_ref = collpath.id
-        ) as updated_items_june_august_2012,
+        ) as updated_items,
         (select count(si.id)
         from users_tracking as ut inner join
           (specimen_individuals as si
@@ -55,9 +64,9 @@ FROM
         on ut.referenced_relation = 'specimen_individuals'
         and ut.action = 'insert'
         and ut.record_id = si.id
-        and modification_date_time between '2012-06-01' and '2012-08-31'
+        and modification_date_time between $2 and $3
         where s.collection_ref = collpath.id
-        ) as new_types_june_august_2012,
+        ) as new_types,
         (select count(si.id)
         from users_tracking as ut inner join
           (specimen_individuals as si
@@ -67,25 +76,25 @@ FROM
         on ut.referenced_relation = 'specimen_individuals'
         and ut.action = 'update'
         and ut.record_id = si.id
-        and modification_date_time between '2012-06-01' and '2012-08-31'
+        and modification_date_time between $2 and $3
         where s.collection_ref = collpath.id
-        ) as updated_types_june_august_2012,
+        ) as updated_types,
         (select count(s.id)
         from users_tracking as ut inner join specimens as s
         on ut.referenced_relation = 'specimens'
         and ut.action = 'insert'
         and ut.record_id = s.id
-        and modification_date_time between '2012-06-01' and '2012-08-31'
+        and modification_date_time between $2 and $3
         where s.collection_ref = collpath.id
-        ) as new_specimens_june_august_2012,
+        ) as new_specimens,
         (select count(s.id)
         from users_tracking as ut inner join specimens as s
         on ut.referenced_relation = 'specimens'
         and ut.action = 'update'
         and ut.record_id = s.id
-        and modification_date_time between '2012-06-01' and '2012-08-31'
+        and modification_date_time between $2 and $3
         where s.collection_ref = collpath.id
-        ) as updated_specimens_june_august_2012,
+        ) as updated_specimens,
         (select count(distinct tax.id)
         from
         (users_tracking as ut inner join taxonomy as tax
@@ -93,331 +102,26 @@ FROM
           and ut.action = 'insert'
           and ut.record_id = tax.id
           and tax.level_ref > 47
-          and ut.modification_date_time between '2012-06-01' and '2012-08-31'
+          and ut.modification_date_time between $2 and $3
         ) inner join
         (specimens as s  inner join users_tracking as ust
           on ust.referenced_relation = 'specimens'
           and ust.action = 'insert'
           and ust.record_id = s.id
-          and ust.modification_date_time between '2012-06-01' and '2012-08-31'
+          and ust.modification_date_time between $2 and $3
         ) on s.taxon_ref = tax.id
         where s.collection_ref = collpath.id
-        ) as new_species_june_august_2012
+        ) as new_species
   FROM collpath
-  ORDER BY collpath
+  ORDER BY collection_path
 ) as subqry
-WHERE subqry.new_species_june_august_2012 != 0
-  OR  subqry.new_items_june_august_2012 != 0
-  OR  subqry.updated_items_june_august_2012 != 0
-  OR  subqry.new_types_june_august_2012 != 0
-  OR  subqry.updated_types_june_august_2012 != 0
-  OR  subqry.new_specimens_june_august_2012 != 0
-  OR  subqry.updated_specimens_june_august_2012 != 0
-;
+WHERE subqry.new_species != 0
+  OR  subqry.new_items != 0
+  OR  subqry.updated_items != 0
+  OR  subqry.new_types != 0
+  OR  subqry.updated_types != 0
+  OR  subqry.new_specimens != 0
+  OR  subqry.updated_specimens != 0
+$$;
 
-/*select name,
-       new_items_2012, updated_items_2012,
-       new_types_2012, updated_types_2012,
-       new_specimens_2012, updated_specimens_2012,
-       new_species_2012
-from
-  (select 1 as level, col.name as name,*//* col.path, col.id, (select array_agg(x) from (select id from collections where path like col.path || col.id || '/%' union select col.id) as x),*/
-/*                      (select count(sp.id)
-                      from users_tracking as ut inner join
-                        (specimen_parts as sp
-                          inner join
-                          (specimen_individuals as si
-                          inner join
-                          specimens as s on s.id = si.specimen_ref
-                          ) on si.id = sp.specimen_individual_ref
-                        )
-                      on ut.referenced_relation = 'specimen_parts'
-                      and ut.action = 'insert'
-                      and ut.record_id = sp.id
-                      and modification_date_time between '2012-06-01' and '2012-08-31'
-                      and extract(year from modification_date_time) = 2012
-                      where s.collection_ref in (select id from collections where path like col.path || col.id || '/%' union select col.id)
-                      ) as new_items_2012,
-                      (select count(distinct sp.id)
-                      from users_tracking as ut inner join
-                        (specimen_parts as sp
-                          inner join
-                          (specimen_individuals as si
-                          inner join
-                          specimens as s on s.id = si.specimen_ref
-                          ) on si.id = sp.specimen_individual_ref
-                        )
-                      on ut.referenced_relation = 'specimen_parts'
-                      and ut.action = 'update'
-                      and ut.record_id = sp.id
-                      and extract(year from modification_date_time) = 2012
-                      where s.collection_ref in (select id from collections where path like col.path || col.id || '/%' union select col.id)
-                      ) as updated_items_2012,
-                      (select count(si.id)
-                      from users_tracking as ut inner join
-                        (specimen_individuals as si
-                          inner join
-                          specimens as s on s.id = si.specimen_ref and si.type != 'specimen'
-                        )
-                      on ut.referenced_relation = 'specimen_individuals'
-                      and ut.action = 'insert'
-                      and ut.record_id = si.id
-                      and extract(year from modification_date_time) = 2012
-                      where s.collection_ref in (select id from collections where path like col.path || col.id || '/%' union select col.id)
-                      ) as new_types_2012,
-                      (select count(si.id)
-                      from users_tracking as ut inner join
-                        (specimen_individuals as si
-                          inner join
-                          specimens as s on s.id = si.specimen_ref and si.type != 'specimen'
-                        )
-                      on ut.referenced_relation = 'specimen_individuals'
-                      and ut.action = 'update'
-                      and ut.record_id = si.id
-                      and extract(year from modification_date_time) = 2012
-                      where s.collection_ref in (select id from collections where path like col.path || col.id || '/%' union select col.id)
-                      ) as updated_types_2012,
-                      (select count(s.id)
-                      from users_tracking as ut inner join specimens as s
-                      on ut.referenced_relation = 'specimens'
-                      and ut.action = 'insert'
-                      and ut.record_id = s.id
-                      and extract(year from modification_date_time) = 2012
-                      where s.collection_ref in (select id from collections where path like col.path || col.id || '/%' union select col.id)
-                      ) as new_specimens_2012,
-                      (select count(s.id)
-                      from users_tracking as ut inner join specimens as s
-                      on ut.referenced_relation = 'specimens'
-                      and ut.action = 'update'
-                      and ut.record_id = s.id
-                      and extract(year from modification_date_time) = 2012
-                      where s.collection_ref in (select id from collections where path like col.path || col.id || '/%' union select col.id)
-                      ) as updated_specimens_2012,
-                      (select count(distinct tax.id)
-                      from
-                      (users_tracking as ut inner join taxonomy as tax
-                        on ut.referenced_relation = 'taxonomy'
-                        and ut.action = 'insert'
-                        and ut.record_id = tax.id
-                        and tax.level_ref > 47
-                        and extract(year from ut.modification_date_time) = 2012
-                      ) inner join
-                      (specimens as s  inner join users_tracking as ust
-                        on ust.referenced_relation = 'specimens'
-                        and ust.action = 'insert'
-                        and ust.record_id = s.id
-                        and extract(year from ust.modification_date_time) = 2012
-                      ) on s.taxon_ref = tax.id
-                      where s.collection_ref in (select id from collections where path like col.path || col.id || '/%' union select col.id)
-                      ) as new_species_2012
-    from collections as col
-    where col.path like '/2/%'
-      and length(regexp_replace(col.path,'\d','')) = 2
-    group by col.id, col.name, col.path
-) as x
-order by x.level, x.name;
-/*UNION
-select 2 as level, usr.formated_name as name,
-(select count(sp.id)
- from users_tracking as ut inner join
-   (specimen_parts as sp
-    inner join
-    (specimen_individuals as si
-     inner join
-     specimens as s on s.id = si.specimen_ref
-    ) on si.id = sp.specimen_individual_ref
-   )
- on ut.referenced_relation = 'specimen_parts'
- and ut.action = 'insert'
- and ut.record_id = sp.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2011
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as new_items_2011,
-(select count(distinct sp.id)
- from users_tracking as ut inner join
-   (specimen_parts as sp
-    inner join
-    (specimen_individuals as si
-     inner join
-     specimens as s on s.id = si.specimen_ref
-    ) on si.id = sp.specimen_individual_ref
-   )
- on ut.referenced_relation = 'specimen_parts'
- and ut.action = 'update'
- and ut.record_id = sp.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2011
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as updated_items_2011,
-(select count(sp.id)
- from users_tracking as ut inner join
-   (specimen_parts as sp
-    inner join
-    (specimen_individuals as si
-     inner join
-     specimens as s on s.id = si.specimen_ref
-    ) on si.id = sp.specimen_individual_ref
-   )
- on ut.referenced_relation = 'specimen_parts'
- and ut.action = 'insert'
- and ut.record_id = sp.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2012
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as new_items_2012,
-(select count(distinct sp.id)
- from users_tracking as ut inner join
-   (specimen_parts as sp
-    inner join
-    (specimen_individuals as si
-     inner join
-     specimens as s on s.id = si.specimen_ref
-    ) on si.id = sp.specimen_individual_ref
-   )
- on ut.referenced_relation = 'specimen_parts'
- and ut.action = 'update'
- and ut.record_id = sp.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2012
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as updated_items_2012,
-(select count(si.id)
- from users_tracking as ut inner join
-   (specimen_individuals as si
-    inner join
-    specimens as s on s.id = si.specimen_ref and si.type != 'specimen'
-   )
- on ut.referenced_relation = 'specimen_individuals'
- and ut.action = 'insert'
- and ut.record_id = si.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2011
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as new_types_2011,
-(select count(si.id)
- from users_tracking as ut inner join
-   (specimen_individuals as si
-    inner join
-    specimens as s on s.id = si.specimen_ref and si.type != 'specimen'
-   )
- on ut.referenced_relation = 'specimen_individuals'
- and ut.action = 'update'
- and ut.record_id = si.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2011
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as updated_types_2011,
-(select count(si.id)
- from users_tracking as ut inner join
-   (specimen_individuals as si
-    inner join
-    specimens as s on s.id = si.specimen_ref and si.type != 'specimen'
-   )
- on ut.referenced_relation = 'specimen_individuals'
- and ut.action = 'insert'
- and ut.record_id = si.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2012
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as new_types_2012,
-(select count(si.id)
- from users_tracking as ut inner join
-   (specimen_individuals as si
-    inner join
-    specimens as s on s.id = si.specimen_ref and si.type != 'specimen'
-   )
- on ut.referenced_relation = 'specimen_individuals'
- and ut.action = 'update'
- and ut.record_id = si.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2012
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as updated_types_2012,
-(select count(s.id)
- from users_tracking as ut inner join specimens as s
- on ut.referenced_relation = 'specimens'
- and ut.action = 'insert'
- and ut.record_id = s.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2011
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as new_specimens_2011,
-(select count(s.id)
- from users_tracking as ut inner join specimens as s
- on ut.referenced_relation = 'specimens'
- and ut.action = 'update'
- and ut.record_id = s.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2011
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as updated_specimens_2011,
-(select count(s.id)
- from users_tracking as ut inner join specimens as s
- on ut.referenced_relation = 'specimens'
- and ut.action = 'insert'
- and ut.record_id = s.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2012
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as new_specimens_2012,
-(select count(s.id)
- from users_tracking as ut inner join specimens as s
- on ut.referenced_relation = 'specimens'
- and ut.action = 'update'
- and ut.record_id = s.id
- and ut.user_ref = usr.id
- and extract(year from modification_date_time) = 2012
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as updated_specimens_2012,
-(select count(distinct tax.id)
- from
- (users_tracking as ut inner join taxonomy as tax
-  on ut.referenced_relation = 'taxonomy'
-  and ut.action = 'insert'
-  and ut.record_id = tax.id
-  and tax.level_ref > 47
-  and extract(year from ut.modification_date_time) = 2011
- ) inner join
- (specimens as s  inner join users_tracking as ust
-  on ust.referenced_relation = 'specimens'
-  and ust.action = 'insert'
-  and ust.record_id = s.id
-  and ust.user_ref = usr.id
-  and extract(year from ust.modification_date_time) = 2011
- ) on s.taxon_ref = tax.id
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as new_species_2011,
-(select count(distinct tax.id)
- from
- (users_tracking as ut inner join taxonomy as tax
-  on ut.referenced_relation = 'taxonomy'
-  and ut.action = 'insert'
-  and ut.record_id = tax.id
-  and tax.level_ref > 47
-  and extract(year from ut.modification_date_time) = 2012
- ) inner join
- (specimens as s  inner join users_tracking as ust
-  on ust.referenced_relation = 'specimens'
-  and ust.action = 'insert'
-  and ust.record_id = s.id
-  and ust.user_ref = usr.id
-  and extract(year from ust.modification_date_time) = 2012
- ) on s.taxon_ref = tax.id
- where s.collection_ref in (select id from collections where path like '/2/%')
-) as new_species_2012
-from users_tracking as utr inner join users as usr
-on utr.user_ref = usr.id
-and usr.id in (7884, 8201, 40915, 41805, 41806, 41811, 42349, 47157, 47424)
-and utr.referenced_relation in ('specimens', 'specimen_individuals', 'specimen_parts')
-and utr.action in ('insert', 'update')
-and extract(year from utr.modification_date_time) in (2011, 2012)
-/*from collections as col
-where col.path like '/2/%'
-  and length(regexp_replace(col.path,'\d','')) = 2
-group by col.id, col.name, col.path*/
-/*) as x
-order by x.level, x.name;
-*/
-/*select * from collections where path like '/2/%' and length(regexp_replace(path,'\d','')) = 2 order by name;*/
-/*select count(*) from specimens where collection_ref = 26;*/
+alter function stats_collections_encoding (collections.id%TYPE, timestamp, timestamp) owner to darwin2;
