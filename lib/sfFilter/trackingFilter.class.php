11c96be54b0cab1d2ecba279dd44c25b32a88115
Brice Maron <brice@bmaron.net>
2013-04-23 12:30:18 +0200

    Add apache note with userid and session id

--- lib/sfFilter/trackingFilter.class.php
+++ lib/sfFilter/trackingFilter.class.php
@@ -32,6 +32,11 @@ class trackingFilter extends sfFilter
         $conn = Doctrine_Manager::connection();
         $conn->exec("select fct_set_user(".$user->getId().");");
       }
+      
+      if($user->isAuthenticated() && function_exists('apache_note')) {
+        apache_note('username', $user->getId() );
+        apache_note('sessionID', session_id());
+      }
     }
 
     $filterChain->execute();
