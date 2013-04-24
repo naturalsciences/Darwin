83e85c607bc8617b9cf498760ab9c949de2d7fcd
Brice Maron <brice@bmaron.net>
2013-04-24 11:15:00 +0200

    Avoid sending related file when cancel

--- lib/form/RelatedFileForm.class.php
+++ lib/form/RelatedFileForm.class.php
@@ -5,7 +5,7 @@ class RelatedFileForm extends BaseForm
   {
     $this->widgetSchema['filenames'] = new sfWidgetFormInputFile();
     $this->widgetSchema['filenames']->setLabel("Add File") ;
-    $this->widgetSchema['filenames']->setAttributes(array('class' => 'Add_related_file'));        
+    $this->widgetSchema['filenames']->setAttributes(array('class' => 'Add_related_file'));
     $this->validatorSchema['filenames'] = new sfValidatorFile(
       array(
           'required' => false,
