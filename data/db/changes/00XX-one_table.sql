34b4f82a3d018e33d3d3b01525114b97439f45e4
Brice Maron <brice@bmaron.net>
2013-06-10 11:22:03 +0200

    Next step for spec migration

--- data/db/changes/00XX-one_table.sql
+++ data/db/changes/00XX-one_table.sql
@@ -5,15 +5,15 @@ set search_path=darwin2,public;
 /***
 * Move Codes from Spec to parts
 **/
-CREATE OR REPLACE FUNCTION move_code_onepart() RETURNS boolean
+CREATE OR REPLACE FUNCTION move_refs() RETURNS boolean
 AS $$
 DECLARE
   tmp RECORD;
+  tmp2 RECORD;
+  source_ref INTEGER;
 BEGIN
 
-   -- Move Codes in spec that have 1 parts OR 
-   RAISE INFO 'Start moving Code/* from spec with 1 part';
-   
+   RAISE INFO 'Start moving template_ref from spec with 1 part';
    FOR tmp IN SELECT *, p.id as part_id, s.id as spec_id from 
     specimens s
     INNER JOIN specimen_individuals i on s.id = i.specimen_ref
@@ -30,7 +30,24 @@ BEGIN
         when unique_violation then
          --IIIK Nothing
       END;
+   END LOOP;
 
+
+   RAISE INFO 'Start moving template_ref from ind with 1 part';
+   FOR tmp IN SELECT *, p.id as part_id, i.id as ind_id from 
+    specimen_individuals i
+    INNER JOIN specimen_parts p on i.id = p.specimen_individual_ref
+
+    where  (select count(*) from specimen_parts p on i.id = p.specimen_individual_ref
+      where i.specimen_ref = s.id) = 1
+    LOOP
+      BEGIN
+        UPDATE template_table_record_ref set referenced_relation='specimen_parts', record_id = tmp.part_id
+          WHERE  referenced_relation ='specimen_individuals' AND record_id=tmp.spec_id;
+      EXCEPTION
+        when unique_violation then
+         --IIIK Nothing
+      END;
    END LOOP;
 
 
@@ -60,7 +77,7 @@ BEGIN
 --          DELETE FROM codes  where id = tmp.code_id;
       END;
    END LOOP;
-   
+
    --- Move suspicious Rbins Codes
    --- Remove Code that are already once in parts
    --- Remove ALL
@@ -68,67 +85,98 @@ BEGIN
    DELETE FROM codes c1 where c1.referenced_relation ='specimens' AND 
         (select count(*) from codes c2 where c1.record_id = c2.record_id and c1.referenced_relation ='specimens') = 1;
    delete from codes c where referenced_relation ='specimens';
+
+
+/**********/
+
+
+   source_ref := -1;
+   RAISE INFO 'Start moving template_ref from Individuals ';
+   FOR tmp IN SELECT s.id as source_ref, p.id as p_id from specimens s
+     INNER JOIN specimen_individuals i on s.id = i.specimen_ref
+     INNER JOIN specimen_parts p on i.id = p.specimen_individual_ref
+    LOOP
+
+      INSERT INTO comments(referenced_relation, record_id, notion_concerned, code_prefix, comment_indexed) 
+        (SELECT 'specimen_parts', tmp.p_id, notion_concerned, comment, comment_indexed
+          FROM comments c WHERE c.referenced_relation='specimens' and record_id=tmp.source_ref  );
+
+
+      INSERT INTO ext_links(referenced_relation, record_id, url, comment, comment_indexed) 
+        (SELECT 'specimen_parts', tmp.p_id, url, comment, comment_indexed
+          FROM ext_links c WHERE c.referenced_relation='specimens' and record_id=tmp.source_ref
+            WHERE not exists( 
+              select 1 from ext_links c2 where  referenced_relation='specimens' and record_id=tmp.source_ref and c2.url=c.url
+             ) 
+         );
+
+      INSERT INTO ext_links(referenced_relation, record_id, url, comment, comment_indexed) 
+        (SELECT 'specimen_parts', tmp.p_id, url, comment, comment_indexed
+          FROM ext_links c WHERE c.referenced_relation='specimens' and record_id=tmp.source_ref
+            WHERE not exists( 
+              select 1 from ext_links c2 where  referenced_relation='specimens' and record_id=tmp.source_ref and c2.url=c.url
+             ) 
+         );
+         
+   FOR tmp2 IN SELECT * from catalogue_properties c where referenced_relation='specimens' and record_id=tmp.source_ref
    
-
-
-    RAISE INFO 'Copy Comments from spec to all parts';
-       FOR tmp IN SELECT c1.*, p.id as p_id  FROM comments  c1
-    INNER JOIN specimens s on s.id = c1.record_id AND c1.referenced_relation ='specimens'
-    INNER JOIN specimen_individuals i on s.id = i.specimen_ref
-    INNER JOIN specimen_parts p on i.id = p.specimen_individual_ref
-      LOOP
-      BEGIN
+      INSERT INTO catalogue_properties(referenced_relation, record_id, 
+        property_type, property_sub_type, 
+        property_sub_type_indexed, property_qualifier, property_qualifier_indexed, 
+        date_from_mask, date_from, date_to_mask, date_to, property_unit, 
+        property_accuracy_unit, property_method, property_method_indexed, 
+        property_tool, property_tool_indexed
+      ) 
+        VALUES ('specimen_parts', tmp.p_id,
+          tmp2.property_type, tmp2.property_sub_type,
+          tmp2.property_sub_type_indexed, tmp2.property_qualifier, tmp2.property_qualifier_indexed, 
+          tmp2.date_from_mask, tmp2.date_from, tmp2.date_to_mask, tmp2.date_to, tmp2.property_unit, 
+          tmp2.property_accuracy_unit, tmp2.property_method, tmp2.property_method_indexed, 
+          tmp2.property_tool, tmp2.property_tool_indexed
+         );
       
-      INSERT INTO comments(
-            referenced_relation, record_id, notion_concerned, code_prefix, 
-            comment_indexed)
-        VALUES('specimen_parts', tmp.p_id, tmp.notion_concerned, tmp.comment, 
-            tmp.comment_indexed);
+        INSERT INTO properties_values(
+            property_ref, property_value, property_value_unified, property_accuracy, 
+            property_accuracy_unified)
+        SELECT currval('catalogue_properties_id_seq'), property_value, property_value_unified, property_accuracy, 
+            property_accuracy_unified from properties_values where property_ref = tmp2.id
+            
+      END LOOP;
+   END LOOP;
 
-      EXCEPTION
-        when unique_violation then
-          --pass
-      END;
-    END LOOP;
-    DELETE FROM Comments c1 where c1.referenced_relation ='specimens';
+  RAISE INFO 'Delete spec comments';
+  DELETE FROM comments where referenced_relation='specimens';
+  RAISE INFO 'Delete spec properties';
+  DELETE FROM catalogue_properties where referenced_relation='specimens';
 
-    RAISE INFO 'Copy Comments from indiv to all parts';
-       FOR tmp IN SELECT c1.*, p.id as p_id  FROM comments  c1
-    INNER JOIN specimen_individuals i on c1.record_id=i.id AND c1.referenced_relation ='specimen_individuals'
-    INNER JOIN specimen_parts p on i.id = p.specimen_individual_ref
-      LOOP
-      BEGIN
-      
-      INSERT INTO comments(
-            referenced_relation, record_id, notion_concerned, code_prefix, 
-            comment_indexed)
-        VALUES('specimen_parts', tmp.p_id, tmp.notion_concerned, tmp.comment, 
-            tmp.comment_indexed);
 
-      EXCEPTION
-        when unique_violation then
-          --pass
-      END;
-    END LOOP;
-    DELETE FROM Comments c1 where c1.referenced_relation ='specimen_individuals';
+    
+/*****/
+
 
    RETURN TRUE;
 END;
 $$
 language plpgsql;
 
+SET SESSION session_replication_role = replica;
 
-select move_code_onepart();
+select move_refs();
 
+SET SESSION session_replication_role = origin;
 
 
 --- Move Comments (Indiv + spec) ==> part?
-
 --- Move Ext links  (Indiv + spec) => part
 --- Move Properties  (Indiv + spec)
+
+
+--- Move Files
 --- Merge Then move Indentifications (Indiv => Part)
+
 --- REwrite ident to simple REF
 --- REwrite code to simple REF ?
+
 --- Cleanup Part auto copy in Collections
 --- Move My saved Searches ==> chg subject
 --- Move  my_widgets
@@ -143,6 +191,6 @@ select move_code_onepart();
 
 
 /** Cleanup migration scripts ****/
-drop function move_code_onepart();
+drop function move_refs();
 
 rollback;
