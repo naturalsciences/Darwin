baa295bb8ad345ac8ac5ea694dd78a51309a585e
Brice Maron <brice@bmaron.net>
2013-10-07 14:24:31 +0200

    Resolve conflicts merging links

--- apps/backend/modules/specimenwidgetview/templates/_refTaxon.php
+++ apps/backend/modules/specimenwidgetview/templates/_refTaxon.php
@@ -1,6 +1,21 @@
 <table class="catalogue_table_view">
   <tr>
-	<td><?php echo $spec->getTaxonName() ; ?>
-	</td>
+    <td><?php echo $spec->getTaxonName() ; ?>
+    </td>
   </tr>
 </table>
+<script type="text/javascript">
+$(document).ready(function ()
+{
+   $('.taxon_view .info').click(function()
+   {
+     if($('.taxon_view .tree').is(":hidden"))
+     {
+       $.get('<?php echo url_for('catalogue/tree?table=taxonomy&id='.$spec->getTaxonRef()) ; ?>',function (html){
+         $('.taxon_view .tree').html(html).slideDown();
+         });
+     }
+     $('.taxon_view .tree').slideUp();
+   });
+});
+</script>
