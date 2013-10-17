dc771d15f24c24aea93db4ed394e1ca1641d6d6d
Yann Chambert <ychambert@naturalsciences.be>
2013-10-17 12:51:12 +0200

    corrected unit test

--- test/unit/model/CollectionsTableTest.php
+++ test/unit/model/CollectionsTableTest.php
@@ -10,7 +10,7 @@ $list = Doctrine::getTable('Collections')->fetchByInstitutionList($user);
 $t->is($list[0]->getFormatedName(),'Institut Royal des Sciences Naturelles de Belgique','Thre list give institutions');
 $collections = $list[0]->Collections;
 
-$t->is(count($collections),4,'Get all collections');
+$t->is(count($collections),5,'Get all collections');
 $t->is($collections[0]->getPath(),'/','The first item has path /');
 $t->is($collections[3]->getName(),'Fossile Aves','The childrens item are also fetched');
 
@@ -18,17 +18,17 @@ $t->is($list[1]->getFormatedName(),'UGMM','Thre list give institutions');
 
 $collections = Doctrine::getTable('Collections')->getAllCollections();
 
-$t->is($collections[0]->getName(),'Molusca','The last item is molusca (correctly order)');
+$t->is($collections[0]->getName(),'Import collection','The last item is Import collection (correctly order)');
 
 $t->info('getCollectionByName');
 
-$col = Doctrine::getTable('Collections')->getCollectionByName('Molusca');
+$col = Doctrine::getTable('Collections')->getCollectionByName('Import collection');
 $t->is($col->getId(), $collections[0]->getId(),'Got the good collection');
 
 $t->info('getDistinctCollectionByInstitution');
 
 $list2 = Doctrine::getTable('Collections')->getDistinctCollectionByInstitution($list[0]->getId());
-$t->is(count($list2), 5, 'We have 5 collections in this institution ("" + 4)');
+$t->is(count($list2), 6, 'We have 6 collections in this institution ("" + 4)');
 
 $list2 = Doctrine::getTable('Collections')->getDistinctCollectionByInstitution($list[1]->getId());
 $t->is(count($list2), 2, 'We have 2 collections in this institution ("" + 1)');
