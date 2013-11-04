dbd5afb918c18f56a4902c0dc6eaab7ff72d2623
Brice Maron <brice@bmaron.net>
2013-11-04 17:11:52 +0100

    Fix initiate data

--- data/db/initiate_data.sql
+++ data/db/initiate_data.sql
@@ -494,7 +494,7 @@ INSERT INTO taxonomy (name, name_indexed, level_ref, status) VALUES ('Eucaryota'
 INSERT INTO taxonomy (name, name_indexed, level_ref, status) VALUES ('Bacteria', 'bacteria', 1, 'valid');
 INSERT INTO taxonomy (name, name_indexed, level_ref, status) VALUES ('Archaea', 'archaea', 1, 'valid');
 INSERT INTO taxonomy (name, name_indexed, level_ref, status) VALUES ('Virus','virus', 1, 'valid');
-INSERT INTO classification_keywords (referenced_relation, record_id, keyword_type, keyword, keyword_indexed) (selectect 'taxonomy', id, 'GenusOrMonomial', 'Virus', 'virus' from taxonomy where name = 'Virus');
-INSERT INTO classification_keywords (referenced_relation, record_id, keyword_type, keyword, keyword_indexed) (selectect 'taxonomy', id, 'GenusOrMonomial', 'Bacteria', 'bacteria' from taxonomy where name = 'Bacteria');
-INSERT INTO classification_keywords (referenced_relation, record_id, keyword_type, keyword, keyword_indexed) (selectect 'taxonomy', id, 'GenusOrMonomial', 'Eucaryota', 'Eucaryota' from taxonomy where name = 'Eucaryota');
-INSERT INTO classification_keywords (referenced_relation, record_id, keyword_type, keyword, keyword_indexed) (selectect 'taxonomy', id, 'GenusOrMonomial', 'Archaea', 'archaea' from taxonomy where name = 'Archaea');
+INSERT INTO classification_keywords (referenced_relation, record_id, keyword_type, keyword, keyword_indexed) (select 'taxonomy', id, 'GenusOrMonomial', 'Virus', 'virus' from taxonomy where name = 'Virus');
+INSERT INTO classification_keywords (referenced_relation, record_id, keyword_type, keyword, keyword_indexed) (select 'taxonomy', id, 'GenusOrMonomial', 'Bacteria', 'bacteria' from taxonomy where name = 'Bacteria');
+INSERT INTO classification_keywords (referenced_relation, record_id, keyword_type, keyword, keyword_indexed) (select 'taxonomy', id, 'GenusOrMonomial', 'Eucaryota', 'Eucaryota' from taxonomy where name = 'Eucaryota');
+INSERT INTO classification_keywords (referenced_relation, record_id, keyword_type, keyword, keyword_indexed) (select 'taxonomy', id, 'GenusOrMonomial', 'Archaea', 'archaea' from taxonomy where name = 'Archaea');
