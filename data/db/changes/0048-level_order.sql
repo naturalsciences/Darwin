51332b8fa69b996fcfaf7c83fad7afdc52f7257f
Brice Maron <brice@bmaron.net>
2013-11-04 17:09:50 +0100

    Fix initiate data

--- data/db/changes/0048-level_order.sql
+++ data/db/changes/0048-level_order.sql
@@ -1,10 +1,10 @@
 begin;
 set search_path=darwin2,public;
 
-alter table catalogue_levels add column lvl_order integer not null default 999;
-update catalogue_levels set lvl_order = id;
-update catalogue_levels set lvl_order = id+1 where id > 63 ;
+alter table catalogue_levels add column level_order integer not null default 999;
+update catalogue_levels set level_order = id;
+update catalogue_levels set level_order = id+1 where id > 63 ;
 
-update catalogue_levels set lvl_order = 64 where id = 85 and level_type='lithostratigraphy';
+update catalogue_levels set level_order = 64 where id = 85 and level_type='lithostratigraphy';
 
 commit;
