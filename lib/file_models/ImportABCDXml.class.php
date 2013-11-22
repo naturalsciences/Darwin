698887e67ee432223de1df33a44d072a4252eab6
Brice Maron <brice@bmaron.net>
2013-11-22 14:05:04 +0100

    Enhance dates parsing

--- lib/file_models/ImportABCDXml.class.php
+++ lib/file_models/ImportABCDXml.class.php
@@ -100,10 +100,12 @@ class ImportABCDXml implements IImportModels
       case "Database" : $this->object->desc .= "Database ref :".$this->cdata.";"  ; break;
       case "DateText" : $this->object->getDateText($this->cdata) ; break;
       case "DateTime" : if($this->getPreviousTag() == "Gathering"){
-                        $this->staging["gtu_from_date"] = $this->object->getFromDate() ;
-                        $this->staging["gtu_to_date"] = $this->object->getToDate() ;
-                        $this->staging["gtu_from_date_mask"] = 56 ; $this->staging["gtu_to_date_mask"] = 56 ;
-                       } ; break;
+      echo $this->object->getFromDate();
+          if( $this->object->getFromDate()) $this->staging["gtu_from_date"] = $this->object->getFromDate()->getDateTime() ;
+          if( $this->object->getToDate()) $this->staging["gtu_to_date"] = $this->object->getToDate()->getDateTime() ;
+          if( $this->object->getFromDate())$this->staging["gtu_from_date_mask"] =  $this->object->getFromDate()->getMask() ;
+          if( $this->object->getToDate()) $this->staging["gtu_to_date_mask"] =  $this->object->getToDate()->getMask() ;
+        } ; break;
       case "dna:Concentration" : $this->property = new ParsingProperties("DNA Concentration") ; $this->property->property->setLowerValue($this->cdata) ; $this->addProperty(true) ; break;
       case "dna:DNASample" : $this->object->addMaintenance($this->staging) ; break;
       case "dna:ExtractionDate" : $dt =  FuzzyDateTime::getValidDate($this->cdata); $this->object->maintenance->setModificationDateTime($dt->getDateTime()); $this->object->maintenance->setModificationDateMask($dt->getMask()); break;
@@ -133,8 +135,8 @@ class ImportABCDXml implements IImportModels
       case "efg:InformalLithostratigraphicName" : $this->staging['litho_local'] = true ; break;
       case "efg:InformalNameString" : $this->object->informal = true ; break;
       case "InheritedName" : $this->people['family_name'] = $this->cdata ; break;
-      case "ISODateTimeBegin" : if($this->getPreviousTag() == "DateTime")  { $this->object->GTUdate['from'] = $this->cdata ;} break;
-      case "ISODateTimeEnd" :  if($this->getPreviousTag() == "DateTime"){ $this->object->GTUdate['to'] = $this->cdata;}  break;
+      case "ISODateTimeBegin" : if($this->getPreviousTag() == "DateTime")  { $this->object->GTUdate['from'] = FuzzyDateTime::getValidDate($this->cdata) ;} break;
+      case "ISODateTimeEnd" :  if($this->getPreviousTag() == "DateTime"){ $this->object->GTUdate['to'] = FuzzyDateTime::getValidDate($this->cdata);}  break;
       case "IsQuantitative" : $this->property->property->setIsQuantitative($this->cdata) ; break;
       case "KindOfUnit" : $this->staging['part'] = $this->cdata ; break;
       case "LatitudeDecimal" : $this->staging['gtu_latitude'] = $this->cdata ; break;
