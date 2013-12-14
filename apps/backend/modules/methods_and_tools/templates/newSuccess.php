478d83293f06784ff2ab55b6d36a2253254c45a8
Brice Maron <brice@bmaron.net>
2013-12-14 23:11:19 +0100

    Fix translatable strings

--- apps/backend/modules/methods_and_tools/templates/newSuccess.php
+++ apps/backend/modules/methods_and_tools/templates/newSuccess.php
@@ -1,5 +1,5 @@
 <?php if(isset($notion)):?>
-  <?php slot('title', __('Add '.$notion));  ?>
+  <?php slot('title', __('New '.$notion));  ?>
   <div class="page">
       <h1 class="edit_mode"><?php echo __('New '.$notion);?></h1>
       <?php include_partial('form', array('form' => $form, 'notion'=>$notion)) ?>
