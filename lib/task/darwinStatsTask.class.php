4a020065e0168e6dc299cab810416f94fb73106b
Brice Maron <brice@bmaron.net>
2013-12-16 11:35:45 +0000

    Fix stats

--- lib/task/darwinStatsTask.class.php
+++ lib/task/darwinStatsTask.class.php
@@ -4,29 +4,29 @@ class darwinStatsTask extends sfBaseTask
 {
   /* Add your requests in the array $request_array
      - title : is the title of the statistic displayed
-     - description : will appear in our help icon 
-     - fields : set yours fields here they MUST be the same as the fields name in the query 
+     - description : will appear in our help icon
+     - fields : set yours fields here they MUST be the same as the fields name in the query
      - request : the request which will be executed
      - expandable : set it to true if your request return too much record, the expandable option will add a "+" button in the widget to avoid having a too big widget
      - level : is level on wich a user is allowed to see or not the stat ( 1 : everybody, 2 : encoder and more, 4 : collection manager ans more, 8 : admin only)
   */
   private $request_array = array(
-  "req1" => array(    
+  "req1" => array(
     "title" => "All types and the record count associated",
     "description" => "",
     "fields" => array("Type","Count"),
-    "request" => "SELECT Type, count(id) AS Count FROM specimen_individuals GROUP BY Type ORDER BY Type",
+    "request" => "SELECT Type, count(id) AS Count FROM specimens GROUP BY Type ORDER BY Type",
     "expandable" => true,
     "level" => 2,
     ),
-  "req2" => array(    
+  "req2" => array(
     "title" => "All objects in DaRWIN",
     "description" => "An average result of all objects already stored in our database",
-    "fields" => array("Total"),    
-    "request" => "SELECT sum((specimen_part_count_min+specimen_part_count_max)/2) as total FROM specimen_parts",
+    "fields" => array("Total"),
+    "request" => "SELECT sum((specimen_count_min+specimen_count_max)/2) as total FROM specimens",
     "expandable" => false,
-    "level" => 1, 
-    ),   
+    "level" => 1,
+    ),
   ) ;
   protected function configure()
   {
@@ -48,18 +48,18 @@ EOF;
   {
     // initialize the database connection
     $databaseManager = new sfDatabaseManager($this->configuration);
-    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();  
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
     $array = array() ;
     foreach($this->request_array as $key=>$request)
     {
-      $conn = Doctrine_Manager::connection()->getDbh();    
+      $conn = Doctrine_Manager::connection()->getDbh();
       $statement = $conn->prepare($request['request']);
-      $statement->execute(); 
+      $statement->execute();
       $results = $statement->fetchAll(PDO::FETCH_ASSOC);
       $array[$key] = array_merge($request, array('result' => $results)) ;
     }
     $array['date_gen_stat'] = date('d/m/Y - H:i');
-    $yaml = sfYaml::dump($array);    
+    $yaml = sfYaml::dump($array);
     file_put_contents(sfConfig::get('sf_data_dir').'/stats/stats.yml', $yaml);
   }
 }
