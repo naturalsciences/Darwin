e6ef570480620c5b098fe8a170b7dbf28442f0dc
Yann Chambert <ychambert@naturalsciences.be>
2013-12-19 13:35:08 +0100

    corrected a bug where tag in public search did not work at all

--- apps/public/modules/search/templates/purposeTagSuccess.php
+++ apps/public/modules/search/templates/purposeTagSuccess.php
@@ -1,5 +1,5 @@
 <ul>
   <?php foreach($tags as $tag):?>
-    <li class="tag_size_2<?php echo (!$tag['precision'])?' unprecise_tag':''?>"><?php echo $tag['tag'];?></li>
+    <li class="tag_size_2<?php echo (! isset($tag['precision']))?' unprecise_tag':''?>"><?php echo $tag['tag'];?></li>
   <?php endforeach;?>
 </ul>
