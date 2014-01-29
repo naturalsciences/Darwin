ef2f487560ca839569b0ac75561f84b79b58aa85
Brice Maron <bmaron@naturalsciences.be>
2014-01-29 21:47:16 +0100

    Fix test after updating the prefs

--- test/functional/backend/userActionsTest.php
+++ test/functional/backend/userActionsTest.php
@@ -246,7 +246,7 @@ $browser->
     checkElement('h1','/My Preferences/')->
     checkElement('.user_table > thead',3)->
     checkElement('.user_table > tbody',3)->
-    checkElement('.user_table > tbody > tr',5)->
+    checkElement('.user_table > tbody > tr',6)->
     checkElement('#preferences_board_spec_rec_pp option[selected]','10')->
     checkElement('#preferences_board_search_rec_pp option[selected]','10')->
   end()->
