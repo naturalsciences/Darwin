cdd734fe940251a71263546c2045922c1498a593
Brice Maron <brice@bmaron.net>
2014-02-06 11:36:32 +0100

    Add vacuum to the analyze proc

--- data/db/maintenance/analyze.sql
+++ data/db/maintenance/analyze.sql
@@ -1,8 +1,8 @@
 \t
 \o analyze_darwin2_obj.sql
 
-select 'ANALYZE darwin2.' || tablename || ';'
-from pg_tables 
+select 'VACUUM ANALYZE darwin2.' || tablename || ';'
+from pg_tables
 where schemaname = 'darwin2';
 
 \o
