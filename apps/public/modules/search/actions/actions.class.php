9c3e548e5ea49039ab4deb4d49f3cfdf316318b8
Marielle Adam <madam@naturalsciences.be>
2014-12-09 15:24:05 +0100

    Update executeGetTaxon

--- apps/public/modules/search/actions/actions.class.php
+++ apps/public/modules/search/actions/actions.class.php
@@ -187,17 +187,15 @@ class searchActions extends DarwinActions
   public function executeGetTaxon (sfWebRequest $request)
   {
   
-    $response = '';
-    if (!empty($request->hasParameter('taxon-name')))    {
-      $taxa = Doctrine::getTable('Taxonomy')->getOneTaxon($request->getParameter('taxon-name'));
-      $taxaCount = count($taxa);
-      if ($taxaCount==1) {
-        $response = $taxa[0]['name'];
-      }
-      elseif($taxaCount>1) {
-        $response = 'multiple match';
-      }
-    }
+ 	$response = $request->getParameter('taxon-name','');
+	$taxa = Doctrine::getTable('Taxonomy')->getOneTaxon($request->getParameter('taxon-name'));
+	$taxaCount = count($taxa);
+    if ($taxaCount==1) {
+	  $response = $taxa[0]['name'];
+	}
+	elseif($taxaCount>1) {
+	  $response = 'multiple match';
+	}
     return $this->renderText($response);
   
   }
