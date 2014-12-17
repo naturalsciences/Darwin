fee255cc9e342f448ff456bad8e5441b6b01c754
Marielle Adam <madam@naturalsciences.be>
2014-12-17 14:43:16 +0100

    Small update (delete old manuals)

--- apps/backend/modules/help/templates/indexSuccess.php
+++ apps/backend/modules/help/templates/indexSuccess.php
@@ -1,8 +1,108 @@
 <?php slot('title',__("Darwin Help"));?>
 <div class="page">
   <h1><?php echo __('Darwin Guide :');?></h1>
+     
+<!--      <p><a href="/help/Darwin2_NL.pdf">Gebruikershandleiding</a></p><br />
+      <p><a href="/help/Darwin2_FR.pdf">Manuel d'utilisation</a></p><br />
+      <p><a href="/help/Darwin2_EN.pdf">User guide</a></p><br />-->
+      
+      <p><a href="/help/Darwin2_FR_2014.pdf">Manuel d'utilisation</a></p><br />
+      
+  <h1><?php echo __('ABCD Templates* :');?></h1>
+  <p>*Only available in english</p><br />
   
-    <p><a href="/help/Darwin2_NL.pdf">Gebruikershandleiding</a></p><br />
-    <p><a href="/help/Darwin2_FR.pdf">Manuel d'utilisation</a></p><br />
-    <p><a href="/help/Darwin2_EN.pdf">User guide</a></p><br />
+  <p><a href="/help/How to import a XML file in Darwin.pdf">How to import a XML file in Darwin?</a></p><br />
+  
+  <table class="results ">
+    <thead>
+      <tr>
+        <th>Section</th>
+        <th>Templates</th>
+        <th>User manuals</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>General</td>
+        <td>
+          <a href="/help/ABCDImport2DaRWIN_General.xlsm">ABCDImport2DaRWIN_General.xlsm</a><br />
+          <a href="/help/ABCDImport2DaRWIN_General.xlsm">ABCDImport2DaRWIN_General_taxonFullName.xlsm</a>
+        </td>
+        <td><a href="/help/ABCDImport2DaRWIN_General_usermanual.pdf">ABCDImport2DaRWIN_General_usermanual.pdf</a></td>    
+      </tr>
+      <tr>
+        <td>DNA / JEMU</td>
+        <td>
+          <a href="/help/ABCDImport2DaRWIN_DNA.xlsm">ABCDImport2DaRWIN_DNA.xlsm</a><br />
+          <a href="/help/ABCDImport2DaRWIN_DNA.xlsm">ABCDImport2DaRWIN_DNA_taxonFullName.xlsm</a>
+        </td>
+        <td><a href="/help/ABCDImport2DaRWIN_DNA_usermanual.pdf">ABCDImport2DaRWIN_DNA_usermanual.pdf</a></td>    
+      </tr>
+      <tr>
+        <td>Invertebrates</td>
+        <td>
+          <a href="/help/ABCDImport2DaRWIN_InvEntomo.xlsm">ABCDImport2DaRWIN_InvEntomo.xlsm</a><br />
+          <a href="/help/ABCDImport2DaRWIN_InvEntomo.xlsm">ABCDImport2DaRWIN_InvEntomo_taxonFullName.xlsm</a>
+        </td>
+        <td><a href="/help/ABCDImport2DaRWIN_InvEntomo_usermanual.pdf">ABCDImport2DaRWIN_InvEntomo_usermanual.pdf</a></td>    
+      </tr>
+      <tr>
+        <td>Entomology</td>
+        <td>
+          <a href="/help/ABCDImport2DaRWIN_Entomo.xlsm">ABCDImport2DaRWIN_Entomo.xlsm</a><br />
+          <a href="/help/ABCDImport2DaRWIN_Entomo.xlsm">ABCDImport2DaRWIN_Entomo_taxonFullName.xlsm</a>
+        </td>
+        <td><a href="/help/ABCDImport2DaRWIN_Entomo_usermanual.pdf">ABCDImport2DaRWIN_Entomo_usermanual.pdf</a></td>    
+      </tr>
+      <tr>
+        <td>Vertebrates</td>
+        <td>
+          <a href="/help/ABCDImport2DaRWIN_Vert.xlsm">ABCDImport2DaRWIN_Vert.xlsm</a><br />
+          <a href="/help/ABCDImport2DaRWIN_Vert.xlsm">ABCDImport2DaRWIN_Vert_taxonFullName.xlsm</a>
+        </td>
+        <td><a href="/help/ABCDImport2DaRWIN_Vert_usermanual.pdf">ABCDImport2DaRWIN_Vert_usermanual.pdf</a></td>    
+      </tr>
+      <tr>
+        <td>Paleontology</td>
+        <td>
+          <a href="/help/ABCDImport2DaRWIN_Paleontology.xlsm">ABCDImport2DaRWIN_Paleontology.xlsm</a><br />
+          <a href="/help/ABCDImport2DaRWIN_Paleontology.xlsm">ABCDImport2DaRWIN_Paleontology_taxonFullName.xlsm</a>
+        </td>
+        <td><a href="/help/ABCDImport2DaRWIN_Paleontology_usermanual.pdf">ABCDImport2DaRWIN_Paleontology_usermanual.pdf</a></td>    
+      </tr>
+      <tr>
+        <td>Geology</td>
+        <td>
+          <a href="/help/ABCDImport2DaRWIN_DNA.xlsm">ABCDImport2DaRWIN_DNA.xlsm</a><br />
+          <a href="/help/ABCDImport2DaRWIN_DNA.xlsm">ABCDImport2DaRWIN_DNA_taxonFullName.xlsm</a>
+        </td>
+        <td><a href="/help/ABCDImport2DaRWIN_DNA_usermanual.pdf">ABCDImport2DaRWIN_DNA_usermanual.pdf</a></td>    
+      </tr>
+    </tbody>
+  </table>
+  
+  <br />
+  
+  <h1><?php echo __('Additional tools for import :');?></h1>
+  
+  <table class="results ">
+    <thead>
+      <tr>
+        <th>Taxonomy import</th>
+        <th>Taxon full name checker</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <p>Template: </p><a href="/help/TaxonomyImport.xlsm">TaxonomyImport.xlsm</a><br />
+          <p>User manual: </p><a href="/help/TaxonomyImport2DaRWIN_usermanual.pdf">TaxonomyImport2DaRWIN_usermanual.pdf</a>
+        </td>
+        <td><a href="/Check_taxonFullName.xlsm">Check_taxonFullName.xlsm</a></td>    
+      </tr>
+    </tbody>
+  </table>
+  
+   <br />
+   
 </div>
