24d3ba99c3719a52bf3a21a88a8c3b3c97d8d245
Yann Chambert <ychambert@naturalsciences.be>
2015-03-02 15:07:59 +0100

    reworked fct_imp_checked_expedition, witch absolutly did not work

--- data/db/changes/0066-reporting.sql
+++ data/db/changes/0066-reporting.sql
@@ -179,34 +179,44 @@ CREATE OR REPLACE FUNCTION fct_imp_checker_expeditions(line staging, import bool
   RETURNS boolean AS
 $$
 DECLARE
+  result_nbr integer :=0;
+  ref_record RECORD;
   ref_rec integer :=0;
+  ref refcursor;
 BEGIN
   IF line.expedition_name is null OR line.expedition_name ='' OR line.expedition_ref is not null THEN
     RETURN true;
   END IF;
+  OPEN ref FOR select * from expeditions where name_indexed = fulltoindex(line.expedition_name) ; 
+  LOOP
+    FETCH ref INTO ref_record ;    
+    IF NOT FOUND THEN
+  EXIT ;
+    END IF;
+    ref_rec = ref_record.id ;
+    result_nbr := result_nbr +1;
+  END LOOP ;
+  IF result_nbr = 0 THEN
+    IF import THEN
+      INSERT INTO expeditions (name, expedition_from_date, expedition_to_date, expedition_from_date_mask,expedition_to_date_mask)
+      VALUES (
+        line.expedition_name, COALESCE(line.expedition_from_date,'01/01/0001'),
+        COALESCE(line.expedition_to_date,'31/12/2038'), COALESCE(line.expedition_from_date_mask,0),
+        COALESCE(line.expedition_to_date_mask,0)
+      )
+      RETURNING id INTO line.expedition_ref;
 
-  select id into ref_rec from expeditions where name_indexed = fulltoindex(line.expedition_name) 
-  /* no expeditions date in the template at the moment, so I comment these lines below to avoid expedition creation each time */
-    /*and
-    expedition_from_date = COALESCE(line.expedition_from_date,'01/01/0001') AND
-    expedition_to_date = COALESCE(line.expedition_to_date,'31/12/2038') */;
-  IF NOT FOUND THEN
-      IF import THEN
-        INSERT INTO expeditions (name, expedition_from_date, expedition_to_date, expedition_from_date_mask,expedition_to_date_mask)
-        VALUES (
-          line.expedition_name, COALESCE(line.expedition_from_date,'01/01/0001'),
-          COALESCE(line.expedition_to_date,'31/12/2038'), COALESCE(line.expedition_from_date_mask,0),
-          COALESCE(line.expedition_to_date_mask,0)
-        )
-        RETURNING id INTO line.expedition_ref;
-
-        ref_rec := line.expedition_ref;
-        PERFORM fct_imp_checker_staging_info(line, 'expeditions');
-      ELSE
-        RETURN TRUE;
-      END IF;
+      ref_rec := line.expedition_ref;
+      PERFORM fct_imp_checker_staging_info(line, 'expeditions');
+    ELSE
+      RETURN TRUE;
+    END IF;
   END IF;
-
+  IF result_nbr >= 2 THEN
+    UPDATE staging SET status = (status || ('expedition' => 'too_much')) where id= line.id;
+    RETURN true;
+  END IF; 
+  /* So result_nbr = 1 ! */
   UPDATE staging SET status = delete(status,'expedition'), expedition_ref = ref_rec where id=line.id;
 
   RETURN true;
