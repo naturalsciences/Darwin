770fd89ce27bdb5333382617ce19be45a9cc8544
Yann Chambert <ychambert@naturalsciences.be>
2015-03-03 15:49:18 +0100

    try to add login button in full view, hope it works well

--- apps/public/templates/refined.php
+++ apps/public/templates/refined.php
@@ -12,6 +12,36 @@
     <link rel="shortcut icon" href="/favicon.ico" />
   </head>
   <body>
+  <div class="page blue_line">
+    <table>
+      <tr>
+        <td class="blue_line_left">
+          <ul class="lang_picker">
+            <li><?php echo link_to('En','board/lang?lang=en');?></li>
+            <li class="sep">|</li>
+            <li><?php echo link_to('Fr','board/lang?lang=fr');?></li>
+            <li class="sep">|</li>
+            <li><?php echo link_to('Nl','board/lang?lang=nl');?></li>
+            <li class="sep">|</li>
+            <li><?php echo link_to('Es','board/lang?lang=es_ES');?></li>
+          </ul>
+        </td>
+        <td class="blue_line_right">
+          <table class="register_form">
+            <tr>
+              <td class="menu_button">
+                <?php echo link_to(__('Login'),$sf_context->getConfiguration()->generateBackendUrl('homepage', array(), $sf_request),array('target'=>'blank')) ;?>
+              </td>
+              <td class="menu_button">
+                <?php echo link_to(__('DaRWIN search'),'search/search',array('target'=>'blank')) ;?>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </div>
+
     <?php echo $sf_content ?>
   </body>
 </html>
