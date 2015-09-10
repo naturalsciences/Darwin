bc574c1813e650f8db93ed1dda9b49af6cf13483
Franck Theeten <franck.theeten@africamuseum.be>
2015-09-10 16:05:26 +0200

    To display some properties in the 'SpecimensRelationships' widget. apps/backend/modules/specimenwidgetview/templates/_specimensRelationships.php modified

--- apps/backend/modules/specimenwidgetview/templates/_specimensRelationships.php
+++ apps/backend/modules/specimenwidgetview/templates/_specimensRelationships.php
@@ -10,6 +10,7 @@
       <th></th>
     </tr>
   </thead>
+  <?php $test="test" ?>
   <?php foreach($spec_related as $val):?>
   <tr>
     <td><?php echo $val->getRelationshipType() ; ?></td>
@@ -19,7 +20,11 @@
       <?php elseif($val->getUnitType()=="taxon") : ?>
         <a href="<?php echo url_for('taxonomy/view?id='.$val->getTaxonRef()) ; ?>"><?php echo $val->Taxonomy->getName(); ?></a>
       <?php elseif($val->getUnitType()=="specimens") : ?>
-        <a href="<?php echo url_for('specimen/view?id='.$val->getSpecimenRelatedRef()) ; ?>"><?php echo __('Specimen'); ?> : <?php echo $val->SpecimenRelated->getName(); ?></a>
+        <a href="<?php echo url_for('specimen/view?id='.$val->getSpecimenRelatedRef()) ; ?>"><?php echo __('Specimen'); ?> : <?php echo $val->SpecimenRelated->getName(); ?></a> 
+			<!--ftheeten 2015 09 10-->
+				<?php echo ucfirst(Doctrine::getTable('Properties')->getOneSubPropertyByType("specimens", $val->getSpecimenRelatedRef(), 'label_created_on'))?'Date created: '.Doctrine::getTable('Properties')->getOneSubPropertyByType("specimens", $val->getSpecimenRelatedRef(), 'label_created_on')->getLowerValue():'';?>
+				<?php echo ucfirst(Doctrine::getTable('Properties')->getOneSubPropertyByType("specimens", $val->getSpecimenRelatedRef(), 'valid_label'))?'Validity: '.Doctrine::getTable('Properties')->getOneSubPropertyByType("specimens", $val->getSpecimenRelatedRef(), 'valid_label')->getLowerValue():'';?>
+			<!--ftheeten 2015 09 10-->	
       <?php elseif($val->getUnitType()=="external") : ?>
         <?php echo $val->getSourceName();?> ID: <?php echo $val->getSourceId();?>
       <?php endif ; ?>
@@ -36,6 +41,7 @@
 </table>
 
 <!--  Insert Inverse relationship-->
+<br><b>Inverse relationships:</b><br/><br/>
 <table class="catalogue_table_view">
   <thead style="<?php echo ($spec_related_inverse->count()?'':'display: none;');?>">
     <tr>
@@ -54,6 +60,10 @@
     <td>
       <?php if($val->getUnitType()=="specimens") : ?>
         <a href="<?php echo url_for('specimen/view?id='.$val->getSpecimenRef()) ; ?>"><?php echo __('Specimen'); ?> : <?php echo $val->Specimen->getName(); ?></a>
+		<!--ftheeten 2015 09 10-->
+				<?php echo ucfirst(Doctrine::getTable('Properties')->getOneSubPropertyByType("specimens", $val->Specimen->getId(), 'label_created_on'))?'Date created: '.Doctrine::getTable('Properties')->getOneSubPropertyByType("specimens", $val->Specimen->getId(), 'label_created_on')->getLowerValue():''||$test;?>
+				<?php echo ucfirst(Doctrine::getTable('Properties')->getOneSubPropertyByType("specimens", $val->Specimen->getId(), 'valid_label'))?'Validity: '.Doctrine::getTable('Properties')->getOneSubPropertyByType("specimens", $val->Specimen->getId(), 'valid_label')->getLowerValue():''||$test;?>
+			<!--ftheeten 2015 09 10-->	
       <?php endif ; ?>
     </td>
     <td>
