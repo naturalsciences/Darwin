31330547182d9cc57cca35c30309671d09372458
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2015-10-28 16:46:41 +0100

    Basis of replacement of stats_encoders_encoding function sql - still need to be rewritten !

---
+++ data/db/reports/stats/annual_stats_per_encoder_per_collections.sql
@@ -0,0 +1,40 @@
+﻿select distinct users.formated_name as "User",
+                ( 
+                  select '/'||array_to_string(array_agg(sc.name),'/')||'/'||c.name
+                  from collections as sc
+                  inner join (select unnest(string_to_array(trim(c.path, '/'),'/'))::bigint as id) as scc on sc.id = scc.id
+                ) as "Collection path",
+                users_tracking.action as "Action",
+                count(*) over (
+                                partition by users.formated_name,
+                                             ( 
+                                               select '/'||array_to_string(array_agg(sc.name),'/')||'/'||c.name
+                                               from collections as sc
+                                               inner join (select unnest(string_to_array(trim(c.path, '/'),'/'))::bigint as id) as scc on sc.id = scc.id
+                                             ), 
+                                             users_tracking.action
+                              ) as "Count"
+from users
+inner join users_tracking 
+   on users.id = users_tracking.user_ref 
+  and users_tracking.modification_date_time between '2014-01-01'::timestamp and '2014-12-31'::timestamp
+  and users_tracking.referenced_relation = 'specimens'
+inner join specimens 
+   on users_tracking.record_id = specimens.id
+inner join collections as c
+   on c.id = specimens.collection_ref
+where case
+        when 0 = 0 then
+          true
+        else
+          users.id = 41811
+      end 
+  and case 
+        when  2 != 0 then
+          c.id in (select id from collections where id = 2 or path like '%/'||2||'/%')
+        else
+          true
+      end
+  and users_tracking.action != 'delete'
+order by "User", "Collection path", "Action"
+     
