70c0b1299b0c74a5c8e7ed1e047f593ca8a37717
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2015-11-25 12:20:11 +0100

    Director's Cut sql ready for recent vertebrates and invertebrates
    refs #1885

--- data/db/reports/stats/directors_cut.sql
+++ data/db/reports/stats/directors_cut.sql
@@ -34,31 +34,130 @@ specimens_data as
 (
 select 'BIO' as "Branch", 
        case
-         when strpos(taxon_path, '/91/') != 0 then
+         when strpos(taxon_path, '/91/') != 0 and 
+	     (collection_ref = 1 or collection_path like '/1/%') then
            'VZ'
-         when strpos(taxon_path, '/37/') != 0 or 
-              strpos(taxon_path, '/61/') != 0  then
-           'IZ'
+        when (strpos(taxon_path, '/37/') != 0 or 
+              strpos(taxon_path, '/61/') != 0 or 
+              strpos(taxon_path, '/353/') != 0 or 
+              strpos(taxon_path, '/184176/') != 0 or 
+              strpos(taxon_path, '/177325/') != 0 or
+              strpos(taxon_path, '/64/') != 0 or
+              strpos(taxon_path, '/65/') != 0 or 
+              strpos(taxon_path, '/177320/') != 0 or
+              strpos(taxon_path, '/276/') != 0 or
+              (strpos(taxon_path, '/2/') != 0 and 
+	       strpos(taxon_path, '/61/') = 0 and 
+	       strpos(taxon_path, '/353/') = 0 and 
+	       strpos(taxon_path, '/184176/') = 0 and 
+	       strpos(taxon_path, '/177325/') = 0 and
+	       strpos(taxon_path, '/64/') = 0 and
+	       strpos(taxon_path, '/65/') = 0 and 
+	       strpos(taxon_path, '/177320/') = 0 and
+	       strpos(taxon_path, '/276/') = 0
+	      ) or
+	      strpos(taxon_path, '/109920/') != 0 or
+	      strpos(taxon_path, '/30/') != 0 or
+	      strpos(taxon_path, '/72/') != 0 or
+	      strpos(taxon_path, '/75/') != 0 or 
+	      strpos(taxon_path, '/206101/') != 0 or
+	      strpos(taxon_path, '/44/') != 0 or
+	      strpos(taxon_path, '/33/') != 0 or 
+	      strpos(taxon_path, '/177319/') != 0 or
+	      strpos(taxon_path, '/28/') != 0 or
+	      strpos(taxon_path, '/79/') != 0 or
+	      strpos(taxon_path, '/19/') != 0 or
+	      strpos(taxon_path, '/81/') != 0 or
+	      strpos(taxon_path, '/83/') != 0 or
+	      strpos(taxon_path, '/90/') != 0 
+	     ) and 
+	     (collection_ref = 2 or collection_path like '/2/%') 
+          then
+          'IZ'
          else
            'Other'
        end as "Department",
        case
-         when strpos(taxon_path, '/110/') != 0 then
+         when strpos(taxon_path, '/110/') != 0 and 
+	     (collection_ref = 1 or collection_path like '/1/%')  then
            'Amphibians'
-         when strpos(taxon_path, '/111/') != 0 then
+         when strpos(taxon_path, '/111/') != 0 and 
+	     (collection_ref = 1 or collection_path like '/1/%')  then
            'Birds'
-         when strpos(taxon_path, '/97/') != 0 or strpos(taxon_path, '/98/') != 0 then
+         when (strpos(taxon_path, '/97/') != 0 or strpos(taxon_path, '/98/') != 0) and 
+	     (collection_ref = 1 or collection_path like '/1/%')  then
            'Fishes'
-         when strpos(taxon_path, '/113/') != 0 then
+         when strpos(taxon_path, '/113/') != 0 and 
+	     (collection_ref = 1 or collection_path like '/1/%')  then
            'Mammals'
-         when strpos(taxon_path, '/114/') != 0 then
+         when strpos(taxon_path, '/114/') != 0 and 
+	     (collection_ref = 1 or collection_path like '/1/%')  then
            'Reptiles'
-         when strpos(taxon_path, '/91/') != 0 then
+         when strpos(taxon_path, '/91/') != 0 and 
+	     (collection_ref = 1 or collection_path like '/1/%')  then
            'Vertebrates'
-         when strpos(taxon_path, '/37/') != 0 then
-           'Annelida (segmented worms)'
-         when strpos(taxon_path, '/61/') != 0 then
+         when strpos(taxon_path, '/37/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Annelida'
+         when strpos(taxon_path, '/61/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
            'Arthropoda - Branchiopoda'
+         when (strpos(taxon_path, '/353/') != 0 or strpos(taxon_path, '/184176/') != 0 or strpos(taxon_path, '/177325/') != 0) and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Arthropoda - Decapoda'
+         when strpos(taxon_path, '/64/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Arthropoda - Maxillopoda'
+         when (strpos(taxon_path, '/65/') != 0 or strpos(taxon_path, '/177320/') != 0) and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Arthropoda - Ostracoda'
+         when strpos(taxon_path, '/276/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Arthropoda - Peracarida'
+	 when strpos(taxon_path, '/2/') != 0 and 
+	     strpos(taxon_path, '/61/') = 0 and 
+	     strpos(taxon_path, '/353/') = 0 and 
+	     strpos(taxon_path, '/184176/') = 0 and 
+	     strpos(taxon_path, '/177325/') = 0 and
+	     strpos(taxon_path, '/64/') = 0 and
+	     strpos(taxon_path, '/65/') = 0 and 
+	     strpos(taxon_path, '/177320/') = 0 and
+	     strpos(taxon_path, '/276/') = 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+	   'Arthropoda - Others'
+         when (strpos(taxon_path, '/109920/') != 0 or strpos(taxon_path, '/30/') != 0) and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Bryozoa'
+         when strpos(taxon_path, '/72/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Cnidaria'
+         when (strpos(taxon_path, '/75/') != 0 or strpos(taxon_path, '/206101/') != 0) and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Echinodermata'
+         when strpos(taxon_path, '/44/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Hemichordata'
+         when (strpos(taxon_path, '/33/') != 0 or strpos(taxon_path, '/177319/') != 0 ) and 
+	     (collection_ref = 2 or collection_path like '/2/%')then
+           'Mollusca'
+         when strpos(taxon_path, '/28/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Nematoda'
+         when strpos(taxon_path, '/79/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Nemertea'
+         when strpos(taxon_path, '/19/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Platyhelminthes'
+         when strpos(taxon_path, '/81/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Porifera'
+         when strpos(taxon_path, '/83/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Protozoans'
+         when strpos(taxon_path, '/90/') != 0 and 
+	     (collection_ref = 2 or collection_path like '/2/%') then
+           'Tunicates'
          else
 	   'Others'
        end as "Group",
@@ -74,11 +173,46 @@ select 'BIO' as "Branch",
        sum(specimen_count_max) as specimen_count_maximum
 from specimens left join gtu_infos on specimens.gtu_ref = gtu_infos.gtu_ref
 where case
-        when strpos(taxon_path, '/91/') != 0 then
-         'VZ'
-        when strpos(taxon_path, '/37/') != 0 or 
-             strpos(taxon_path, '/61/') != 0  then
-         'IZ'
+        when strpos(taxon_path, '/91/') != 0 and 
+	     (collection_ref = 1 or collection_path like '/1/%') then
+          'VZ'
+        when (strpos(taxon_path, '/37/') != 0 or 
+              strpos(taxon_path, '/61/') != 0 or 
+              strpos(taxon_path, '/353/') != 0 or 
+              strpos(taxon_path, '/184176/') != 0 or 
+              strpos(taxon_path, '/177325/') != 0 or
+              strpos(taxon_path, '/64/') != 0 or
+              strpos(taxon_path, '/65/') != 0 or 
+              strpos(taxon_path, '/177320/') != 0 or
+              strpos(taxon_path, '/276/') != 0 or
+              (strpos(taxon_path, '/2/') != 0 and 
+	       strpos(taxon_path, '/61/') = 0 and 
+	       strpos(taxon_path, '/353/') = 0 and 
+	       strpos(taxon_path, '/184176/') = 0 and 
+	       strpos(taxon_path, '/177325/') = 0 and
+	       strpos(taxon_path, '/64/') = 0 and
+	       strpos(taxon_path, '/65/') = 0 and 
+	       strpos(taxon_path, '/177320/') = 0 and
+	       strpos(taxon_path, '/276/') = 0
+	      ) or
+	      strpos(taxon_path, '/109920/') != 0 or
+	      strpos(taxon_path, '/30/') != 0 or
+	      strpos(taxon_path, '/72/') != 0 or
+	      strpos(taxon_path, '/75/') != 0 or 
+	      strpos(taxon_path, '/206101/') != 0 or
+	      strpos(taxon_path, '/44/') != 0 or
+	      strpos(taxon_path, '/33/') != 0 or 
+	      strpos(taxon_path, '/177319/') != 0 or
+	      strpos(taxon_path, '/28/') != 0 or
+	      strpos(taxon_path, '/79/') != 0 or
+	      strpos(taxon_path, '/19/') != 0 or
+	      strpos(taxon_path, '/81/') != 0 or
+	      strpos(taxon_path, '/83/') != 0 or
+	      strpos(taxon_path, '/90/') != 0
+	     ) and 
+	     (collection_ref = 2 or collection_path like '/2/%') 
+          then
+          'IZ'
         else
           'Other'
       end in ('VZ', 'IZ')
@@ -106,12 +240,12 @@ select "Branch",
           and sub_specimen_data."Group" = main_specimen_data."Group"
           and sub_specimen_data.continent = main_specimen_data.continent
        ) as "Type Taxa",
-       (select sum(specimen_count_maximum) 
+       coalesce((select sum(specimen_count_maximum) 
         from specimens_data as sub_specimen_data
         where sub_specimen_data.type != 'specimen'
           and sub_specimen_data."Group" = main_specimen_data."Group"
           and sub_specimen_data.continent = main_specimen_data.continent
-       ) as "Type",
+       ),0) as "Type",
        count(distinct taxon_ref) as "Specimen Taxa"
 from specimens_data as main_specimen_data
 group by "Branch", "Department", "Group", continent
