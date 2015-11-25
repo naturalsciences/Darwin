6877172904f7b7d341f6d2bdf361da0381342805
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2015-11-25 18:20:36 +0100

    Second version of Director's cut report - good for recent vert inv and entomo
    refs #1885

--- data/db/reports/stats/directors_cut.sql
+++ data/db/reports/stats/directors_cut.sql
@@ -32,7 +32,14 @@ from gtu_continents
 ),
 specimens_data as 
 (
-select 'BIO' as "Branch", 
+select case 
+         when (collection_ref = 3 or collection_path like '/3/%') then
+           'PALEO'
+         when (collection_ref = 231 or collection_path like '/231/%') then
+           'MIN'
+         else
+           'BIO'
+       end as "Branch", 
        case
          when strpos(taxon_path, '/91/') != 0 and 
 	     (collection_ref = 1 or collection_path like '/1/%') then
@@ -46,16 +53,6 @@ select 'BIO' as "Branch",
               strpos(taxon_path, '/65/') != 0 or 
               strpos(taxon_path, '/177320/') != 0 or
               strpos(taxon_path, '/276/') != 0 or
-              (strpos(taxon_path, '/2/') != 0 and 
-	       strpos(taxon_path, '/61/') = 0 and 
-	       strpos(taxon_path, '/353/') = 0 and 
-	       strpos(taxon_path, '/184176/') = 0 and 
-	       strpos(taxon_path, '/177325/') = 0 and
-	       strpos(taxon_path, '/64/') = 0 and
-	       strpos(taxon_path, '/65/') = 0 and 
-	       strpos(taxon_path, '/177320/') = 0 and
-	       strpos(taxon_path, '/276/') = 0
-	      ) or
 	      strpos(taxon_path, '/109920/') != 0 or
 	      strpos(taxon_path, '/30/') != 0 or
 	      strpos(taxon_path, '/72/') != 0 or
@@ -69,11 +66,64 @@ select 'BIO' as "Branch",
 	      strpos(taxon_path, '/19/') != 0 or
 	      strpos(taxon_path, '/81/') != 0 or
 	      strpos(taxon_path, '/83/') != 0 or
-	      strpos(taxon_path, '/90/') != 0 
+	      strpos(taxon_path, '/90/') != 0  or
+              (strpos(taxon_path, '/2/') != 0 and 
+	       strpos(taxon_path, '/61/') = 0 and 
+	       strpos(taxon_path, '/353/') = 0 and 
+	       strpos(taxon_path, '/184176/') = 0 and 
+	       strpos(taxon_path, '/177325/') = 0 and
+	       strpos(taxon_path, '/64/') = 0 and
+	       strpos(taxon_path, '/65/') = 0 and 
+	       strpos(taxon_path, '/177320/') = 0 and
+	       strpos(taxon_path, '/276/') = 0
+	      )
 	     ) and 
 	     (collection_ref = 2 or collection_path like '/2/%') 
           then
-          'IZ'
+           'IZ'
+         when (
+                strpos(taxon_path, '/4/') != 0 or
+                strpos(taxon_path, '/12/') != 0 or 
+                strpos(taxon_path, '/154425/') != 0 or 
+                strpos(taxon_path, '/10/') != 0 or
+                strpos(taxon_path, '/406/') != 0 or
+                strpos(taxon_path, '/408/') != 0 or
+                strpos(taxon_path, '/50/') != 0 or
+                strpos(taxon_path, '/423/') != 0 or
+                strpos(taxon_path, '/413/') != 0 or
+                strpos(taxon_path, '/11/') != 0 or
+	        strpos(taxon_path, '/47/') != 0 or 
+	        strpos(taxon_path, '/106418/') != 0 or
+                (
+                  strpos(taxon_path, '/2/') != 0 and 
+	          strpos(taxon_path, '/4/') = 0 and 
+	          strpos(taxon_path, '/12/') = 0 and 
+	          strpos(taxon_path, '/10/') = 0 and 
+	          strpos(taxon_path, '/406/') = 0 and
+	          strpos(taxon_path, '/408/') = 0 and
+	          strpos(taxon_path, '/50/') = 0 and 
+	          strpos(taxon_path, '/423/') = 0 and
+	          strpos(taxon_path, '/413/') = 0 and 
+	          strpos(taxon_path, '/11/') = 0 and
+	          strpos(taxon_path, '/47/') = 0
+                )
+              ) and 
+	     (collection_ref = 4 or collection_path like '/4/%') 
+          then
+           'Entomology'
+         when strpos(chrono_path, '/11/') != 0 and
+             (collection_ref = 3 or collection_path like '/3/%') then
+           'Cenozoic'           
+         when strpos(chrono_path, '/26/') != 0 and
+             (collection_ref = 3 or collection_path like '/3/%') then
+           'Mesozoic'           
+         when strpos(chrono_path, '/5/') != 0 and
+             (collection_ref = 3 or collection_path like '/3/%') then
+           'Paleozoic'
+         when (collection_ref = 200 or collection_path like '/200/%') then
+           'Meteorites'
+         when (collection_ref = 276) then
+           'Gems'
          else
            'Other'
        end as "Department",
@@ -158,6 +208,61 @@ select 'BIO' as "Branch",
          when strpos(taxon_path, '/90/') != 0 and 
 	     (collection_ref = 2 or collection_path like '/2/%') then
            'Tunicates'
+         when strpos(taxon_path, '/4/') != 0  and 
+	     (collection_ref = 4 or collection_path like '/4/%') then
+	   'Insecta - Coleoptera'
+         when (strpos(taxon_path, '/12/') != 0 or strpos(taxon_path, '/154425/') != 0) and 
+	     (collection_ref = 4 or collection_path like '/4/%')then
+           'Insecta - Diptera'
+         when strpos(taxon_path, '/10/') != 0  and 
+	     (collection_ref = 4 or collection_path like '/4/%') then
+	   'Insecta - Hemiptera'
+         when strpos(taxon_path, '/406/') != 0  and 
+	     (collection_ref = 4 or collection_path like '/4/%') then
+	   'Insecta - Hymenoptera'
+         when strpos(taxon_path, '/408/') != 0  and 
+	     (collection_ref = 4 or collection_path like '/4/%') then
+	   'Insecta - Lepidoptera'
+         when strpos(taxon_path, '/50/') != 0  and 
+	     (collection_ref = 4 or collection_path like '/4/%') then
+	   'Insecta - Myriapoda'
+         when strpos(taxon_path, '/423/') != 0  and 
+	     (collection_ref = 4 or collection_path like '/4/%') then
+	   'Insecta - Odonata'
+         when strpos(taxon_path, '/413/') != 0  and 
+	     (collection_ref = 4 or collection_path like '/4/%') then
+	   'Insecta - Orthoptera'
+         when strpos(taxon_path, '/11/') != 0  and 
+	     (collection_ref = 4 or collection_path like '/4/%') then
+	   'Insecta - Trichoptera'
+         when (strpos(taxon_path, '/47/') != 0 or strpos(taxon_path, '/106418/') != 0) and 
+	     (collection_ref = 4 or collection_path like '/4/%')then
+           'Arthropoda - Chelicerata'
+	 when strpos(taxon_path, '/2/') != 0 and 
+	      strpos(taxon_path, '/4/') = 0 and 
+	      strpos(taxon_path, '/12/') = 0 and 
+	      strpos(taxon_path, '/10/') = 0 and 
+	      strpos(taxon_path, '/406/') = 0 and
+	      strpos(taxon_path, '/408/') = 0 and
+	      strpos(taxon_path, '/50/') = 0 and 
+	      strpos(taxon_path, '/423/') = 0 and
+	      strpos(taxon_path, '/413/') = 0 and 
+	      strpos(taxon_path, '/11/') = 0 and 
+	      strpos(taxon_path, '/47/') = 0 and
+	      (collection_ref = 4 or collection_path like '/4/%') then
+	   'Arthropoda - Others'
+	 when (strpos(lithology_path, '/29/') != 0 or lithology_ref = 29) and
+	   (collection_ref = 200 or collection_path like '/200/%') then
+	   'Achondrites'
+	 when (strpos(lithology_path, '/28/') != 0 or lithology_ref = 28) and
+	   (collection_ref = 200 or collection_path like '/200/%') then
+	   'Chondrites'
+	 when (strpos(lithology_path, '/169/') != 0 or lithology_ref = 169) and
+	   (collection_ref = 200 or collection_path like '/200/%') then
+	   'Irons'
+	 when (strpos(lithology_path, '/168/') != 0 or lithology_ref = 168) and
+	   (collection_ref = 200 or collection_path like '/200/%') then
+	   'Stony-Irons'
          else
 	   'Others'
        end as "Group",
@@ -169,6 +274,7 @@ select 'BIO' as "Branch",
        end as continent,
        taxon_ref,
        taxon_level_ref,
+       lithology_ref,
        type,
        sum(specimen_count_max) as specimen_count_maximum
 from specimens left join gtu_infos on specimens.gtu_ref = gtu_infos.gtu_ref
@@ -185,16 +291,6 @@ where case
               strpos(taxon_path, '/65/') != 0 or 
               strpos(taxon_path, '/177320/') != 0 or
               strpos(taxon_path, '/276/') != 0 or
-              (strpos(taxon_path, '/2/') != 0 and 
-	       strpos(taxon_path, '/61/') = 0 and 
-	       strpos(taxon_path, '/353/') = 0 and 
-	       strpos(taxon_path, '/184176/') = 0 and 
-	       strpos(taxon_path, '/177325/') = 0 and
-	       strpos(taxon_path, '/64/') = 0 and
-	       strpos(taxon_path, '/65/') = 0 and 
-	       strpos(taxon_path, '/177320/') = 0 and
-	       strpos(taxon_path, '/276/') = 0
-	      ) or
 	      strpos(taxon_path, '/109920/') != 0 or
 	      strpos(taxon_path, '/30/') != 0 or
 	      strpos(taxon_path, '/72/') != 0 or
@@ -208,14 +304,66 @@ where case
 	      strpos(taxon_path, '/19/') != 0 or
 	      strpos(taxon_path, '/81/') != 0 or
 	      strpos(taxon_path, '/83/') != 0 or
-	      strpos(taxon_path, '/90/') != 0
+	      strpos(taxon_path, '/90/') != 0 or
+              (strpos(taxon_path, '/2/') != 0 and 
+	       strpos(taxon_path, '/61/') = 0 and 
+	       strpos(taxon_path, '/353/') = 0 and 
+	       strpos(taxon_path, '/184176/') = 0 and 
+	       strpos(taxon_path, '/177325/') = 0 and
+	       strpos(taxon_path, '/64/') = 0 and
+	       strpos(taxon_path, '/65/') = 0 and 
+	       strpos(taxon_path, '/177320/') = 0 and
+	       strpos(taxon_path, '/276/') = 0
+	      ) 
 	     ) and 
 	     (collection_ref = 2 or collection_path like '/2/%') 
           then
           'IZ'
+        when (
+               strpos(taxon_path, '/4/') != 0 or
+               strpos(taxon_path, '/12/') != 0 or 
+               strpos(taxon_path, '/154425/') != 0 or 
+               strpos(taxon_path, '/10/') != 0 or
+               strpos(taxon_path, '/406/') != 0 or
+               strpos(taxon_path, '/408/') != 0 or
+               strpos(taxon_path, '/50/') != 0 or
+               strpos(taxon_path, '/423/') != 0 or
+               strpos(taxon_path, '/413/') != 0 or
+               strpos(taxon_path, '/11/') != 0 or
+	       strpos(taxon_path, '/47/') != 0 or 
+	       strpos(taxon_path, '/106418/') != 0 or
+               (
+                 strpos(taxon_path, '/2/') != 0 and 
+	         strpos(taxon_path, '/4/') = 0 and 
+	         strpos(taxon_path, '/12/') = 0 and 
+	         strpos(taxon_path, '/10/') = 0 and 
+	         strpos(taxon_path, '/406/') = 0 and
+	         strpos(taxon_path, '/408/') = 0 and
+	         strpos(taxon_path, '/50/') = 0 and 
+	         strpos(taxon_path, '/423/') = 0 and
+	         strpos(taxon_path, '/413/') = 0 and 
+	         strpos(taxon_path, '/11/') = 0 and
+	         strpos(taxon_path, '/47/') = 0
+               )
+             ) and 
+	     (collection_ref = 4 or collection_path like '/4/%') then
+	  'Entomology'
+        when strpos(chrono_path, '/11/') != 0 and
+             (collection_ref = 3 or collection_path like '/3/%') then
+          'Cenozoic'           
+        when strpos(chrono_path, '/26/') != 0 and
+             (collection_ref = 3 or collection_path like '/3/%') then
+          'Mesozoic'           
+        when strpos(chrono_path, '/5/') != 0 and
+             (collection_ref = 3 or collection_path like '/3/%') then
+          'Paleozoic'           
+        when (collection_ref = 200 or collection_path like '/200/%') then
+          'Meteorites'
+        when (collection_ref = 276) then
+          'Gems'
         else
           'Other'
-      end in ('VZ', 'IZ')
+      end in ('VZ', 'IZ', 'Entomology', 'Cenozoic', 'Mesozoic', 'Paleozoic', 'n/a', 'Meteorites', 'Gems', 'Other')
 group by "Branch", 
        "Department",
        "Group",
@@ -227,26 +375,50 @@ group by "Branch",
        end,
        taxon_ref,
        taxon_level_ref,
+       lithology_ref,
        type
 )
 select "Branch", 
        "Department", 
        "Group", 
        continent, 
-       (select count(distinct taxon_ref) 
-        from specimens_data as sub_specimen_data
-        where sub_specimen_data.taxon_level_ref >= 48 
-          and sub_specimen_data.type != 'specimen'
-          and sub_specimen_data."Group" = main_specimen_data."Group"
-          and sub_specimen_data.continent = main_specimen_data.continent
-       ) as "Type Taxa",
+       case 
+         when "Branch" = 'MIN' then
+           (
+             select count(distinct lithology_ref) 
+             from specimens_data as sub_specimen_data
+             where sub_specimen_data.type != 'specimen'
+               and sub_specimen_data."Branch" = main_specimen_data."Branch"
+               and sub_specimen_data."Department" = main_specimen_data."Department"
+               and sub_specimen_data."Group" = main_specimen_data."Group"
+               and sub_specimen_data.continent = main_specimen_data.continent
+           )
+         else
+           (
+             select count(distinct taxon_ref) 
+             from specimens_data as sub_specimen_data
+             where sub_specimen_data.taxon_level_ref >= 48 
+               and sub_specimen_data.type != 'specimen'
+               and sub_specimen_data."Branch" = main_specimen_data."Branch"
+               and sub_specimen_data."Department" = main_specimen_data."Department"
+               and sub_specimen_data."Group" = main_specimen_data."Group"
+               and sub_specimen_data.continent = main_specimen_data.continent
+           ) 
+       end as "Type Taxa",
        coalesce((select sum(specimen_count_maximum) 
         from specimens_data as sub_specimen_data
         where sub_specimen_data.type != 'specimen'
+          and sub_specimen_data."Branch" = main_specimen_data."Branch"
+          and sub_specimen_data."Department" = main_specimen_data."Department"
           and sub_specimen_data."Group" = main_specimen_data."Group"
           and sub_specimen_data.continent = main_specimen_data.continent
        ),0) as "Type",
-       count(distinct taxon_ref) as "Specimen Taxa"
+       case 
+         when "Branch" = 'MIN' then
+           count(distinct lithology_ref)
+         else
+           count(distinct taxon_ref) 
+       end as "Specimen Taxa"
 from specimens_data as main_specimen_data
 group by "Branch", "Department", "Group", continent
 order by "Branch", "Department", "Group", continent
