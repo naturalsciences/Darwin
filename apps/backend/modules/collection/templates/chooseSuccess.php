2a18b030312025777f3ee21cbb667e529a66ef1f
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-01-20 11:09:28 +0100

    Corrections of wrong tests: boolean was not given for with_js argument but 1... corrected and left possibility to pass boolean

--- apps/backend/modules/collection/templates/chooseSuccess.php
+++ apps/backend/modules/collection/templates/chooseSuccess.php
@@ -1,14 +1,14 @@
 <div class="page">
     <h1><?php echo __('Choose a collection');?></h1>
-<script language="javascript">
-$(document).ready(function () {
-    $('.col_name span').click(function () {
-      ref_element_id = getIdInClasses($(this).parent().parent());
-      ref_element_name = $(this).text();
-      $('body').trigger('close_modal');
-    });
-});
-</script>
+    <script language="javascript">
+      $(document).ready(function () {
+          $('.col_name span').click(function () {
+            ref_element_id = getIdInClasses($(this).parent().parent());
+            ref_element_name = $(this).text();
+            $('body').trigger('close_modal');
+          });
+      });
+    </script>
     <?php include_partial('collectionTree', array('institutions' => $institutions,'is_choose' => true)) ?>
     <br /><br />
 </div>
--- apps/backend/modules/lithostratigraphy/templates/chooseSuccess.php
+++ apps/backend/modules/lithostratigraphy/templates/chooseSuccess.php
@@ -1,9 +1,9 @@
 <div class="page">
     <h1><?php echo __('Choose a Lithostratigraphic unit');?></h1>
-<script language="javascript">
-$(document).ready(function () {
-    $('.result_choose').live('click', result_choose);
-});
-</script>
+    <script language="javascript">
+        $(document).ready(function () {
+            $('.result_choose').live('click', result_choose);
+        });
+    </script>
     <?php include_partial('catalogue/chooseItem', array('searchForm' => $searchForm,'is_choose' => true)) ?>
 </div>
