d087601395f6a5a8fe9f3f97d167a8f5c6981610
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-01-22 14:32:39 +0100

    Wrote premises of specimensearchActions test
    - Very basic test written to check new xxx_child_syn_included field is there ... nothing else. Wider tests will be written in future to check that call to addCatalogueRelationColumnQuery is correct
    
    fix #1874

--- test/functional/backend/specimensearchActionsTest.php
+++ test/functional/backend/specimensearchActionsTest.php
@@ -2,7 +2,8 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+$browser = new DarwinTestFunctional(new sfBrowser());
+$browser->loadData($configuration)->login('root','evil');
 
 $browser->
   get('/specimensearch/index')->
@@ -15,5 +16,6 @@ $browser->
   with('response')->begin()->
     isStatusCode(200)->
     checkElement('body', '!/This is a temporary page/')->
-  end()
-;
+    checkElement('ul#taxon_child_syn_included input[name="specimen_search_filters[taxon_child_syn_included]"][type="checkbox"]')->
+    checkElement('ul#taxon_child_syn_included input[name="specimen_search_filters[taxon_child_syn_included]"][type="checkbox"][checked="checked"]', '')->
+  end();
