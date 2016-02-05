a172ab4977da6c37b51e95c88431ce8f1aaaac94
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-02-05 18:00:23 +0100

    Code indentation tiny correction

--- apps/backend/modules/gtu/templates/_searchForm.php
+++ apps/backend/modules/gtu/templates/_searchForm.php
@@ -67,57 +67,55 @@
             <?php echo __('Show accuracy of each point');?> <input type="checkbox" id="show_accuracy" /><br /><br />
             <div style="height:400px;width:100%" id="smap"></div>
 
- <div class="pager paging_info hidden">
-   <?php echo image_tag('info2.png');?>
-    <span class="inner_text"></span>
-  </div>
-
-         </div>
-    </fieldset>
-    <?php echo $form->renderHiddenFields();?>
-    <div class="edit">
-      <input class="search_submit" type="submit" name="search" value="<?php echo __('Search'); ?>" />
-    </div>
-<div class="clear"></div>
-    <script  type="text/javascript">
-    initSearchMap();
-
-    $(document).ready(function () {
-
-      $('.catalogue_gtu').choose_form({});
-
-      $(".new_link").click( function() {
-        url = $(this).find('a').attr('href'),
-        data= $('.search_form').serialize(),
-        reg=new RegExp("(<?php echo $form->getName() ; ?>)", "g");   
-        open(url+'?'+data.replace(reg,'gtu'));
-        return false;  
-      });
-
-
-      var num_fld = 1;
-      $('.and_tag').click(function()
-      {
-        hideForRefresh('#gtu_filter');
-        $.ajax({
-            type: "GET",
-            url: $(this).attr('href') + '/num/' + (num_fld++) ,
-            success: function(html)
-            {
-              $('table.search > tbody .and_row').before(html);
-              showAfterRefresh('#gtu_filter');
-            }
-        });
-        return false;
-
-      });
-    });
-    </script>
-    <div class="search_results">
-      <div class="search_results_content"> 
+            <div class="pager paging_info hidden">
+              <?php echo image_tag('info2.png');?>
+              <span class="inner_text"></span>
+            </div>
+          </div>
+      </fieldset>
+      <?php echo $form->renderHiddenFields();?>
+      <div class="edit">
+        <input class="search_submit" type="submit" name="search" value="<?php echo __('Search'); ?>" />
       </div>
-    </div>
-    <div class='new_link'><a <?php echo !(isset($is_choose) && $is_choose)?'':'target="_blank"';?> href="<?php echo url_for('gtu/new') ?>"><?php echo __('New');?></a></div>
+      <div class="clear"></div>
+      <script type="text/javascript">
+        initSearchMap();
+
+        $(document).ready(function () {
+
+          $('.catalogue_gtu').choose_form({});
+
+          $(".new_link").click( function() {
+            url = $(this).find('a').attr('href'),
+            data= $('.search_form').serialize(),
+            reg=new RegExp("(<?php echo $form->getName() ; ?>)", "g");
+            open(url+'?'+data.replace(reg,'gtu'));
+            return false;
+          });
+
+
+          var num_fld = 1;
+          $('.and_tag').click(function()
+          {
+            hideForRefresh('#gtu_filter');
+            $.ajax({
+                type: "GET",
+                url: $(this).attr('href') + '/num/' + (num_fld++) ,
+                success: function(html)
+                {
+                  $('table.search > tbody .and_row').before(html);
+                  showAfterRefresh('#gtu_filter');
+                }
+            });
+            return false;
+
+          });
+        });
+      </script>
+      <div class="search_results">
+        <div class="search_results_content"></div>
+      </div>
+      <div class='new_link'><a <?php echo !(isset($is_choose) && $is_choose)?'':'target="_blank"';?> href="<?php echo url_for('gtu/new') ?>"><?php echo __('New');?></a></div>
   </div>
 </form> 
 </div>
