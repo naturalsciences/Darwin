ca4247819acbcb760a173b01d4c2ff0dbc438ce7
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-04-20 16:00:10 +0200

    Enhanced version of sql used to produce report of list of specimens under a given taxonomy entry

--- data/db/reports/stats/collections_specimens_list_extended.sql
+++ data/db/reports/stats/collections_specimens_list_extended.sql
@@ -1,16 +1,48 @@
-﻿with taxa_ids as 
+﻿with family_ids as 
 (
-  select subset.id, taxon_id::bigint, taxonomy.name as family_name
+  select subset.id, taxon_id::bigint, family.name as family_name
   from 
   (
     select id,unnest(string_to_array(taxon_path,'/')) as taxon_id
     from specimens
     where  (collection_ref = 1
-            or collection_path LIKE '/1/%'
-            or collection_path LIKE '/1/%'
+            or collection_path LIKE '/2/%'
+            or collection_path LIKE '/2/%'
            )
   ) as subset
-  inner join taxonomy on subset.taxon_id::bigint = taxonomy.id and taxonomy.level_ref = 34
+  inner join taxonomy as family on subset.taxon_id::bigint = family.id and family.level_ref = 34
+  where subset.taxon_id is not null 
+    and subset.taxon_id != ''
+),
+order_ids as 
+(
+  select subset.id, taxon_id::bigint, orders.name as order_name
+  from 
+  (
+    select id,unnest(string_to_array(taxon_path,'/')) as taxon_id
+    from specimens
+    where  (collection_ref = 1
+            or collection_path LIKE '/2/%'
+            or collection_path LIKE '/2/%'
+           )
+  ) as subset
+  inner join taxonomy as orders on subset.taxon_id::bigint = orders.id and orders.level_ref = 28
+  where subset.taxon_id is not null 
+    and subset.taxon_id != ''
+),
+class_ids as 
+(
+  select subset.id, taxon_id::bigint, classes.name as class_name
+  from 
+  (
+    select id,unnest(string_to_array(taxon_path,'/')) as taxon_id
+    from specimens
+    where  (collection_ref = 1
+            or collection_path LIKE '/2/%'
+            or collection_path LIKE '/2/%'
+           )
+  ) as subset
+  inner join taxonomy as classes on subset.taxon_id::bigint = classes.id and classes.level_ref = 12
   where subset.taxon_id is not null 
     and subset.taxon_id != ''
 )
@@ -32,7 +64,9 @@ select collection_name as "Collection",
        taxon_name as "Taxon",
        taxon_level_name as "Taxon level",
        taxon_status as "Taxon status",
-       taxa_ids.family_name as "Family name",
+       class_ids.class_name as "Class name",
+       order_ids.order_name as "Order name",
+       family_ids.family_name as "Family name",
        specimen_part as "Part state",
        type as "Type",
        sex as "Sex",
@@ -87,18 +121,22 @@ select collection_name as "Collection",
          ) as com    
        ) as "Comment(s)"
 from specimens
-left join taxa_ids on specimens.id = taxa_ids.id
+left join family_ids on specimens.id = family_ids.id
+left join order_ids on specimens.id = order_ids.id
+left join class_ids on specimens.id = class_ids.id
 where 
   (
-   collection_ref = 1
+   collection_ref = 2
    or 
-   collection_path LIKE '/1/%'
+   collection_path LIKE '/2/%'
   )
   and
+  collection_ref != 324
+  and
   (
-   taxon_ref = 498
+   taxon_ref = 357
    or
-   taxon_path like '%/498/%'
+   taxon_path like '%/357/%'
   ) 
-  and gtu_country_tag_value IN ('Burundi', 'Democratic Republic of the Congo', 'Rwanda')
+  --and gtu_country_tag_value IN ('Burundi', 'Democratic Republic of the Congo', 'Rwanda')
 order by collection_ref, "Countries", "Code(s)", "Taxon";
