97c134582db47dab1ddb0187b405780478d7d550
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-04-20 12:39:12 +0200

    Export made possible for P. Semal listing of specimens of a given family

---
+++ data/db/reports/stats/collections_specimens_list_extended.sql
@@ -0,0 +1,104 @@
+﻿with taxa_ids as 
+(
+  select subset.id, taxon_id::bigint, taxonomy.name as family_name
+  from 
+  (
+    select id,unnest(string_to_array(taxon_path,'/')) as taxon_id
+    from specimens
+    where  (collection_ref = 1
+            or collection_path LIKE '/1/%'
+            or collection_path LIKE '/1/%'
+           )
+  ) as subset
+  inner join taxonomy on subset.taxon_id::bigint = taxonomy.id and taxonomy.level_ref = 34
+  where subset.taxon_id is not null 
+    and subset.taxon_id != ''
+)
+select collection_name as "Collection",
+       ( select array_to_string(array_agg(codes), ';') from
+         ( select
+             coalesce(code_prefix, '') ||
+             coalesce(code_prefix_separator, '') ||
+             code ||
+             coalesce(code_suffix_separator, '') ||
+             coalesce(code_suffix, '') as codes
+           from codes
+           where referenced_relation = 'specimens'
+             and record_id = specimens.id
+             and code_category = 'main'
+         ) as cod    
+       ) as "Code(s)",
+       object_name as "Object name",
+       taxon_name as "Taxon",
+       taxon_level_name as "Taxon level",
+       taxon_status as "Taxon status",
+       taxa_ids.family_name as "Family name",
+       specimen_part as "Part state",
+       type as "Type",
+       sex as "Sex",
+       state as "Sexual state",
+       stage as "Stage",
+       social_status as "Social status",
+       complete as "Complete specimen",
+       (
+         select array_to_string(array_agg("formated_name"), ';') from
+         (
+           select formated_name
+           from people
+           where id = ANY (spec_ident_ids)
+         ) as identifiers_list
+       ) as "Identifiers",
+       acquisition_category as "Acquisition type",
+       fct_mask_date(acquisition_date, acquisition_date_mask) as "Acquisition date",
+       ig_num as "I.G.",
+       fct_mask_date(ig_date, ig_date_mask) as "I.G. date",
+       (
+         select array_to_string(array_agg("formated_name"), ';') from
+         (
+           select formated_name
+           from people
+           where id = ANY (spec_coll_ids)
+         ) as collectors_list
+       ) as "Collectors",
+       expedition_name as "Expedition",
+       gtu_code as "Sampling location code",
+       gtu_location[0] as "Sampling location latitude",
+       gtu_location[1] as "Sampling location longitude",
+       fct_mask_date(gtu_from_date, gtu_from_date_mask) as "Sampling start date",
+       fct_mask_date(gtu_to_date, gtu_to_date_mask) as "Sampling end date",
+       gtu_country_tag_value as "Countries",
+       gtu_province_tag_value as "Provinces",
+       gtu_others_tag_value as "Other location tags",
+       building as "Building",
+       floor as "Floor",
+       room as "Room",
+       "row" as "Row",
+       shelf as "Shelf",
+       container as "Container",
+       sub_container as "Sub-Container",
+       specimen_count_min as "Count min",
+       specimen_count_max as "Count max",
+       ( select array_to_string(array_agg("Comments"), '\n') from
+         ( select
+	     notion_concerned || ': ' || comment as "Comments"
+           from comments
+           where referenced_relation = 'specimens'
+             and record_id = specimens.id
+         ) as com    
+       ) as "Comment(s)"
+from specimens
+left join taxa_ids on specimens.id = taxa_ids.id
+where 
+  (
+   collection_ref = 1
+   or 
+   collection_path LIKE '/1/%'
+  )
+  and
+  (
+   taxon_ref = 498
+   or
+   taxon_path like '%/498/%'
+  ) 
+  and gtu_country_tag_value IN ('Burundi', 'Democratic Republic of the Congo', 'Rwanda')
+order by collection_ref, "Countries", "Code(s)", "Taxon";
