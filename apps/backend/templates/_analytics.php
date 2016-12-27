eb8908aa420f1caa89b89e2eaf7adbdb758b3dcf
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-12-27 16:52:35 +0100

    Upgraded Google analytics reference

--- apps/backend/templates/_analytics.php
+++ apps/backend/templates/_analytics.php
@@ -1,17 +1,5 @@
 <script type="text/javascript">
 
-/*
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '<?php echo __(sfConfig::get('dw_analytics_code', ''));?>']);
-  _gaq.push(['_trackPageview']);
-  _gaq.push(['_setCustomVar', 1, 'UserType', '<?php echo $sf_user->getDbUserType();?>', 3 ]);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-*/
-
     (function(i,s,o,g,r,a,m){
         i['GoogleAnalyticsObject']=r;
         i[r]=i[r]||function(){
