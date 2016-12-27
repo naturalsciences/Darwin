5e73516176dfbedd2b2e53270b6d85c9ba055656
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-12-27 16:48:02 +0100

    Upgraded Google analytics reference

--- apps/backend/templates/_analytics.php
+++ apps/backend/templates/_analytics.php
@@ -1,5 +1,6 @@
 <script type="text/javascript">
 
+/*
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', '<?php echo __(sfConfig::get('dw_analytics_code', ''));?>']);
   _gaq.push(['_trackPageview']);
@@ -9,5 +10,21 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+*/
+
+    (function(i,s,o,g,r,a,m){
+        i['GoogleAnalyticsObject']=r;
+        i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)
+        },i[r].l=1*new Date();
+        a=s.createElement(o),m=s.getElementsByTagName(o)[0];
+        a.async=1;
+        a.src=g;
+        m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-89067518-3', 'auto');
+
+    ga('send', 'pageview');
 
 </script>
