1c6e623721a07f2680a9baa681e77066a87804a6
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2017-02-21 09:54:04 +0100

    Alexandra taxonomy stats added

---
+++ data/db/reports/stats/stats_taxonomy_encoding.sql
@@ -0,0 +1,7 @@
+﻿/* SQL to get the encoding stats for a given user for taxonomy */
+select distinct referenced_relation, action, count(action) over (partition by referenced_relation, action)
+from users_tracking
+where user_ref = 40915 -- replace by the wished user name
+ and referenced_relation in ('taxonomy', 'classification_synonymies', 'classification_keywords')
+ and modification_date_time between '2017-02-14 00:00:01'::timestamp and '2017-02-14 23:59:59'::timestamp
+order by referenced_relation, action;
